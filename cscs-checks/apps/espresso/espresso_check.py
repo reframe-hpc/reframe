2bc4426f0e7aa3ed3f11c6b0ca6a8eae7ddb0b33
sarafael <rafael.sarmiento@cscs.ch>
2018-03-05 10:48:12 +0100

    Numerical sanity checn in quantum espresso test

--- cscs-checks/apps/espresso/espresso_check.py
+++ cscs-checks/apps/espresso/espresso_check.py
@@ -15,9 +15,15 @@ class EspressoBaseCheck(RunOnlyRegressionTest):
                                        'Espresso')
         self.executable = 'pw.x'
         self.executable_opts = '-in ausurf.in'.split()
-
-        self.sanity_patterns = sn.assert_found(
-            r'convergence has been achieved', self.stdout)
+        property_patterns = {
+            'energy': sn.extractsingle(r'!    total energy              ='
+                                       r'\s+(?P<energy>\S+) Ry',
+                                       self.stdout, 'energy', float)
+        }
+        self.sanity_patterns = sn.all([
+            sn.assert_found(r'convergence has been achieved', self.stdout),
+            sn.assert_reference(property_patterns['energy'], -11427.08612278,
+                                lower_thres=-1e-10, upper_thres=1e-10)])
         self.perf_patterns = {
             'sec': sn.extractsingle(r'electrons    :\s+(?P<sec>\S+)s CPU ',
                                     self.stdout, 'sec', float)
