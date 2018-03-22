471cd0508c5da1868f6a59f84665665fd6a43280
Theofilos Manitaras <manitaras@cscs.ch>
2018-03-22 13:37:42 +0100

    Support for multiple node check

--- cscs-checks/microbenchmarks/gpu_burn/gpu_burn_test.py
+++ cscs-checks/microbenchmarks/gpu_burn/gpu_burn_test.py
@@ -1,6 +1,7 @@
 import os
 
 from reframe.core.pipeline import RunOnlyRegressionTest
+from reframe.core.deferrable import deferrable
 import reframe.utility.sanity as sn
 
 
@@ -21,12 +22,13 @@ class GpuBurnTest(RunOnlyRegressionTest):
         # Option -d represent the time (in secs) to run the stress test
         self.executable_opts = ['-d 20']
 
-        self.sanity_patterns = sn.assert_found('OK', self.stdout)
+        self.sanity_patterns = sn.assert_eq(
+            sn.count(sn.findall('OK', self.stdout)), self.job_num_tasks())
 
         self.perf_patterns = {
-            'perf': sn.extractsingle(
+            'perf': sn.min(sn.extractall(
                 r'\S*: GPU \d+\(\S*\): (?P<perf>\S*) GF\/s', self.stdout,
-                'perf', float)
+                'perf', float))
         }
 
         self.reference = {
@@ -43,6 +45,10 @@ class GpuBurnTest(RunOnlyRegressionTest):
         self.num_tasks_per_node = 1
 
         self.maintainers = ['AJ', 'VK']
+
+    @deferrable
+    def job_num_tasks(self):
+        return self.job.num_tasks
 
 
 def _get_checks(**kwargs):
