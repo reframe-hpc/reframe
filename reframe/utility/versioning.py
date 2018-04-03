ffc32f1dae2537f1ed2df2f2d15475cb925679fd
sarafael <rafael.sarmiento@cscs.ch>
2018-04-03 17:24:52 +0200

    Fixes from discussion

--- reframe/utility/versioning.py
+++ reframe/utility/versioning.py
@@ -1,15 +1,34 @@
-import abc
-import re
+import functools
+import math
 
 
-class CustomBooleanOperations(abc.ABC):
-    """Abstract class to customize Boolean operations."""
+@functools.total_ordering
+class Version:
+    def __init__(self, version):
+        try:
+            str_base, dev = version.split('-dev')
+            base = str_base.split('.')
+            try:
+                self.dev = int(dev)
+            except ValueError:
+                raise ValueError('invalid version: %s' % version)
 
-    def __lt__(self, other):
-        return self._compare(other, lambda s, o: s < o)
+        except:
+            base = version.split('.')
+            self.dev = None
 
-    def __le__(self, other):
-        return self._compare(other, lambda s, o: s <= o)
+        if len(base) < 2 or len(base) > 3:
+            raise ValueError('invalid version: %s' % version)
+        elif len(base) == 2:
+            base.append('0')
+
+        for i, j in enumerate(base):
+            try:
+                base[i] = int(j)
+            except ValueError:
+                raise ValueError('invalid version: %s' % version)
+
+        self.base = tuple(base)
 
     def __eq__(self, other):
         return self._compare(other, lambda s, o: s == o)
@@ -17,88 +36,28 @@ class CustomBooleanOperations(abc.ABC):
     def __ge__(self, other):
         return self._compare(other, lambda s, o: s >= o)
 
-    def __gt__(self, other):
-        return self._compare(other, lambda s, o: s > o)
-
-    def __ne__(self, other):
-        return self._compare(other, lambda s, o: s != o)
-
-    @abc.abstractmethod
     def _compare(self, other, operation):
-        """Define the method for comparing versions.
+        if not isinstance(other, self.__class__):
+            return NotImplemented
 
-        This method returns a bool.
-        """
-
-
-class ReleaseTag(CustomBooleanOperations):
-    def __repr__(self):
-        return "release"
-
-    def _compare(self, other, operation):
-        if isinstance(other, self.__class__):
-            return operation(0, 0)
-        elif isinstance(other, DevelopmentTag):
-            return operation(1, 0)
+        if self.dev is None:
+            self_dev = math.inf
         else:
-            return NotImplemented
+            self_dev = self.dev
 
+        if other.dev is None:
+            other_dev = math.inf
+        else:
+            other_dev = other.dev
 
-class DevelopmentTag(CustomBooleanOperations):
-    def __init__(self, dev_level):
-        super().__init__()
-        if not isinstance(dev_level, int):
-            raise TypeError('expecting int type argument')
-
-        self.dev_level = dev_level
+        return operation((self.base, self_dev), (other.base, other_dev))
 
     def __repr__(self):
-        return "dev" + str(self.dev_level)
+        return "Version('%s')" % self.__str__()
 
-    def _compare(self, other, operation):
-        if not isinstance(other, self.__class__):
-            return NotImplemented
+    def __str__(self):
+        base = '.'.join(str(i) for i in self.base)
+        if self.dev is None:
+            return base
 
-        return operation(self.dev_level, other.dev_level)
-
-
-class Version(CustomBooleanOperations):
-    def __init__(self, version):
-        self._version = version.strip()
-
-        if not self._is_version_valid():
-            raise ValueError('invalid version format')
-
-    def _compare(self, other, operation):
-        if not isinstance(other, self.__class__):
-            return NotImplemented
-
-        return operation((self.base, self.tag), (other.base, other.tag))
-
-    def __repr__(self):
-        return self._version
-
-    @property
-    def base(self):
-        version_base = self._version.split('-')[0]
-        if len(version_base.split('.')) == 2:
-            version_base += '.0'
-
-        return tuple(int(i) for i in version_base.split('.'))
-
-    @property
-    def tag(self):
-        try:
-            dev_level = self._version.split('-dev')[1]
-            tag = DevelopmentTag(int(dev_level))
-        except:
-            tag = ReleaseTag()
-
-        return tag
-
-    def _is_version_valid(self):
-        pattern = re.compile(r'^\d+\.\d+(\.\d){0,1}\d*(-dev\d+){0,1}$')
-        if pattern.match(self._version):
-            return True
-
-        return False
+        return base + '-dev%s' % self.dev
--- unittests/test_versioning.py
+++ unittests/test_versioning.py
@@ -1,42 +1,27 @@
 import unittest
 
-from reframe.utility.versioning import DevelopmentTag, ReleaseTag, Version
+from reframe.utility.versioning import Version
 
 
 class TestVersioning(unittest.TestCase):
     def test_version_format(self):
-        self.assertIsInstance(Version('1.2'), Version)
-        self.assertIsInstance(Version('1.2.3'), Version)
-        self.assertIsInstance(Version('1.2-dev0'), Version)
-        self.assertIsInstance(Version('1.2-dev5'), Version)
-        self.assertIsInstance(Version('1.2.3-dev2'), Version)
+        Version('1.2')
+        Version('1.2.3')
+        Version('1.2-dev0')
+        Version('1.2-dev5')
+        Version('1.2.3-dev2')
+        self.assertRaises(ValueError, Version, '1')
+        self.assertRaises(ValueError, Version, '1.2a')
+        self.assertRaises(ValueError, Version, 'a.b.c')
+        self.assertRaises(ValueError, Version, '1.2.3-dev')
 
     def test_comparing_versions(self):
-        self.assertTrue(Version('1.2') == Version('1.2'))
-        self.assertTrue(Version('1.2') < Version('1.3-dev0'))
-        self.assertTrue(Version('1.3-dev0') < Version('1.3-dev1'))
-        self.assertTrue(Version('1.3-dev1') < Version('1.4'))
-        self.assertTrue(Version('1.2') == Version('1.2.0'))
-        self.assertTrue(Version('1.2') < Version('1.2.1'))
-        self.assertTrue(Version('1.2.1') < Version('1.2.2'))
-        self.assertTrue(Version('1.2-dev1') == Version('1.2.0-dev1'))
-        self.assertTrue(Version('1.2') > Version('1.2.0-dev1'))
-        self.assertTrue(Version('1.2.0') > Version('1.2.0-dev1'))
-        self.assertTrue(Version('1.2.1') > Version('1.2.0-dev1'))
-        self.assertTrue(Version('1.2.1-dev1') > Version('1.2.0-dev1'))
-        self.assertTrue(Version('1.12.3') > Version('1.2.3'))
-        self.assertTrue(Version('1.2.23') > Version('1.2.3'))
-
-    def test_version_tags(self):
-        self.assertIsInstance(Version('1.2').tag, ReleaseTag)
-        self.assertIsInstance(Version('1.2-dev0').tag, DevelopmentTag)
-
-    def test_comparing_version_tags(self):
-        self.assertTrue(DevelopmentTag(0) == DevelopmentTag(0))
-        self.assertFalse(DevelopmentTag(0) != DevelopmentTag(0))
-        self.assertTrue(DevelopmentTag(1) > DevelopmentTag(0))
-        self.assertTrue(ReleaseTag() > DevelopmentTag(0))
-        self.assertTrue(DevelopmentTag(0) < ReleaseTag())
-        self.assertTrue(ReleaseTag() == ReleaseTag())
-        self.assertFalse(ReleaseTag() != ReleaseTag())
-        self.assertFalse(ReleaseTag() > ReleaseTag())
+        self.assertLess(Version('1.2'), Version('1.2.1'))
+        self.assertLess(Version('1.2.1'), Version('1.2.2'))
+        self.assertLess(Version('1.2.2'), Version('1.3-dev0'))
+        self.assertLess(Version('1.3-dev0'), Version('1.3-dev1'))
+        self.assertLess(Version('1.3-dev1'), Version('1.3'))
+        self.assertEqual(Version('1.3'), Version('1.3.0'))
+        self.assertEqual(Version('1.3-dev1'), Version('1.3.0-dev1'))
+        self.assertGreater(Version('1.12.3'), Version('1.2.3'))
+        self.assertGreater(Version('1.2.23'), Version('1.2.3'))
