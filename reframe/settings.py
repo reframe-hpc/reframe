bfbdb3742e06e420fd0d73b2d061bf0bb35f17eb
jgp <jgp@cscs.ch>
2018-04-17 11:09:42 +0200

    settingsjg
    Signed-off-by: jgp <jgp@cscs.ch>

--- reframe/settings.py
+++ reframe/settings.py
@@ -11,17 +11,180 @@ class ReframeSettings:
     _checks_path_recurse = True
     _site_configuration = {
         'systems': {
-            # Generic system used also in unit tests
-            'generic': {
-                'descr': 'Generic example system',
-
-                # Adjust to your system's hostname
-                'hostnames': ['localhost'],
+            'daint': {
+                'descr': 'Piz Daint',
+                'hostnames': ['daint'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/regression/resources',
                 'partitions': {
                     'login': {
                         'scheduler': 'local',
                         'modules': [],
                         'access':  [],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Login nodes',
+                        'max_jobs': 4
+                    },
+
+                    'gpu': {
+                        'scheduler': 'nativeslurm',
+                        'modules': ['daint-gpu'],
+                        'access':  ['--constraint=gpu'],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Hybrid nodes (Haswell/P100)',
+                        'max_jobs': 100,
+                        # Uncomment as soon as --switches is fixed on Daint
+                        # 'resources': {
+                        #     'switches': ['--switches={num_switches}']
+                        # }
+                    },
+
+                    'mc': {
+                        'scheduler': 'nativeslurm',
+                        'modules': ['daint-mc'],
+                        'access':  ['--constraint=mc'],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Multicore nodes (Broadwell)',
+                        'max_jobs': 100,
+                        # Uncomment as soon as --switches is fixed on Daint
+                        # 'resources': {
+                        #     'switches': ['--switches={num_switches}']
+                        # }
+                    }
+                }
+            },
+
+            'dom': {
+                'descr': 'Dom TDS',
+                'hostnames': ['dom'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/regression/resources',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'modules': [],
+                        'access':  [],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Login nodes',
+                        'max_jobs': 4
+                    },
+
+                    'gpu': {
+                        'scheduler': 'nativeslurm',
+                        'modules': ['daint-gpu'],
+                        'access':  ['--constraint=gpu'],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Hybrid nodes (Haswell/P100)',
+                        'max_jobs': 100,
+                        'resources': {
+                            'switches': ['--switches={num_switches}']
+                        }
+                    },
+
+                    'mc': {
+                        'scheduler': 'nativeslurm',
+                        'modules': ['daint-mc'],
+                        'access':  ['--constraint=mc'],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Multicore nodes (Broadwell)',
+                        'max_jobs': 100,
+                        'resources': {
+                            'switches': ['--switches={num_switches}']
+                        }
+                    },
+                }
+            },
+
+            'kesch': {
+                'descr': 'Kesch MCH',
+                'hostnames': ['keschln-\d+'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/regression/resources',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'environs': ['PrgEnv-gnu', 'PrgEnv-cray'],
+                        'descr': 'Kesch login nodes',
+                    },
+                    'pn': {
+                        'scheduler': 'nativeslurm',
+                        'access': ['--partition=pn-regression'],
+                        'environs': ['PrgEnv-gnu', 'PrgEnv-cray'],
+                        'descr': 'Kesch post-processing nodes'
+                    },
+
+                    'cn': {
+                        'scheduler': 'nativeslurm',
+                        'access': ['--partition=cn-regression'],
+                        'environs': ['PrgEnv-gnu', 'PrgEnv-cray'],
+                        'descr': 'Kesch compute nodes',
+                        'resources': {
+                            '_rfm_gpu': ['--gres=gpu:{num_gpus_per_node}']
+                        }
+                    }
+                }
+            },
+
+            'leone': {
+                'descr': 'Leone',
+                'hostnames': ['leone'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/regression/resources',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'environs': ['PrgEnv-gnu'],
+                        'descr': 'Leone login nodes',
+                        'max_jobs': 1
+                    },
+
+                    'normal': {
+                        'scheduler': 'nativeslurm',
+                        'environs': ['PrgEnv-gnu'],
+                        'descr': ('Leone compute nodes - '
+                                  'default partition'),
+                        'max_jobs': 10
+                    },
+                }
+            },
+
+            'monch': {
+                'descr': 'Monch PASC',
+                'hostnames': ['monch'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/regression/resources',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'environs': ['PrgEnv-gnu'],
+                        'descr': 'Monch login nodes',
+                        'max_jobs': 1
+                    },
+
+                    'compute': {
+                        'scheduler': 'slurm+mpirun',
+                        'access': ['--partition=compute'],
+                        'environs': ['PrgEnv-gnu'],
+                        'descr': 'Monch compute nodes',
+                        'max_jobs': 10
+                    }
+                }
+            },
+
+            # Generic system used for cli unit tests
+            'generic': {
+                'descr': 'Generic example system',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'modules': [],
+                        'access': [],
                         'environs': ['builtin-gcc'],
                         'descr': 'Login nodes'
                     }
@@ -30,7 +193,54 @@ class ReframeSettings:
         },
 
         'environments': {
+            'kesch': {
+                'PrgEnv-gnu': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-gnu'],
+                    'cc': 'mpicc',
+                    'cxx': 'mpicxx',
+                    'ftn': 'mpif90',
+                },
+            },
+            'leone': {
+                'PrgEnv-gnu': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-gnu/leone-foss-2016b'],
+                    'cc':  'mpicc',
+                    'cxx': 'mpicxx',
+                    'ftn': 'mpif90',
+                },
+            },
+            'monch': {
+                'PrgEnv-gnu': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-gnu'],
+                    'cc':  'mpicc',
+                    'cxx': 'mpicxx',
+                    'ftn': 'mpif90',
+                }
+            },
             '*': {
+                'PrgEnv-cray': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-cray'],
+                },
+
+                'PrgEnv-gnu': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-gnu'],
+                },
+
+                'PrgEnv-intel': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-intel'],
+                },
+
+                'PrgEnv-pgi': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-pgi'],
+                },
+
                 'builtin': {
                     'type': 'ProgEnvironment',
                     'cc':  'cc',
@@ -44,6 +254,32 @@ class ReframeSettings:
                     'cxx': 'g++',
                     'ftn': 'gfortran',
                 }
+            }
+        },
+
+        'modes': {
+            '*': {
+                'maintenance': [
+                    '--exec-policy=async',
+                    '--strict',
+                    '--output=$APPS/UES/$USER/regression/maintenance',
+                    '--logdir=$APPS/UES/$USER/regression/maintenance/logs',
+                    '--stage=$SCRATCH/regression/maintenance/stage',
+                    '--reservation=maintenance',
+                    '--save-log-files',
+                    '--tag=maintenance',
+                    '--timestamp=%F_%H-%M-%S'
+                ],
+                'production': [
+                    '--exec-policy=async',
+                    '--strict',
+                    '--output=$APPS/UES/$USER/regression/production',
+                    '--logdir=$APPS/UES/$USER/regression/production/logs',
+                    '--stage=$SCRATCH/regression/production/stage',
+                    '--save-log-files',
+                    '--tag=production',
+                    '--timestamp=%F_%H-%M-%S'
+                ]
             }
         }
     }
