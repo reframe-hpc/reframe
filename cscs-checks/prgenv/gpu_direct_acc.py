0ae72781ffa6b06fb3f9b7818be350ffc0852053
ajocksch <andreas.jocksch@cscs.ch>
2018-05-09 12:17:45 +0200

    adapted gpu_direct_acc

--- cscs-checks/prgenv/gpu_direct_acc.py
+++ cscs-checks/prgenv/gpu_direct_acc.py
@@ -13,23 +13,25 @@ class GpuDirectAccCheck(RegressionTest):
             self.modules = ['craype-accel-nvidia60']
             self._pgi_flags = '-acc -ta=tesla:cc60'
             self.variables = {'MPICH_RDMA_ENABLED_CUDA': '1'}
+            self.num_tasks = 2
+            self.num_gpus_per_node = 1
+            self.num_tasks_per_node = 1
         elif self.current_system.name in ['kesch']:
             self.modules = ['craype-accel-nvidia35']
             self._pgi_flags = '-acc -ta=tesla:cc35'
             self.variables = {'MPICH_RDMA_ENABLED_CUDA': '1',
                               'MV2_USE_CUDA': '1',
                               'MV2_USE_GPUDIRECT': '1',
-                              'G2G': '1',
-                              'MPICH_G2G_PIPELINE': '1'}
+                              'G2G': '1'}
+            self.num_tasks = 8
+            self.num_gpus_per_node = 8
+            self.num_tasks_per_node = 8
 
-        self.num_tasks = 2
-        self.num_gpus_per_node = 1
-        self.sourcepath = 'gpu_direct_acc.f90'
-        self.num_tasks_per_node = 1
+        self.sourcepath = 'gpu_direct_acc.F90'
 
-        result = sn.extractsingle(r'Result :\s+(?P<result>\d+\.?\d*)',
-            self.stdout, 'result', float)
-        self.sanity_patterns = sn.assert_reference(result, 1., -1e-5, 1e-5)
+        result = sn.findall(r'Result :\s+OK', self.stdout)
+        self.sanity_patterns = sn.all(
+            sn.chain([sn.assert_eq(sn.count(result), 1)]))
 
         self.maintainers = ['AJ', 'VK']
         self.tags = {'production'}
