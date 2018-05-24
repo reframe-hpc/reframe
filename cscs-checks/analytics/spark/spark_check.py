c74ee7605fd0a8c3b72f2e6ff2238a20bd5e4462
Theofilos Manitaras <manitaras@cscs.ch>
2018-05-24 09:21:58 +0200

    Address PR comments

--- cscs-checks/analytics/spark/spark_check.py
+++ cscs-checks/analytics/spark/spark_check.py
@@ -1,31 +1,25 @@
 import os
 import math
 
+import reframe as rfm
 import reframe.utility.sanity as sn
-from reframe.core.pipeline import RunOnlyRegressionTest
 from reframe.core.launchers import LauncherWrapper
 from reframe.core.launchers.registry import getlauncher
 
 
-class SparkAnalyticsCheck(RunOnlyRegressionTest):
-    def __init__(self, **kwargs):
-        super().__init__('spark_check',
-                         os.path.dirname(__file__), **kwargs)
+@rfm.simple_test
+class SparkAnalyticsCheck(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        super().__init__()
         self.descr = 'Simple calculation of pi with Spark'
         self.valid_systems = ['daint:gpu', 'daint:mc']
         self.valid_prog_environs = ['PrgEnv-cray']
-
         self.modules = ['analytics']
-
         self.executable = 'start_analytics -t "spark-submit spark_pi.py"'
 
         pi_value = sn.extractsingle(r'Pi is roughly\s+(?P<pi>\S+)',
                                     self.stdout, 'pi', float)
-        pi_reference = math.pi
-        pi_diff = sn.abs(pi_value - pi_reference)
-
-        self.sanity_patterns = sn.assert_lt(pi_diff, 0.01)
-
+        self.sanity_patterns = sn.assert_lt(sn.abs(pi_value - math.pi), 0.01)
         self.maintainers = ['TM']
         self.tags = {'production'}
 
@@ -36,9 +30,8 @@ class SparkAnalyticsCheck(RunOnlyRegressionTest):
         else:
             self.num_tasks = 72
             self.num_tasks_per_node = 18
+
         super().setup(partition, environ, **job_opts)
+        # The job launcher has to be changed since the `start_analytics`
+        # script is not used with srun.
         self.job.launcher = getlauncher('local')()
-
-
-def _get_checks(**kwargs):
-    return [SparkAnalyticsCheck(**kwargs)]
