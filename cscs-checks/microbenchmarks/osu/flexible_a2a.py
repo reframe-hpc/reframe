c346f5a59e58f0ada5bb57fded553f667217f83b
rafael <rafael.sarmiento@cscs.ch>
2018-05-30 08:36:03 +0200

    Flexible a2a

---
+++ cscs-checks/microbenchmarks/osu/flexible_a2a.py
@@ -0,0 +1,28 @@
+import os
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.simple_test
+class FlexibleAlltoallTest(rfm.RegressionTest):
+    def __init__(self):
+        super().__init__()
+        self.valid_systems = ['daint:gpu', 'daint:mc',
+                              'dom:gpu', 'dom:mc',
+                              'kesch:cn', 'kesch:pn',
+                              'leone:normal', 'monch:compute']
+        self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-gnu',
+                                    'PrgEnv-intel', 'PrgEnv-pgi']
+        if self.current_system.name == 'kesch':
+            self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-gnu',
+                                        'PrgEnv-intel']
+
+        self.descr = 'Flexible alltoall osu microbenchmark'
+        self.executable = 'osu_alltoall'
+        self.maintainers = ['RS', 'VK']
+        self.num_tasks_per_node = 1
+        self.num_tasks = 0
+        self.sanity_patterns = sn.assert_found(r'^1048576', self.stdout)
+
+    def compile(self):
+        super().compile(makefile='Makefile_alltoall')
