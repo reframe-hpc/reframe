c537f3bc3b7af04c3be7673089fa34a7c324e8fb
ajocksch <andreas.jocksch@cscs.ch>
2018-06-14 17:25:07 +0200

    new reference numbers for dom and daint; simplified sanity check

--- cscs-checks/mch/automatic_arrays.py
+++ cscs-checks/mch/automatic_arrays.py
@@ -11,7 +11,7 @@ class AutomaticArraysCheck(rfm.RegressionTest):
                                     'PrgEnv-gnu']
         if self.current_system.name in ['daint', 'dom']:
             self.modules = ['craype-accel-nvidia60']
-            self._pgi_flags = '-acc -ta=tesla:cc60'
+            self._pgi_flags = '-acc -ta=tesla:cc60 -Mnorpath'
         elif self.current_system.name in ['kesch']:
             self.modules = ['craype-accel-nvidia35']
             self._pgi_flags = '-O2 -ta=tesla,cc35,cuda8.0'
@@ -20,7 +20,7 @@ class AutomaticArraysCheck(rfm.RegressionTest):
         self.num_gpus_per_node = 1
         self.num_tasks_per_node = 1
         self.sourcepath = 'automatic_arrays.f90'
-        self.sanity_patterns = sn.assert_found(r'Result:\s+OK', self.stdout)
+        self.sanity_patterns = sn.assert_found(r'Result: ', self.stdout)
         self.perf_patterns = {
             'perf': sn.extractsingle(r'Timing:\s+(?P<perf>\S+)',
                                      self.stdout, 'perf', float)
@@ -28,18 +28,18 @@ class AutomaticArraysCheck(rfm.RegressionTest):
 
         self.arrays_reference = {
             'PrgEnv-cray': {
-                'daint:gpu': {'perf': (1.4E-04, None, 0.15)},
-                'dom:gpu': {'perf': (1.4E-04, None, 0.15)},
+                'daint:gpu': {'perf': (5.7E-05, None, 0.15)},
+                'dom:gpu': {'perf': (5.8E-05, None, 0.15)},
                 'kesch:cn': {'perf': (2.9E-04, None, 0.15)},
             },
             'PrgEnv-gnu': {
-                'daint:gpu': {'perf': (7.4E-03, None, 0.15)},
-                'dom:gpu': {'perf': (7.4E-03, None, 0.15)},
+                'daint:gpu': {'perf': (7.0E-03, None, 0.15)},
+                'dom:gpu': {'perf': (7.3E-03, None, 0.15)},
                 'kesch:cn': {'perf': (6.5E-03, None, 0.15)},
             },
             'PrgEnv-pgi': {
-                'daint:gpu': {'perf': (1.4E-04, None, 0.15)},
-                'dom:gpu': {'perf': (1.4E-04, None, 0.15)},
+                'daint:gpu': {'perf': (6.4E-05, None, 0.15)},
+                'dom:gpu': {'perf': (6.3E-05, None, 0.15)},
                 'kesch:cn': {'perf': (1.4E-04, None, 0.15)},
             }
         }
