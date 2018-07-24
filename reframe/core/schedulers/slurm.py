6f99eab6b35c99a66da1b4f4de10dda6bbad0ee7
Vasileios Karakasis <karakasis@cscs.ch>
2018-07-24 20:32:57 +0200

    Address PR comments

--- reframe/core/schedulers/slurm.py
+++ reframe/core/schedulers/slurm.py
@@ -79,7 +79,6 @@ class SlurmJob(sched.Job):
     def emit_preamble(self):
         preamble = [
             self._format_option(self.name, '--job-name="{0}"'),
-            self._format_option(self.name, '--job-name="{0}"'),
             self._format_option('%d:%d:%d' % self.time_limit, '--time={0}'),
             self._format_option(self.num_tasks, '--ntasks={0}'),
             self._format_option(self.num_tasks_per_node,
