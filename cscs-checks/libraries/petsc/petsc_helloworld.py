098e47ebd1a603fc0ec4c39c2490fc3f3fba60e4
Theofilos Manitaras <manitaras@cscs.ch>
2018-07-25 10:47:07 +0200

    Address PR comments
    * Also use the build systems instead of the `compile` method.

--- cscs-checks/libraries/petsc/petsc_helloworld.py
+++ cscs-checks/libraries/petsc/petsc_helloworld.py
@@ -1,5 +1,3 @@
-import filecmp
-import os
 import reframe as rfm
 import reframe.utility.sanity as sn
 
@@ -9,7 +7,7 @@ class PetscPoisson2DCheck(rfm.RegressionTest):
     def __init__(self, variant):
         super().__init__()
         self.descr = ('Compile/run PETSc 2D Poisson example with cray-petsc '
-                      '(%s linking case)') % variant
+                      '(%s linking)') % variant
         self.valid_systems = ['daint:gpu', 'daint:mc',
                               'dom:gpu', 'dom:mc']
         self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-gnu',
@@ -18,19 +16,16 @@ class PetscPoisson2DCheck(rfm.RegressionTest):
         self.modules = ['cray-petsc']
         self.num_tasks = 16
         self.num_tasks_per_node = 8
-        self.dynamic = True if variant == 'dynamic' else False
+        self.build_system = 'SingleSource'
+        if variant == 'dynamic':
+            self.build_system.cflags = ['-dynamic']
+
         self.executable_opts = ['-da_grid_x 4', '-da_grid_y 4', '-ksp_monitor']
 
-        norms = sn.extractall(r'\s+\d+\s+KSP Residual norm\s+(?P<norm>\S+)',
-                              self.stdout, 'norm', float)
-
         # Check the final residual norm for convergence
-        self.sanity_patterns = sn.assert_lt(norms[-1], 1.0e-5)
+        norm = sn.extractsingle(r'\s+\d+\s+KSP Residual norm\s+(?P<norm>\S+)',
+                                self.stdout, 'norm', float, -1)
+        self.sanity_patterns = sn.assert_lt(norm, 1.0e-5)
 
         self.tags = {'production'}
         self.maintainers = ['WS', 'AJ', 'TM']
-
-    def compile(self):
-        if self.dynamic:
-            self.current_environ.cxxflags = '-dynamic'
-        super().compile()
