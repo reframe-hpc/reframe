2d6be1f8975cc6fa5c4fff4a6f2a02ccc8a408ef
Theofilos Manitaras <manitaras@cscs.ch>
2018-08-20 07:54:11 +0200

    Adapt trilinos to build systems

--- cscs-checks/libraries/math/trilinos_compile_run.py
+++ cscs-checks/libraries/math/trilinos_compile_run.py
@@ -1,28 +1,13 @@
 import os
+
+import reframe as rfm
 import reframe.utility.sanity as sn
 
-from reframe.core.pipeline import RegressionTest
 
-
-class TrilinosTest(RegressionTest):
-    def __init__(self, linkage, **kwargs):
-        super().__init__('trilinos_compile_run_%s' % linkage,
-                         os.path.dirname(__file__), **kwargs)
-
-        self.flags = ' -DHAVE_MPI -DEPETRA_MPI -lparmetis -%s ' % linkage
-        self.prgenv_flags = {
-            'PrgEnv-cray': ' -homp -hstd=c++11 -hmsglevel_4 ',
-            'PrgEnv-gnu': ' -fopenmp -std=c++11 -w -fpermissive ',
-            'PrgEnv-intel': ' -qopenmp -w -std=c++11 ',
-            'PrgEnv-pgi': ' -mp -w '
-        }
-        self.descr = 'Trilinos ' + linkage.capitalize()
-        self.sourcepath = 'example_AmesosFactory_HB.cpp'
-        #self.sourcepath = 'trilinos_compile_run.cpp'
-        self.input_file = os.path.join(self.current_system.resourcesdir,
-                                       'Trilinos', 'trilinos_compile_run.rua')
-
-        self.executable_opts = self.input_file.split()
+@rfm.parameterized_test(['static'], ['dynamic'])
+class TrilinosTest(rfm.RegressionTest):
+    def __init__(self, linkage):
+        super().__init__()
         self.valid_systems = ['daint:gpu', 'daint:mc',
                               'dom:gpu', 'dom:mc']
 
@@ -33,6 +18,21 @@ class TrilinosTest(RegressionTest):
             self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-gnu',
                                         'PrgEnv-intel']
 
+        self.build_system = 'SingleSource'
+        self.build_system.ldflags = ['-%s' % linkage]
+        self.build_system.cppflags = ['-DHAVE_MPI', '-DEPETRA_MPI']
+        self.build_system.cxxflags = ['-lparmetis']
+        self.prgenv_flags = {
+            'PrgEnv-cray': ['-homp', '-hstd=c++11', '-hmsglevel_4'],
+            'PrgEnv-gnu': ['-fopenmp', '-std=c++11', '-w', '-fpermissive'],
+            'PrgEnv-intel': ['-qopenmp', '-w', '-std=c++11'],
+            'PrgEnv-pgi': ['-mp', '-w']
+        }
+        self.sourcepath = 'example_AmesosFactory_HB.cpp'
+        # self.sourcepath = 'trilinos_compile_run.cpp'
+        input_file = os.path.join(self.current_system.resourcesdir,
+                                  'Trilinos', 'trilinos_compile_run.rua')
+        self.executable_opts = input_file.split()
         self.modules = ['cray-mpich', 'cray-hdf5-parallel',
                         'cray-tpsl', 'cray-trilinos']
         self.num_tasks = 2
@@ -45,18 +45,10 @@ class TrilinosTest(RegressionTest):
         self.tags = {'production'}
 
     def setup(self, partition, environ, **job_opts):
+        prgenv_flags = self.prgenv_flags[environ.name]
+        self.build_system.cxxflags += prgenv_flags
         if environ.name == 'PrgEnv-intel':
             # CrayBug/836679
             self.modules += ['gcc/4.9.3']
 
         super().setup(partition, environ, **job_opts)
-
-    def compile(self):
-        prgenv_flags = self.prgenv_flags[self.current_environ.name]
-        self.current_environ.cxxflags = self.flags + prgenv_flags
-        super().compile()
-
-
-def _get_checks(**kwargs):
-    return [TrilinosTest('dynamic', **kwargs),
-            TrilinosTest('static',  **kwargs)]
