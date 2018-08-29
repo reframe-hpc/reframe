72247de3d3377d8318388273e706627a505acaf0
Vasileios Karakasis <karakasis@cscs.ch>
2018-08-29 15:54:00 +0200

    Add documentation for the build systems feature
    - Tutorials were udpated
    - Reference guide was updated

--- reframe/core/pipeline.py
+++ reframe/core/pipeline.py
@@ -125,6 +125,20 @@ class RegressionTest:
     #:        Support for Git repositories was added.
     sourcesdir = fields.StringField('sourcesdir', allow_none=True)
 
+    #: The build system to be used for this test.
+    #: If not specified, the framework will try to figure it out automatically
+    #: based on the value the :attr:`sourcepath`.
+    #:
+    #: This field may be set using either a string referring to a concrete build
+    #: system class name (see `build systems <reference.html#build-systems>`__)
+    #: or an instance of :class:`reframe.core.buildsystems.BuildSystem`.
+    #: The former is the recommended way.
+    #:
+    #:
+    #: :type: :class:`str` or :class:`reframe.core.buildsystems.BuildSystem`.
+    #: :default: :class:`None`.
+    #:
+    #: .. versionadded:: 2.14
     build_system = BuildSystemField('build_system', allow_none=True)
 
     #: List of shell commands to be executed before compiling.
