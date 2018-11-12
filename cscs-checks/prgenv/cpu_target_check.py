77613d49db43ea973a70697e0f40fcb3f5a7b9f4
Theofilos Manitaras <manitaras@cscs.ch>
2018-11-12 08:15:38 +0100

    Address PR comments

--- cscs-checks/prgenv/cpu_target_check.py
+++ cscs-checks/prgenv/cpu_target_check.py
@@ -10,10 +10,9 @@ class CrayCPUTargetTest(rfm.RunOnlyRegressionTest):
         self.valid_systems = ['daint:login', 'dom:login']
         self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-gnu',
                                     'PrgEnv-intel', 'PrgEnv-pgi']
-        self.num_tasks = 1
-        self.num_tasks_per_node = 1
-        self.executable = 'echo ${CRAY_CPU_TARGET:-Unknown}'
-        self.sanity_patterns = sn.assert_not_found('Unknown', self.stdout)
+        self.executable = 'echo CRAY_CPU_TARGET=$CRAY_CPU_TARGET'
+        self.sanity_patterns = sn.assert_found(r'CRAY_CPU_TARGET=\S+',
+                                               self.stdout)
 
         self.maintainers = ['TM']
-        self.tags = {'maintenance'}
+        self.tags = {'production', 'maintenance'}
