9abcc8611d289afa0fd0e4da131830471b1a8362
jgp <jgp@cscs.ch>
2018-11-27 18:52:07 +0100

    fix for @victorusu
    Signed-off-by: jgp <jgp@cscs.ch>

--- cscs-checks/tools/profiling_and_debugging/gdb4hpc.py
+++ cscs-checks/tools/profiling_and_debugging/gdb4hpc.py
@@ -3,44 +3,86 @@ import os
 import reframe as rfm
 import reframe.utility.sanity as sn
 
+from reframe.core.launchers import LauncherWrapper
 
-@rfm.simple_test
-class SbucheckCommandCheck(rfm.RunOnlyRegressionTest):
-    def __init__(self):
+
+class Gdb4hpcCheck(rfm.RegressionTest):
+    def __init__(self, lang, extension):
         super().__init__()
-        self.valid_systems = ['daint:login', 'dom:login']
-        self.descr = 'Cray parallel debugger'
+        self.name = 'Gdb4hpcCheck_' + lang.replace('+', 'p')
+        self.descr = 'Cray gdb4hpc check for %s' % lang
+        self.lang = lang
+        self.extension = extension
+        self.build_system = 'Make'
+        # NOTE: Restrict concurrency to allow creation of Fortran modules
+        if lang == 'F90':
+            self.build_system.max_concurrency = 1
+
+        #self.executable = './jacobi'
+        self.executable = 'gdb4hpc -v'
+        self.sourcesdir = os.path.join('src', lang)
         self.valid_prog_environs = ['PrgEnv-gnu']
         self.modules = ['gdb4hpc']
-
-        self.executable  = 'gdb4hpc -v'
-        self.executable += ';cd F90; pwd'
-        self.executable += ";make -j 1 CC='cc' CXX='CC' FC='ftn' NVCC='nvcc' CFLAGS='-g -O2 -fopenmp' CXXFLAGS='-g -O2 -fopenmp' FCFLAGS='-g -O2 -fopenmp'"
-        self.executable += ';ls -lrt'
-        self.executable += ';gdb4hpc -b ./gdb4hpc.in &> gdb4hpc.rpt'
-
-        self.variables = {
-            'OMP_PROC_BIND': 'true',
-            'CRAYPE_LINK_TYPE': 'dynamic'
+        self.prgenv_flags = {
+            # 'PrgEnv-cray': ' -O2 -homp',
+            'PrgEnv-gnu': ['-g', '-O2', '-fopenmp'],
+            # 'PrgEnv-intel': ' -O2 -qopenmp',
+            # 'PrgEnv-pgi': ' -O2 -mp'
         }
+#        if self.current_system.name == 'kesch':
+#            self.exclusive_access = True
 
         self.num_tasks = 1
         self.num_tasks_per_node = 1
+        self.num_cpus_per_task = 4
+        self.num_tasks_per_core= 1
+        self.time_limit = (0,0,30)
+        self.num_iterations = 5
+        self.variables = {
+            'CRAYPE_LINK_TYPE': 'dynamic',
+            'OMP_NUM_THREADS': str(self.num_cpus_per_task),
+            'ITERATIONS': str(self.num_iterations),
+            'OMP_PROC_BIND': 'true',
+        }
+        self.maintainers = ['MK', 'JG']
         self.tags = {'production'}
+
+        # gdb4hpc has its own way to launch a job:
+        self.post_run = ['gdb4hpc -b ./gdb4hpc.in &> gdb4hpc.rpt']
+
+    def setup(self, partition, environ, **job_opts):
+        super().setup(partition, environ, **job_opts)
+        prgenv_flags = self.prgenv_flags[self.current_environ.name]
+        self.build_system.cflags = prgenv_flags
+        self.build_system.cxxflags = prgenv_flags
+        self.build_system.fflags = prgenv_flags
+
+
+@rfm.required_version('>=2.14')
+@rfm.parameterized_test(['F90', 'F90'])
+#@rfm.parameterized_test(['F90', 'F90'], ['C', 'c'], ['C++', 'cc'])
+class Gdb4hpcCpuCheck(Gdb4hpcCheck):
+    def __init__(self, lang, extension):
+        super().__init__(lang, extension)
+
+        self.valid_systems = ['dom:gpu']
+#        self.valid_systems = ['daint:gpu', 'daint:mc',
+#                              'dom:gpu', 'dom:mc']
+
+#        self.valid_systems.append('kesch:cn')
+#        if self.current_system.name == 'kesch' and self.lang == 'C':
+#            self.build_system.ldflags = ['-lm']
 
         self.sanity_patterns = sn.all([
             sn.assert_reference(sn.extractsingle(
                 r'^tst\{0\}:\s+(?P<result>\d+.\d+[eE]-\d+)',
-                'F90/gdb4hpc.rpt',
+                'gdb4hpc.rpt',
                 'result', float), 2.572e-6, -1e-1, 1.0e-1),
 
             sn.assert_found(r'gdb4hpc 3.0 - Cray Line Mode Parallel Debugger',
-                            'F90/gdb4hpc.rpt'),
+                            'gdb4hpc.rpt'),
 
             sn.assert_found(r'Shutting down debugger and killing application',
-                            'F90/gdb4hpc.rpt')
+                            'gdb4hpc.rpt')
         ])
-
-
-        self.maintainers = ['JG', 'MK']
 
