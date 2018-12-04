cb3314f16548156ded5db3d89d5d5cd07c39b6e4
rafael <rafael.sarmiento@cscs.ch>
2018-12-04 14:03:07 +0100

    show bug

--- cscs-checks/prgenv/ssh_environ_check.py
+++ cscs-checks/prgenv/ssh_environ_check.py
@@ -2,20 +2,15 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 
-@rfm.parameterized_test(['CRAY_CPU_TARGET'], ['CRAYPE_NETWORK_TARGET'],
-                        ['MODULEPATH'], ['MODULESHOME'], ['PE_PRODUCT_LIST'],
-                        ['SCRATCH'], ['XDG_RUNTIME_DIR'])
+@rfm.simple_test
 class SSHLoginEnvCheck(rfm.RunOnlyRegressionTest):
-    def __init__(self, envvar):
+    def __init__(self):
         super().__init__()
-        self.descr = ('Check if $%s is defined when accessing '
-                      'remotely over SSH' % envvar)
+        self.descr = ('Check if a set of environment variables is '
+                      'defined when accessing remotely over SSH')
         self.valid_systems = ['daint:login', 'dom:login']
         self.valid_prog_environs = ['PrgEnv-cray']
-        self.executable = 'ssh'
-        self.executable_opts = [self.current_system.name,
-                                'echo', "'$%s'" % envvar]
-        values = {
+        reference = {
             'CRAY_CPU_TARGET': 'haswell',
             'CRAYPE_NETWORK_TARGET': 'aries',
             'MODULEPATH': r'[\S+]',
@@ -27,6 +22,14 @@ class SSHLoginEnvCheck(rfm.RunOnlyRegressionTest):
             'SCRATCH': r'/scratch/[\S+]',
             'XDG_RUNTIME_DIR': r'/run/user/[\d+]'
         }
-        self.sanity_patterns = sn.assert_found(values[envvar], self.stdout)
+        self.executable = 'ssh'
+        echo_args = ' '.join('%s=$%s' % (i, i)  for i in reference.keys())
+        self.executable_opts = [self.current_system.name,
+                                'echo', "'%s'" % echo_args]
+        # self.sanity_patterns = sn.all([sn.assert_found('CRAY_CPU_TARGET=haswell', self.stdout)])
+        self.sanity_patterns = sn.all(
+            sn.map(lambda x: sn.assert_found(x, self.stdout), ['CRAY_CPU_TARGET=haswell']))
+        # self.sanity_patterns = sn.map(lambda k, v: sn.assert_found('%s=%s' % (k, v), self.stdout),
+        #     reference.keys(), reference.values()))
         self.maintainers = ['RS', 'LM']
         self.tags = {'maintenance'}
