c2d4e30f15d4bdd2ca591c3b1be6dc25e5d0a2af
jgp <jgp@cscs.ch>
2018-12-17 18:19:17 +0100

    answering comments

--- cscs-checks/tools/profiling_and_debugging/gdb4hpc.py
+++ cscs-checks/tools/profiling_and_debugging/gdb4hpc.py
@@ -18,25 +18,19 @@ class Gdb4hpcCheck(rfm.RegressionTest):
         if lang == 'F90':
             self.build_system.max_concurrency = 1
 
-        #self.executable = './jacobi'
         self.executable = 'gdb4hpc -v'
         self.sourcesdir = os.path.join('src', lang)
         self.valid_prog_environs = ['PrgEnv-gnu']
         self.modules = ['gdb4hpc']
-        self.prgenv_flags = {
-            # 'PrgEnv-cray': ' -O2 -homp',
-            'PrgEnv-gnu': ['-g', '-O2', '-fopenmp'],
-            # 'PrgEnv-intel': ' -O2 -qopenmp',
-            # 'PrgEnv-pgi': ' -O2 -mp'
-        }
-#        if self.current_system.name == 'kesch':
-#            self.exclusive_access = True
+        self.prgenv_flags = ['-g -O2 -fopenmp']
+        self.build_system.cflags = self.prgenv_flags
+        self.build_system.cxxflags = self.prgenv_flags
+        self.build_system.fflags = self.prgenv_flags
 
         self.num_tasks = 1
         self.num_tasks_per_node = 1
         self.num_cpus_per_task = 4
         self.num_tasks_per_core= 1
-        self.time_limit = (0,0,30)
         self.num_iterations = 5
         self.variables = {
             'CRAYPE_LINK_TYPE': 'dynamic',
@@ -50,16 +44,10 @@ class Gdb4hpcCheck(rfm.RegressionTest):
         # gdb4hpc has its own way to launch a job:
         self.post_run = ['gdb4hpc -b ./gdb4hpc.in &> gdb4hpc.rpt']
 
-    def setup(self, partition, environ, **job_opts):
-        super().setup(partition, environ, **job_opts)
-        prgenv_flags = self.prgenv_flags[self.current_environ.name]
-        self.build_system.cflags = prgenv_flags
-        self.build_system.cxxflags = prgenv_flags
-        self.build_system.fflags = prgenv_flags
-
 
 @rfm.required_version('>=2.14')
 @rfm.parameterized_test(['F90', 'F90'])
+#@rfm.parameterized_test(['F90', 'F90'], ['C', 'c'])
 #@rfm.parameterized_test(['F90', 'F90'], ['C', 'c'], ['C++', 'cc'])
 class Gdb4hpcCpuCheck(Gdb4hpcCheck):
     def __init__(self, lang, extension):
@@ -69,17 +57,13 @@ class Gdb4hpcCpuCheck(Gdb4hpcCheck):
 #        self.valid_systems = ['daint:gpu', 'daint:mc',
 #                              'dom:gpu', 'dom:mc']
 
-#        self.valid_systems.append('kesch:cn')
-#        if self.current_system.name == 'kesch' and self.lang == 'C':
-#            self.build_system.ldflags = ['-lm']
-
         self.sanity_patterns = sn.all([
             sn.assert_reference(sn.extractsingle(
                 r'^tst\{0\}:\s+(?P<result>\d+.\d+[eE]-\d+)',
                 'gdb4hpc.rpt',
                 'result', float), 2.572e-6, -1e-1, 1.0e-1),
 
-            sn.assert_found(r'gdb4hpc 3.0 - Cray Line Mode Parallel Debugger',
+            sn.assert_found(r'gdb4hpc \d\.\d - Cray Line Mode Parallel Debugger',
                             'gdb4hpc.rpt'),
 
             sn.assert_found(r'Shutting down debugger and killing application',
