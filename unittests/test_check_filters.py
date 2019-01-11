2ff924f6b4f28c3ec6ce0294eb34c215028939fc
Vasileios Karakasis <karakasis@cscs.ch>
2019-01-11 14:28:33 +0100

    Minor fixes + style enhancements

--- unittests/test_check_filters.py
+++ unittests/test_check_filters.py
@@ -16,7 +16,6 @@ class TestCheckFilters(unittest.TestCase):
         return ret
 
     def setUp(self):
-        # TODO: add check
         self.checks = [
             self.create_check({
                 'name': 'check1',
@@ -29,61 +28,66 @@ class TestCheckFilters(unittest.TestCase):
                 'tags': {'x', 'y', 'z'},
                 'valid_prog_environs': ['env3'],
                 'valid_systems': ['testsys:mc', 'testsys2:mc'],
-                'num_gpus_per_node': 0})]
+                'num_gpus_per_node': 0}),
+            self.create_check({
+                'name': 'check3',
+                'tags': {'a', 'z'},
+                'valid_prog_environs': ['env3', 'env4'],
+                'valid_systems': ['testsys:gpu'],
+                'num_gpus_per_node': 1})
+        ]
 
     def test_have_name(self):
-        self.assertEqual(1, sn.count(filter(filters.have_name('check1'), 
+        self.assertEqual(1, sn.count(filter(filters.have_name('check1'),
                                             self.checks)))
-        self.assertEqual(1, sn.count(filter(filters.have_name('check2'), 
+        self.assertEqual(1, sn.count(filter(filters.have_name('check2'),
                                             self.checks)))
-        self.assertEqual(0, sn.count(filter(filters.have_name('check3'), 
+        self.assertEqual(1, sn.count(filter(filters.have_name('check3'),
                                             self.checks)))
-        self.assertEqual(0, sn.count(filter(filters.have_name('check4'), 
+        self.assertEqual(0, sn.count(filter(filters.have_name('check4'),
                                             self.checks)))
 
     def test_have_not_name(self):
-        self.assertEqual(1, sn.count(filter(filters.have_not_name('check1'), 
+        self.assertEqual(1, sn.count(filter(filters.have_not_name('check1'),
                                             self.checks)))
-        self.assertEqual(1, sn.count(filter(filters.have_not_name('check2'), 
+        self.assertEqual(1, sn.count(filter(filters.have_not_name('check2'),
                                             self.checks)))
-        self.assertEqual(2, sn.count(filter(filters.have_not_name('check3'), 
+        self.assertEqual(1, sn.count(filter(filters.have_not_name('check3'),
                                             self.checks)))
-        self.assertEqual(2, sn.count(filter(filters.have_not_name('check4'),
+        self.assertEqual(3, sn.count(filter(filters.have_not_name('check4'),
                                             self.checks)))
 
     def test_have_tags(self):
-        self.assertEqual(1, sn.count(filter(filters.have_tag(['a', 'c']), 
+        self.assertEqual(1, sn.count(filter(filters.have_tag(['a', 'c']),
                                             self.checks)))
-        self.assertEqual(0, sn.count(filter(
-            filters.have_tag(['a', 'b', 'c', 'd', 'e']), self.checks)))
-        self.assertEqual(1, sn.count(filter(filters.have_tag(['x', 'y', 'z']), 
+        self.assertEqual(0, sn.count(filter(filters.have_tag(['p', 'q']),
                                             self.checks)))
-        self.assertEqual(0, sn.count(filter(filters.have_tag(['d', 'y']),
+        self.assertEqual(3, sn.count(filter(filters.have_tag(['z']),
                                             self.checks)))
 
     def test_have_prgenv(self):
-        self.assertEqual(1, sn.count(filter(filters.have_prgenv(['env1', 'env2']), 
+        self.assertEqual(1, sn.count(filter(
+            filters.have_prgenv(['env1', 'env2']), self.checks)))
+        self.assertEqual(3, sn.count(filter(filters.have_prgenv(['env3']),
                                             self.checks)))
-        self.assertEqual(1, sn.count(filter(filters.have_prgenv(['env3']), 
+        self.assertEqual(1, sn.count(filter(filters.have_prgenv(['env4']),
                                             self.checks)))
-        self.assertEqual(0, sn.count(filter(filters.have_prgenv(['env4']), 
-                                            self.checks)))
-        self.assertEqual(0, sn.count(filter(filters.have_prgenv(['env1', 'env3']), 
-                                            self.checks)))
+        self.assertEqual(0, sn.count(filter(
+            filters.have_prgenv(['env1', 'env3']), self.checks)))
 
     @rt.switch_runtime(fixtures.TEST_SITE_CONFIG, 'testsys')
     def test_partition(self):
         p = rt.runtime().system.partition('gpu')
-        self.assertEqual(1, sn.count(filter(filters.have_partition([p]), 
+        self.assertEqual(1, sn.count(filter(filters.have_partition([p]),
                                             self.checks)))
         p = rt.runtime().system.partition('login')
-        self.assertEqual(0, sn.count(filter(filters.have_partition([p]), 
+        self.assertEqual(0, sn.count(filter(filters.have_partition([p]),
                                             self.checks)))
 
     def test_have_gpu_only(self):
-        self.assertEqual(1, sn.count(filter(filters.have_gpu_only(), 
+        self.assertEqual(1, sn.count(filter(filters.have_gpu_only(),
                                             self.checks)))
 
     def test_have_cpu_only(self):
-        self.assertEqual(1, sn.count(filter(filters.have_cpu_only(), 
+        self.assertEqual(1, sn.count(filter(filters.have_cpu_only(),
                                             self.checks)))
