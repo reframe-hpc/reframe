d71a0601d0cf49587b56d2f370ce6bcc66781612
Theofilos Manitaras <manitaras@cscs.ch>
2019-01-11 14:58:03 +0100

    Add required ReFrame version

--- cscs-checks/microbenchmarks/kernel_latency/kernel_latency.py
+++ cscs-checks/microbenchmarks/kernel_latency/kernel_latency.py
@@ -2,6 +2,7 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 
+@rfm.required_version('>=2.16-dev0')
 @rfm.parameterized_test(['sync'], ['async'])
 class KernelLatencyTest(rfm.RegressionTest):
     def __init__(self, kernel_version):
