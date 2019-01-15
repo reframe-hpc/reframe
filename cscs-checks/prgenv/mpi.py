ac1f77eed39eaf6fb52aa68be94508c78298f815
jgp <jgp@cscs.ch>
2019-01-15 15:23:52 +0100

    pep8

--- cscs-checks/prgenv/mpi.py
+++ cscs-checks/prgenv/mpi.py
@@ -1,18 +1,28 @@
 import reframe as rfm
 import reframe.utility.sanity as sn
 
+
 @rfm.required_version('>=2.14')
 @rfm.parameterized_test(['single'], ['funneled'], ['serialized'], ['multiple'])
 class Mpi_InitTest(rfm.RegressionTest):
     '''
-    This test checks the value returned by calling MPI_Init_thread:
-    Output should look the same for every prgenv (cray, gnu, intel, pgi)
-    (mpi_thread_multiple seems to be not supported):
-    # 'single':     ['mpi_thread_supported=MPI_THREAD_SINGLE mpi_thread_queried=MPI_THREAD_SINGLE 0'],
-    # 'funneled':   ['mpi_thread_supported=MPI_THREAD_FUNNELED mpi_thread_queried=MPI_THREAD_FUNNELED 1'],
-    # 'serialized': ['mpi_thread_supported=MPI_THREAD_SERIALIZED mpi_thread_queried=MPI_THREAD_SERIALIZED 2'],
-    # 'multiple':   ['mpi_thread_supported=MPI_THREAD_SERIALIZED mpi_thread_queried=MPI_THREAD_SERIALIZED 2']
+This test checks the value returned by calling MPI_Init_thread:
+Output should look the same for every prgenv (cray, gnu, intel, pgi)
+(mpi_thread_multiple seems to be not supported):
+    # 'single':
+    ['mpi_thread_supported=MPI_THREAD_SINGLE
+      mpi_thread_queried=MPI_THREAD_SINGLE 0'],
+    # 'funneled':
+    ['mpi_thread_supported=MPI_THREAD_FUNNELED
+      mpi_thread_queried=MPI_THREAD_FUNNELED 1'],
+    # 'serialized':
+    ['mpi_thread_supported=MPI_THREAD_SERIALIZED
+      mpi_thread_queried=MPI_THREAD_SERIALIZED 2'],
+    # 'multiple':
+    ['mpi_thread_supported=MPI_THREAD_SERIALIZED
+      mpi_thread_queried=MPI_THREAD_SERIALIZED 2']
     '''
+
     def __init__(self, required_thread):
         super().__init__()
         self.valid_systems = ['daint:gpu', 'daint:mc', 'dom:gpu', 'dom:mc']
@@ -26,7 +36,7 @@ class Mpi_InitTest(rfm.RegressionTest):
             'serialized': ['-D_MPI_THREAD_SERIALIZED'],
             'multiple':   ['-D_MPI_THREAD_MULTIPLE']
         }
-        self.build_system.cppflags  = self.cppflags[required_thread]
+        self.build_system.cppflags = self.cppflags[required_thread]
         self.build_system.cppflags += ['-static']
         self.time_limit = (0, 1, 0)
         found_mpithread = sn.extractsingle(
@@ -39,13 +49,11 @@ class Mpi_InitTest(rfm.RegressionTest):
             'serialized': 2,
             'multiple':   2
         }
-
         self.sanity_patterns = sn.all([
             sn.assert_found(r'tid=0 out of 1 from rank 0 out of 1',
-                self.stdout),
+                            self.stdout),
             sn.assert_eq(found_mpithread,
                          self.mpithread_version[required_thread])
         ])
-
         self.maintainers = ['JG']
         self.tags = {'production'}
