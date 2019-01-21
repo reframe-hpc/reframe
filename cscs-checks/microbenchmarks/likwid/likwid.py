c7344f92582995b4d2b374d51c03bc346e207d44
Sebastian Keller <keller@cscs.ch>
2019-01-21 15:45:06 +0100

    use installed likwid rather than compile per test

--- cscs-checks/microbenchmarks/likwid/likwid.py
+++ cscs-checks/microbenchmarks/likwid/likwid.py
@@ -2,22 +2,17 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 
-class BandwidthBase(rfm.RegressionTest):
+class MemBandwidthTest(rfm.RunOnlyRegressionTest):
     def __init__(self):
         super().__init__()
 
+        self.modules = ['likwid']
         self.valid_prog_environs = ['PrgEnv-gnu']
-        self.build_system = 'Make'
-        self.build_system.flags_from_environ = False
-        self.sourcesdir = 'https://github.com/RRZE-HPC/likwid.git'
-        self.variables = {
-            'LD_LIBRARY_PATH': './lib:$LD_LIBRARY_PATH',
-            'PATH': './bin:./sbin:$PATH'
-        }
+        self.sourcesdir = None
 
-        self.executable = 'bin/likwid-bench'
+        self.executable = 'likwid-bench'
 
-        self.num_tasks = 1
+        self.num_tasks = 0
         self.num_tasks_per_core = 2
         self.system_num_cpus = {
             'daint:mc':  72,
@@ -45,7 +40,7 @@ class BandwidthBase(rfm.RegressionTest):
         }
 
         self.maintainers = ['SK']
-        self.tags = {'diagnostic'}
+        self.tags = {'benchmark', 'diagnostic'}
 
         bw_pattern = sn.extractsingle(r'MByte/s:\s*(?P<bw>\S+)',
                                       self.stdout, 'bw',  float)
@@ -55,29 +50,21 @@ class BandwidthBase(rfm.RegressionTest):
             'bandwidth': bw_pattern
         }
 
-    def setup(self, partition, environ, **job_opts):
-        super().setup(partition, environ, **job_opts)
-
-        self.postbuild_cmd = ['make install PREFIX=%s INSTALL_CHOWN='
-                              '\'-g csstaff -o sebkelle\'' % self.stagedir]
-
-
 @rfm.required_version('>=2.16-dev0')
 @rfm.parameterized_test(*[[l,k] for l in ['L1', 'L2', 'L3']
                         for k in ['load_avx', 'store_avx']],
                         ['memory', 'load_avx'],
                         ['memory', 'store_mem_avx'])
-class CPUBandwidth(BandwidthBase):
+class CPUBandwidth(MemBandwidthTest):
     def __init__(self, mem_level, kernel_name):
         super().__init__()
 
         self.descr = 'CPU <- %s %s benchmark' % (mem_level, kernel_name)
-
         self.valid_systems = ['daint:mc', 'daint:gpu', 'dom:gpu', 'dom:mc']
 
         # the kernel to run in likwid
         self.kernel_name = kernel_name
-        self.ml = mem_level
+        self.mem_level = mem_level
 
         self.refs = {
             'mc':  {
@@ -112,28 +99,26 @@ class CPUBandwidth(BandwidthBase):
         }
 
     def setup(self, partition, environ, **job_opts):
-        self.data_size = self.system_cache_sizes[partition.fullname][self.ml]
-
+        self.data_size = self.system_cache_sizes[partition.fullname][self.mem_level]
         self.num_cpus_per_task = self.system_num_cpus[partition.fullname]
         numa_domains = self.system_numa_domains[partition.fullname]
-
-        num_cpu_dom = self.num_cpus_per_task / (len(numa_domains) *
+        num_cpu_domain = self.num_cpus_per_task / (len(numa_domains) *
                                                 self.num_tasks_per_core)
         # result for daint:mc: '-w S0:100MB:18:1:2 -w S1:100MB:18:1:2'
         # format: -w domain:data_size:nthreads:chunk_size:stride
         # chunk_size and stride affect which cpus from <domain> are selected
-        workgroups = ' '.join(['-w %s:%s:%d:1:2' %
-                              (dom, self.data_size, num_cpu_dom)
-                              for dom in numa_domains])
+        workgroups = ['-w %s:%s:%d:1:2' %
+                     (dom, self.data_size, num_cpu_domain)
+                     for dom in numa_domains]
 
-        self.executable_opts = ['-t %s' % self.kernel_name, workgroups]
+        self.executable_opts = ['-t %s' % self.kernel_name] + workgroups
 
         super().setup(partition, environ, **job_opts)
 
 
 @rfm.required_version('>=2.16-dev0')
 @rfm.simple_test
-class CPUBandwidthCrossSocket(BandwidthBase):
+class CPUBandwidthCrossSocket(MemBandwidthTest):
     def __init__(self):
         super().__init__()
 
@@ -141,9 +126,7 @@ class CPUBandwidthCrossSocket(BandwidthBase):
                       "CPU S1 <- main memory S0 read")
 
         self.valid_systems = ['daint:mc', 'dom:mc']
-
         self.kernel_name = 'load_avx'
-
         self.reference = {
             'daint:mc': {
                 'bandwidth': (56000, -0.1, None, 'MB/s')
@@ -158,19 +141,18 @@ class CPUBandwidthCrossSocket(BandwidthBase):
         self.num_cpus_per_task = self.system_num_cpus[partition.fullname]
         numa_domains = self.system_numa_domains[partition.fullname]
 
-        num_cpu_dom = self.num_cpus_per_task / (len(numa_domains) *
+        num_cpu_domain = self.num_cpus_per_task / (len(numa_domains) *
                                                 self.num_tasks_per_core)
 
         # daint:mc: '-w S0:100MB:18:1:2-0:S1 -w S1:100MB:18:1:2-0:S0'
         # format:
         # -w domain:data_size:nthreads:chunk_size:stride-stream_nr:mem_domain
         # chunk_size and stride affect which cpus from <domain> are selected
-        workgroups = ' '.join(['-w %s:100MB:%d:1:2-0:%s' %
-                              (dom_cpu, num_cpu_dom, dom_mem)
-                              for dom_cpu, dom_mem in
-                              zip(numa_domains[:2],
-                                  reversed(numa_domains[:2]))])
+        workgroups = ['-w %s:100MB:%d:1:2-0:%s' %
+                     (dom_cpu, num_cpu_domain, dom_mem)
+                     for dom_cpu, dom_mem in
+                     zip(numa_domains[:2], reversed(numa_domains[:2]))]
 
-        self.executable_opts = ['-t %s' % self.kernel_name, workgroups]
+        self.executable_opts = ['-t %s' % self.kernel_name] + workgroups
 
         super().setup(partition, environ, **job_opts)
