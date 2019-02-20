1d46730571fb11a817adcb99a61fd952e3516f87
bignamic <bignamini@cscs.ch>
2019-02-20 15:28:24 +0100

    Requested changes implemented

--- reframe/frontend/check_filters.py
+++ reframe/frontend/check_filters.py
@@ -8,7 +8,8 @@ def have_name(regexp_names):
     def _fn(c):
         for p in regexp_names:
             if p.search(c.name):
-                return c.name
+                return True
+        return False
 
     return _fn
 
@@ -24,7 +25,7 @@ def have_tag(regexp_tags):
     def _fn(c):
         if regexp_tags:
             for p in regexp_tags:
-                if(any(p.search(tag) for tag in c.tags) is False):
+                if not any(p.search(tag) for tag in c.tags):
                     return False
         return True
 
@@ -35,8 +36,7 @@ def have_prgenv(regexp_prgenv):
     def _fn(c):
         if regexp_prgenv:
             for p in regexp_prgenv:
-                if(any((p.search(prgenv) or prgenv == '*')
-                   for prgenv in c.valid_prog_environs) is False):
+                if not any(p.search(prgenv) or prgenv == '*' for prgenv in c.valid_prog_environs):
                     return False
             return True
         else:
