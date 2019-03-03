f8e3bef787d3041a0aeb1ae263295400b09e7751
ajocksch <andreas.jocksch@cscs.ch>
2019-03-03 23:51:22 +0100

    almost all required changes in

--- cscs-checks/microbenchmarks/mpi/halo_cell_exchange.py
+++ cscs-checks/microbenchmarks/mpi/halo_cell_exchange.py
@@ -9,6 +9,7 @@ class HaloCellExchangeTest(rfm.RegressionTest):
         super().__init__()
         self.sourcepath = 'halo_cell_exchange.c'
         self.build_system = 'SingleSource'
+        self.build_system.cflags = ['-O2']
         self.valid_systems = ['daint:gpu', 'dom:gpu', 'kesch:cn']
         self.valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-pgi',
                                     'PrgEnv-gnu']
@@ -16,79 +17,73 @@ class HaloCellExchangeTest(rfm.RegressionTest):
         self.num_tasks_per_node = 1
         self.num_gpus_per_node = 0
 
-        self.build_system.cflags = ['-O2']
+        self.executable_opts = ['input.txt']
 
-        self.executable_opts = ['< input.txt']
+        self.sanity_patterns = sn.assert_eq(
+            sn.count(sn.findall(r'halo_cell_exchange', self.stdout)), 9)
 
-        self.sanity_patterns = sn.all([
-            sn.assert_eq(
-                sn.count(sn.findall(r'halo_cell_exchange',
-                                    self.stdout)), 9)
-        ])
-
-        # the (?P<dummy>\S+) should be replaced
         self.perf_patterns = {
             'time_2_10': sn.extractsingle(
                 r'halo_cell_exchange 6 2 1 1 10 10 10'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_2_10000': sn.extractsingle(
                 r'halo_cell_exchange 6 2 1 1 10000 10000 10000'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_2_1000000': sn.extractsingle(
                 r'halo_cell_exchange 6 2 1 1 1000000 1000000 1000000'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_4_10': sn.extractsingle(
                 r'halo_cell_exchange 6 2 2 1 10 10 10'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_4_10000': sn.extractsingle(
                 r'halo_cell_exchange 6 2 2 1 10000 10000 10000'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_4_1000000': sn.extractsingle(
                 r'halo_cell_exchange 6 2 2 1 1000000 1000000 1000000'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_6_10': sn.extractsingle(
                 r'halo_cell_exchange 6 3 2 1 10 10 10'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_6_10000': sn.extractsingle(
                 r'halo_cell_exchange 6 3 2 1 10000 10000 10000'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float),
             'time_6_1000000': sn.extractsingle(
                 r'halo_cell_exchange 6 3 2 1 1000000 1000000 1000000'
-                r' (?P<dummy>\S+) (?P<time_mpi>\S+)', self.stdout,
+                r' \S+ (?P<time_mpi>\S+)', self.stdout,
                 'time_mpi', float)
         }
 
         # the kesch values need to be added
         self.sys_reference = {
             'dom:gpu': {
-                'time_2_10': (3.925395e-02, None, 0.50, 's'),
-                'time_2_10000': (9.721279e-02, None, 0.50, 's'),
-                'time_2_1000000': (4.934530e+00, None, 0.50, 's'),
-                'time_4_10': (5.878997e-02, None, 0.50, 's'),
-                'time_4_10000': (1.495080e-01, None, 0.50, 's'),
-                'time_4_1000000': (6.791397e+00, None, 0.50, 's'),
-                'time_6_10': (5.428815e-02, None, 0.50, 's'),
-                'time_6_10000': (1.540580e-01, None, 0.50, 's'),
-                'time_6_1000000': (9.179296e+00, None, 0.50, 's')
+                'time_2_10': (3.925395e-06, None, 0.50, 's'),
+                'time_2_10000': (9.721279e-06, None, 0.50, 's'),
+                'time_2_1000000': (4.934530e-04, None, 0.50, 's'),
+                'time_4_10': (5.878997e-06, None, 0.50, 's'),
+                'time_4_10000': (1.495080e-05, None, 0.50, 's'),
+                'time_4_1000000': (6.791397e-04, None, 0.50, 's'),
+                'time_6_10': (5.428815e-06, None, 0.50, 's'),
+                'time_6_10000': (1.540580e-05, None, 0.50, 's'),
+                'time_6_1000000': (9.179296e-04, None, 0.50, 's')
             },
             'daint:gpu': {
-                'time_2_10': (3.925395e-02, None, 0.50, 's'),
-                'time_2_10000': (9.721279e-02, None, 0.50, 's'),
-                'time_2_1000000': (4.934530e+00, None, 0.50, 's'),
-                'time_4_10': (5.878997e-02, None, 0.50, 's'),
-                'time_4_10000': (1.495080e-01, None, 0.50, 's'),
-                'time_4_1000000': (6.791397e+00, None, 0.50, 's'),
-                'time_6_10': (5.428815e-02, None, 0.50, 's'),
-                'time_6_10000': (1.540580e-01, None, 0.50, 's'),
-                'time_6_1000000': (9.179296e+00, None, 0.50, 's')
+                'time_2_10': (3.925395e-06, None, 0.50, 's'),
+                'time_2_10000': (9.721279e-06, None, 0.50, 's'),
+                'time_2_1000000': (4.934530e-04, None, 0.50, 's'),
+                'time_4_10': (5.878997e-06, None, 0.50, 's'),
+                'time_4_10000': (1.495080e-05, None, 0.50, 's'),
+                'time_4_1000000': (6.791397e-04, None, 0.50, 's'),
+                'time_6_10': (5.428815e-06, None, 0.50, 's'),
+                'time_6_10000': (1.540580e-05, None, 0.50, 's'),
+                'time_6_1000000': (9.179296e-04, None, 0.50, 's')
             },
             'kesch:cn': {
             }
@@ -97,4 +92,4 @@ class HaloCellExchangeTest(rfm.RegressionTest):
         self.reference = self.sys_reference
 
         self.maintainers = ['AJ']
-        self.tags = {'benchmark', 'diagnostic'}
+        self.tags = {'benchmark'}
--- cscs-checks/microbenchmarks/mpi/src/halo_cell_exchange.c
+++ cscs-checks/microbenchmarks/mpi/src/halo_cell_exchange.c
@@ -3,154 +3,318 @@
 #include <string.h>
 #include <mpi.h>
 
-int main(int argc, const char* argv[]){
-    MPI_Comm cart_comm, red_comm;
-    MPI_Request *request;
-    MPI_Status *status;
-    int ndims, reorder, color, end, ierr;
-    int *dim_size, *periods, *halosize;
-    int comm_size, comm_rank, comm_size_cart, comm_rank_cart;
-    char *sendbuf, *recvbuf, inputbuf[1000], *pinputbuf;
-    double start, stop, deltatmin, deltatmax, ttt;
-    int rank_source, rank_dest, i, j;
+#define NCALLS 10000
 
-    if (argc>1){
-        printf("%s < ndims dim1 dim2 ... halosize1 halosize2 ...\n", argv[0]);
-        exit(0);
+int
+main (int argc, const char *argv[])
+{
+  MPI_Comm cart_comm, red_comm;
+  MPI_Request *request;
+  MPI_Status *status;
+  FILE *pFile;
+  int ndims, reorder, color, end;
+  int *dim_size, *periods, *halosize;
+  int comm_size, comm_rank, comm_size_cart, comm_rank_cart;
+  char *sendbuf, *recvbuf, inputbuf[1000], *pinputbuf;
+  double start, stop, deltatmin, deltatmax, elapsed_time;
+  int rank_source, rank_dest, i, j;
+
+  if (MPI_Init (NULL, NULL) != 0)
+    {
+      fprintf (stderr, "MPI_Init() failed\n");
+      exit (1);
     }
-    ierr = MPI_Init(NULL, NULL);
-    if (ierr!=0) exit(1);
-    ierr = MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
-    if (ierr!=0) exit(1);
-    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank);
-    if (ierr!=0) exit(1);
-    end = 0;
-    while (end == 0){
-        if (comm_rank==0){
-            end = (fgets(inputbuf, sizeof(inputbuf), stdin) == NULL);
-            if (end == 0){
-                pinputbuf = inputbuf;
-                while (pinputbuf[0]==' ') pinputbuf++;
-                end = !((pinputbuf[0]>='0')&&(pinputbuf[0]<='9'));
-                pinputbuf = inputbuf;
-            }
-        }
-        MPI_Bcast(&end, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        if (end == 0){
-            if (comm_rank==0){
-                sscanf(pinputbuf, "%d", &ndims);
-            }
-            MPI_Bcast(&ndims, 1, MPI_INT, 0, MPI_COMM_WORLD);
-            dim_size = (int*) malloc(ndims*sizeof(int));
-            periods = (int*) malloc(ndims*sizeof(int));
-            halosize = (int*) malloc(ndims*sizeof(int));
-
-            for (i=0; i<ndims; i++){
-                periods[i] = 1;
-            }
-            reorder = 1;
-
-            j = 1;
-            for (i=0; i<ndims; i++){
-                if (comm_rank==0){
-                    while (pinputbuf[0]==' ') pinputbuf++;
-                    while (pinputbuf[0]!=' ') pinputbuf++;
-                    while (pinputbuf[0]==' ') pinputbuf++;
-                    sscanf(pinputbuf, "%d", &dim_size[i]);
-                }
-                MPI_Bcast(&dim_size[i], 1, MPI_INT, 0, MPI_COMM_WORLD);
-                j *= dim_size[i];
-            }
-            for (i=0; i<ndims; i++){
-                if (comm_rank==0){
-                    while (pinputbuf[0]==' ') pinputbuf++;
-                    while (pinputbuf[0]!=' ') pinputbuf++;
-                    while (pinputbuf[0]==' ') pinputbuf++;
-                    sscanf(pinputbuf, "%d", &halosize[i]);
-                }
-                MPI_Bcast(&halosize[i], 1, MPI_INT, 0, MPI_COMM_WORLD);
-            }
-            if (j>comm_size){
-                if (comm_rank==0){
-                    printf("Please run with at least %d MPI ranks.\n", j);
-                }
-                ierr = MPI_Finalize();
-                if (ierr!=0) exit(1);
-                exit(0);
-            }
-
-            color = (comm_rank < j);
-            if (color == 0){
-                ierr = MPI_Comm_split(MPI_COMM_WORLD, MPI_UNDEFINED, comm_rank, &red_comm);
-            }else{
-                ierr = MPI_Comm_split(MPI_COMM_WORLD, color, comm_rank, &red_comm);
-                ierr = MPI_Cart_create(red_comm, ndims, dim_size, periods, reorder, &cart_comm);
-                if (ierr!=0) exit(1);
-                ierr = MPI_Comm_size(cart_comm, &comm_size_cart);
-                if (ierr!=0) exit(1);
-                ierr = MPI_Comm_rank(cart_comm, &comm_rank_cart);
-                if (ierr!=0) exit(1);
-
-                j=0;
-                for (i=0; i<ndims; i++){
-                    if (halosize[i]>j){
-                        j=halosize[i];
-                    }
-                }
-                sendbuf = (char*) malloc(ndims*2*j*sizeof(char));
-                recvbuf = (char*) malloc(ndims*2*j*sizeof(char));
-                request = (MPI_Request*) malloc(ndims*2*2*sizeof(MPI_Request));
-                status = (MPI_Status*) malloc(ndims*2*2*sizeof(MPI_Status));
-
-                start = MPI_Wtime ();
-
-                for (j=0; j<10000; j++){
-                    for (i=0; i<ndims; i++){
-                        ierr = MPI_Cart_shift(cart_comm, i, 1, &rank_source, &rank_dest);
-                        if (ierr!=0) exit(1);
-                        ierr = MPI_Irecv(recvbuf+i*2*halosize[i]*sizeof(char), halosize[i], MPI_CHAR, rank_source, 1, cart_comm, request+i*2);
-                        if (ierr!=0) exit(1);
-                        ierr = MPI_Irecv(recvbuf+(i*2+1)*halosize[i]*sizeof(char), halosize[i], MPI_CHAR, rank_dest, 1, cart_comm, request+i*2+1);
-                        if (ierr!=0) exit(1);
-                    }
-                    for (i=0; i<ndims; i++){
-                        ierr = MPI_Cart_shift(cart_comm, i, 1, &rank_source, &rank_dest);
-                        if (ierr!=0) exit(1);
-                        ierr = MPI_Isend(sendbuf+i*2*halosize[i]*sizeof(char), halosize[i], MPI_CHAR, rank_source, 1, cart_comm, request+i*2+ndims*2);
-                        if (ierr!=0) exit(1);
-                        ierr = MPI_Isend(sendbuf+(i*2+1)*halosize[i]*sizeof(char), halosize[i], MPI_CHAR, rank_dest, 1, cart_comm, request+i*2+1+ndims*2);
-                        if (ierr!=0) exit(1);
-                    }
-                    ierr = MPI_Waitall(ndims*2*2, request, status);
-                    if (ierr!=0) exit(1);
-                }
-
-                stop = MPI_Wtime ();
-                ttt = stop - start;
-                ierr = MPI_Reduce (&ttt, &deltatmin, 1, MPI_DOUBLE, MPI_MIN, 0, cart_comm);
-                if (ierr!=0) exit(1);
-                ierr = MPI_Reduce (&ttt, &deltatmax, 1, MPI_DOUBLE, MPI_MAX, 0, cart_comm);
-                if (ierr!=0) exit(1);
-                if (comm_rank_cart == 0){
-                    printf ("halo_cell_exchange %d", comm_size);
-                    for (i=0; i<ndims; i++){
-                        printf (" %d", dim_size[i]);
-                    }
-                    for (i=0; i<ndims; i++){
-                        printf (" %d", halosize[i]);
-                    }
-                    printf (" %e %e\n", deltatmin, deltatmax);
-                }
-                free(status);
-                free(request);
-                free(recvbuf);
-                free(sendbuf);
-                ierr = MPI_Comm_free(&cart_comm);
-                if (ierr!=0) exit(1);
-                ierr = MPI_Comm_free(&red_comm);
-                if (ierr!=0) exit(1);
-            }
-        }
+  if (MPI_Comm_size (MPI_COMM_WORLD, &comm_size) != 0)
+    {
+      fprintf (stderr, "MPI_Comm_size() failed\n");
+      exit (1);
     }
-    ierr = MPI_Finalize();
+  if (MPI_Comm_rank (MPI_COMM_WORLD, &comm_rank) != 0)
+    {
+      fprintf (stderr, "MPI_Comm_rank() failed\n");
+      exit (1);
+    }
+  if (argc == 1)
+    {
+      if (comm_rank == 0)
+	{
+	  printf ("%s inputfile\n", argv[0]);
+	  printf ("ndims dim1 dim2 ... halosize1 halosize2 ...\n");
+	}
+      exit (0);
+    }
+  if (comm_rank == 0)
+    {
+      if (strcmp (argv[1], "-") == 0)
+	{
+	  pFile = stdin;
+	}
+      else
+	{
+	  pFile = fopen (argv[1], "r");
+	}
+    }
+  end = 0;
+  while (end == 0)
+    {
+      if (comm_rank == 0)
+	{
+	  end = (fgets (inputbuf, sizeof (inputbuf) - 1, pFile) == NULL);
+	  if (end == 0)
+	    {
+	      pinputbuf = inputbuf;
+	      while (*pinputbuf == ' ')
+		pinputbuf++;
+	      end = !((*pinputbuf >= '0') && (*pinputbuf <= '9'));
+	      pinputbuf = inputbuf;
+	    }
+	}
+      if (MPI_Bcast (&end, 1, MPI_INT, 0, MPI_COMM_WORLD) != 0)
+	{
+	  fprintf (stderr, "MPI_Bcast() failed\n");
+	  exit (1);
+	}
+      if (end == 0)
+	{
+	  if (comm_rank == 0)
+	    {
+	      sscanf (pinputbuf, "%d", &ndims);
+	    }
+	  if (MPI_Bcast (&ndims, 1, MPI_INT, 0, MPI_COMM_WORLD) != 0)
+	    {
+	      fprintf (stderr, "MPI_Bcast() failed\n");
+	      exit (1);
+	    }
+	  dim_size = (int *) malloc (ndims * sizeof (*dim_size));
+	  periods = (int *) malloc (ndims * sizeof (*periods));
+	  halosize = (int *) malloc (ndims * sizeof (*halosize));
+
+	  for (i = 0; i < ndims; i++)
+	    {
+	      periods[i] = 1;
+	    }
+	  reorder = 1;
+
+	  j = 1;
+	  for (i = 0; i < ndims; i++)
+	    {
+	      if (comm_rank == 0)
+		{
+		  while (*pinputbuf == ' ')
+		    pinputbuf++;
+		  while (*pinputbuf != ' ')
+		    pinputbuf++;
+		  while (*pinputbuf == ' ')
+		    pinputbuf++;
+		  sscanf (pinputbuf, "%d", &dim_size[i]);
+		}
+	      if (MPI_Bcast (&dim_size[i], 1, MPI_INT, 0, MPI_COMM_WORLD) !=
+		  0)
+		{
+		  fprintf (stderr, "MPI_Bcast() failed\n");
+		  exit (1);
+		}
+	      j *= dim_size[i];
+	    }
+	  for (i = 0; i < ndims; i++)
+	    {
+	      if (comm_rank == 0)
+		{
+		  while (*pinputbuf == ' ')
+		    pinputbuf++;
+		  while (*pinputbuf != ' ')
+		    pinputbuf++;
+		  while (*pinputbuf == ' ')
+		    pinputbuf++;
+		  sscanf (pinputbuf, "%d", &halosize[i]);
+		}
+	      if (MPI_Bcast (&halosize[i], 1, MPI_INT, 0, MPI_COMM_WORLD) !=
+		  0)
+		{
+		  fprintf (stderr, "MPI_Bcast() failed\n");
+		  exit (1);
+		}
+	    }
+	  if (j > comm_size)
+	    {
+	      if (comm_rank == 0)
+		{
+		  printf ("Please run with at least %d MPI ranks.\n", j);
+		}
+	      if (MPI_Finalize () != 0)
+		{
+		  fprintf (stderr, "MPI_Finalize() failed\n");
+		  exit (1);
+		}
+	      exit (0);
+	    }
+
+	  color = (comm_rank < j);
+	  if (color == 0)
+	    {
+	      if (MPI_Comm_split
+		  (MPI_COMM_WORLD, MPI_UNDEFINED, comm_rank, &red_comm) != 0)
+		{
+		  fprintf (stderr, "MPI_Comm_split() failed\n");
+		  exit (1);
+		}
+	    }
+	  else
+	    {
+	      if (MPI_Comm_split (MPI_COMM_WORLD, color, comm_rank, &red_comm)
+		  != 0)
+		{
+		  fprintf (stderr, "MPI_Comm_split() failed\n");
+		  exit (1);
+		}
+	      if (MPI_Cart_create
+		  (red_comm, ndims, dim_size, periods, reorder,
+		   &cart_comm) != 0)
+		{
+		  fprintf (stderr, "MPI_Comm_create() failed\n");
+		  exit (1);
+		}
+	      if (MPI_Comm_size (cart_comm, &comm_size_cart) != 0)
+		{
+		  fprintf (stderr, "MPI_Comm_size() failed\n");
+		  exit (1);
+		}
+	      if (MPI_Comm_rank (cart_comm, &comm_rank_cart) != 0)
+		{
+		  fprintf (stderr, "MPI_Comm_rank() failed\n");
+		  exit (1);
+		}
+
+	      j = 0;
+	      for (i = 0; i < ndims; i++)
+		{
+		  if (halosize[i] > j)
+		    {
+		      j = halosize[i];
+		    }
+		}
+	      sendbuf = (char *) malloc (ndims * 2 * j * sizeof (char));
+	      recvbuf = (char *) malloc (ndims * 2 * j * sizeof (char));
+	      request =
+		(MPI_Request *) malloc (ndims * 2 * 2 * sizeof (MPI_Request));
+	      status =
+		(MPI_Status *) malloc (ndims * 2 * 2 * sizeof (MPI_Status));
+
+	      start = MPI_Wtime ();
+
+	      for (j = 0; j < NCALLS; j++)
+		{
+		  for (i = 0; i < ndims; i++)
+		    {
+		      if (MPI_Cart_shift
+			  (cart_comm, i, 1, &rank_source, &rank_dest) != 0)
+			{
+			  fprintf (stderr, "MPI_Cart_shift() failed\n");
+			  exit (1);
+			}
+		      if (MPI_Irecv
+			  (recvbuf + i * 2 * halosize[i] * sizeof (char),
+			   halosize[i], MPI_CHAR, rank_source, 1, cart_comm,
+			   request + i * 2) != 0)
+			{
+			  fprintf (stderr, "MPI_Irecv() failed\n");
+			  exit (1);
+			}
+		      if (MPI_Irecv
+			  (recvbuf +
+			   (i * 2 + 1) * halosize[i] * sizeof (char),
+			   halosize[i], MPI_CHAR, rank_dest, 1, cart_comm,
+			   request + i * 2 + 1) != 0)
+			{
+			  fprintf (stderr, "MPI_Irecv() failed\n");
+			  exit (1);
+			}
+		    }
+		  for (i = 0; i < ndims; i++)
+		    {
+		      if (MPI_Cart_shift
+			  (cart_comm, i, 1, &rank_source, &rank_dest) != 0)
+			{
+			  fprintf (stderr, "MPI_Cart_shift() failed\n");
+			  exit (1);
+			}
+		      if (MPI_Isend
+			  (sendbuf + i * 2 * halosize[i] * sizeof (char),
+			   halosize[i], MPI_CHAR, rank_source, 1, cart_comm,
+			   request + i * 2 + ndims * 2) != 0)
+			{
+			  fprintf (stderr, "MPI_Irecv() failed\n");
+			  exit (1);
+			}
+		      if (MPI_Isend
+			  (sendbuf +
+			   (i * 2 + 1) * halosize[i] * sizeof (char),
+			   halosize[i], MPI_CHAR, rank_dest, 1, cart_comm,
+			   request + i * 2 + 1 + ndims * 2) != 0)
+			{
+			  fprintf (stderr, "MPI_Irecv() failed\n");
+			  exit (1);
+			}
+		    }
+		  if (MPI_Waitall (ndims * 2 * 2, request, status) != 0)
+		    {
+		      fprintf (stderr, "MPI_Waitall() failed\n");
+		      exit (1);
+		    }
+		}
+
+	      stop = MPI_Wtime ();
+	      elapsed_time = stop - start;
+	      if (MPI_Reduce
+		  (&elapsed_time, &deltatmin, 1, MPI_DOUBLE, MPI_MIN, 0,
+		   cart_comm) != 0)
+		{
+		  fprintf (stderr, "MPI_Reduce() failed\n");
+		  exit (1);
+		}
+	      if (MPI_Reduce
+		  (&elapsed_time, &deltatmax, 1, MPI_DOUBLE, MPI_MAX, 0,
+		   cart_comm) != 0)
+		{
+		  fprintf (stderr, "MPI_Reduce() failed\n");
+		  exit (1);
+		}
+	      if (comm_rank_cart == 0)
+		{
+		  printf ("halo_cell_exchange %d", comm_size);
+		  for (i = 0; i < ndims; i++)
+		    {
+		      printf (" %d", dim_size[i]);
+		    }
+		  for (i = 0; i < ndims; i++)
+		    {
+		      printf (" %d", halosize[i]);
+		    }
+		  printf (" %e %e\n", deltatmin / NCALLS, deltatmax / NCALLS);
+		}
+	      free (status);
+	      free (request);
+	      free (recvbuf);
+	      free (sendbuf);
+	      if (MPI_Comm_free (&cart_comm) != 0)
+		{
+		  fprintf (stderr, "MPI_Comm_free() failed\n");
+		  exit (1);
+		}
+	      if (MPI_Comm_free (&red_comm) != 0)
+		{
+		  fprintf (stderr, "MPI_Comm_free() failed\n");
+		  exit (1);
+		}
+	    }
+	  free (halosize);
+	  free (periods);
+	  free (dim_size);
+	}
+    }
+  if (MPI_Finalize () != 0)
+    {
+      fprintf (stderr, "MPI_Finalize() failed\n");
+      exit (1);
+    }
 }
