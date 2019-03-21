a492fed67641225586e6f023be3ef581242c8133
jgp <jgp@cscs.ch>
2019-03-21 15:36:12 +0100

    loosening perf limits

--- cscs-checks/tools/profiling_and_debugging/gperftools_mpi_omp.py
+++ cscs-checks/tools/profiling_and_debugging/gperftools_mpi_omp.py
@@ -85,8 +85,8 @@ class GperftoolsMpiCheck(rfm.RegressionTest):
                 self.rpt_file_txt, 'flatPercentage', float)
         }
         self.reference = {
-            'daint:mc':  {'hotspot1': (26.0, -0.5, 0.5, '%')},
-            'daint:gpu': {'hotspot1': (46.0, -0.5, 0.5, '%')},
+            'daint:mc':  {'hotspot1': (26.0, -0.6, 0.6, '%')},
+            'daint:gpu': {'hotspot1': (46.0, -0.6, 0.6, '%')},
             'dom:mc':    {'hotspot1': (26.0, -0.5, 0.5, '%')},
             'dom:gpu':   {'hotspot1': (46.0, -0.5, 0.5, '%')},
         }
