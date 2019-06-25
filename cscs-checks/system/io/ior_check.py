34dc09b7c2288aab9e3c087e5d9ba06ed0dcc5f0
Giuseppe Lo Re <loreg@cscs.ch>
2019-06-25 14:21:10 -0700

    Update on ior_check
    few comment/style fixes

--- cscs-checks/system/io/ior_check.py
+++ cscs-checks/system/io/ior_check.py
@@ -4,74 +4,6 @@ import getpass
 import reframe as rfm
 import reframe.utility.sanity as sn
 
-fs = {
-    '/scratch/snx1600tds': {
-        'valid_systems': ['dom:gpu'],
-        'dom': {
-            'num_tasks': 2,
-            # 1 task per node to avoid cache effects on read (other options
-            # like -C did produce the desired impact) 8 tasks are enough
-            # to get ~peak perf (write 5.4 GB/s, read 4.3 GB/s)
-        }
-    },
-    '/scratch/snx1600': {
-        'valid_systems': ['daint:gpu'],
-        'daint': {
-            'num_tasks': 1,  # to validate
-        }
-    },
-    '/scratch/snx3000tds': {
-        'valid_systems': ['dom:gpu'],
-        'dom': {
-            'num_tasks': 2,  # to validate
-        }
-    },
-    '/scratch/snx3000': {
-        'valid_systems': ['daint:gpu'],
-        'daint': {
-            'num_tasks': 1,  # to validate
-        }
-    },
-    '/users': {
-        'valid_systems': ['daint:gpu', 'dom:gpu', 'fulen:normal'],
-        'ior_block_size': '8g',
-        'daint': {
-            'num_tasks': 1,  # to validate
-        },
-        'dom': {
-            'num_tasks': 1,  # to validate
-        },
-        'fulen': {
-            'num_tasks': 1,
-            'build_system_cc': 'mpicc',
-            'build_system_cxx': 'mpic++',
-            'valid_prog_environs': ['PrgEnv-gnu']
-        }
-    },
-    '/scratch/shared/fulen': {
-        'valid_systems': ['fulen:normal'],
-        'ior_block_size': '48g',
-        'fulen': {
-            'num_tasks': 8,
-            'build_system_cc': 'mpicc',
-            'build_system_cxx': 'mpic++',
-            'valid_prog_environs': ['PrgEnv-gnu']
-        }
-    }
-}
-
-# Setting some default values
-for data in fs.values():
-    if 'ior_block_size' not in data:
-        data['ior_block_size'] = '24g'
-    if 'ior_access_type' not in data:
-        data['ior_access_type'] = 'MPIIO'
-    if 'reference' not in data:
-        data['reference'] = {
-            'read_bw': (1, -0.5, None),
-            'write_bw': (1, -0.5, None)
-        }
-
 
 class IorCheck(rfm.RegressionTest):
     def __init__(self, base_dir):
@@ -89,22 +21,88 @@ class IorCheck(rfm.RegressionTest):
         except OSError:
             pass
 
-        self.valid_systems = fs[base_dir]['valid_systems']
+        self.fs = {
+            '/scratch/snx1600tds': {
+                'valid_systems': ['dom:gpu'],
+                'dom': {
+                    'num_tasks': 2,
+                    # We will be using 1 task per node to avoid cache 
+                    # effects on read. The option "-C" could be used 
+                    # with many tasks per node. 8 tasks are enough
+                    # to get ~peak perf (write 5.4 GB/s, read 4.3 GB/s)
+                    }
+                },
+            '/scratch/snx1600': {
+                'valid_systems': ['daint:gpu'],
+                'daint': {
+                    'num_tasks': 1,  # to validate
+                    }
+                },
+            '/scratch/snx3000tds': {
+                'valid_systems': ['dom:gpu'],
+                'dom': {
+                    'num_tasks': 2,  # to validate
+                    }
+                },
+            '/scratch/snx3000': {
+                'valid_systems': ['daint:gpu'],
+                'daint': {
+                    'num_tasks': 1,  # to validate
+                    }
+                },
+            '/users': {
+                'valid_systems': ['daint:gpu', 'dom:gpu', 'fulen:normal'],
+                'ior_block_size': '8g',
+                'daint': {
+                    'num_tasks': 1,  # to validate
+                    },
+                'dom': {
+                    'num_tasks': 1,  # to validate
+                    },
+                'fulen': {
+                    'num_tasks': 1,
+                    'build_system_cc': 'mpicc',
+                    'build_system_cxx': 'mpic++',
+                    'valid_prog_environs': ['PrgEnv-gnu']
+                    }
+                },
+            '/scratch/shared/fulen': {
+                'valid_systems': ['fulen:normal'],
+                'ior_block_size': '48g',
+                'fulen': {
+                    'num_tasks': 8,
+                    'build_system_cc': 'mpicc',
+                    'build_system_cxx': 'mpic++',
+                    'valid_prog_environs': ['PrgEnv-gnu']
+                    }
+                }
+            }
+
+        # Setting some default values
+        for data in self.fs.values():
+            data.setdefault('ior_block_size', '24g')
+            data.setdefault('ior_access_type', 'MPIIO')
+            data.setdefault('reference', 
+                            {'read_bw': (1, -0.5, None),
+                             'write_bw': (1, -0.5, None)
+                             })
+            
+        self.valid_systems = self.fs[base_dir]['valid_systems']
 
         cur_sys = self.current_system.name
 
         try:
-            self.num_tasks = fs[base_dir][cur_sys]['num_tasks']
+            self.num_tasks = self.fs[base_dir][cur_sys]['num_tasks']
         except KeyError:
             self.num_tasks = 1
         try:
-            tasks_per_node = fs[base_dir][cur_sys]['num_tasks_per_node']
+            tasks_per_node = self.fs[base_dir][cur_sys]['num_tasks_per_node']
             self.num_tasks_per_node = tasks_per_node
         except KeyError:
             self.num_tasks_per_node = 1
 
-        self.ior_block_size = fs[base_dir]['ior_block_size']
-        self.ior_access_type = fs[base_dir]['ior_access_type']
+        self.ior_block_size = self.fs[base_dir]['ior_block_size']
+        self.ior_access_type = self.fs[base_dir]['ior_access_type']
         self.executable_opts = ['-B', '-F', '-C ', '-Q 1', '-t 4m', '-D 30',
                                 '-b', self.ior_block_size,
                                 '-a', self.ior_access_type,
@@ -114,17 +112,17 @@ class IorCheck(rfm.RegressionTest):
         self.build_system = 'Make'
 
         try:
-            prog_env = fs[base_dir][cur_sys]['valid_prog_environs']
+            prog_env = self.fs[base_dir][cur_sys]['valid_prog_environs']
             self.valid_prog_environs = prog_env
         except KeyError:
             self.valid_prog_environs = ['PrgEnv-cray']
 
         try:
-            self.build_system.cc = fs[base_dir][cur_sys]['build_system_cc']
+            self.build_system.cc = self.fs[base_dir][cur_sys]['build_system_cc']
         except KeyError:
             pass
         try:
-            self.build_system.cxx = fs[base_dir][cur_sys]['build_system_cxx']
+            self.build_system.cxx = self.fs[base_dir][cur_sys]['build_system_cxx']
         except KeyError:
             pass
 
@@ -139,7 +137,7 @@ class IorCheck(rfm.RegressionTest):
         # Our references are based on fs types but regression needs reference
         # per system.
         self.reference = {
-            '*': fs[base_dir]['reference']
+            '*': self.fs[base_dir]['reference']
         }
 
         self.maintainers = ['SO', 'GLR']
