f6fc2b5257911c3ab04cc38cdafefeb6b7d3a7ae
Vasileios Karakasis <karakasis@cscs.ch>
2019-07-03 23:31:40 +0200

    Run unit tests for Daint in the CI partition

---
+++ config/cscs-ci.py
@@ -0,0 +1,209 @@
+#
+# CSCS ReFrame CI settings
+#
+
+
+class ReframeSettings:
+    reframe_module = 'reframe'
+    job_poll_intervals = [1, 2, 3]
+    job_submit_timeout = 60
+    checks_path = ['checks/']
+    checks_path_recurse = True
+    site_configuration = {
+        'systems': {
+            'daint': {
+                'descr': 'Piz Daint CI nodes',
+                'hostnames': ['daint'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/UES/reframe/resources',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'modules': [],
+                        'access':  [],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Login nodes',
+                        'max_jobs': 4
+                    },
+                    'gpu': {
+                        'scheduler': 'nativeslurm',
+                        'modules': ['daint-gpu'],
+                        'access':  ['--constraint=gpu', '--partition=cscsci'],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Hybrid nodes (Haswell/P100)',
+                        'max_jobs': 100,
+                        'resources': {
+                            'switches': ['--switches={num_switches}']
+                        }
+                    },
+                }
+            },
+            'dom': {
+                'descr': 'Dom TDS',
+                'hostnames': ['dom'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/UES/reframe/resources',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'modules': [],
+                        'access':  [],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel', 'PrgEnv-pgi'],
+                        'descr': 'Login nodes',
+                        'max_jobs': 4
+                    },
+                    'gpu': {
+                        'scheduler': 'nativeslurm',
+                        'modules': ['daint-gpu'],
+                        'access':  ['--constraint=gpu'],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-gnu',
+                                     'PrgEnv-intel'],
+                        'descr': 'Hybrid nodes (Haswell/P100)',
+                        'max_jobs': 100,
+                        'resources': {
+                            'switches': ['--switches={num_switches}']
+                        }
+                    },
+                }
+            },
+            'kesch': {
+                'descr': 'Kesch MCH',
+                'hostnames': ['keschln-\d+'],
+                'modules_system': 'tmod',
+                'resourcesdir': '/apps/common/UES/reframe/resources',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'environs': ['PrgEnv-cray', 'PrgEnv-pgi',
+                                     'PrgEnv-gnu'],
+                        'descr': 'Kesch login nodes',
+                    },
+                    'cn': {
+                        'scheduler': 'nativeslurm',
+                        'access': ['--partition=cn-regression'],
+                        'environs': ['PrgEnv-cray', 'PrgEnv-pgi',
+                                     'PrgEnv-gnu'],
+                        'descr': 'Kesch compute nodes',
+                        'resources': {
+                            '_rfm_gpu': ['--gres=gpu:{num_gpus_per_node}'],
+                        }
+                    }
+                }
+            },
+            'generic': {
+                'descr': 'Generic example system',
+                'partitions': {
+                    'login': {
+                        'scheduler': 'local',
+                        'modules': [],
+                        'access': [],
+                        'environs': ['builtin-gcc'],
+                        'descr': 'Login nodes'
+                    }
+                }
+            }
+        },
+
+        'environments': {
+            'kesch': {
+                'PrgEnv-gnu': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-gnu'],
+                    'cc': 'gcc',
+                    'cxx': 'g++',
+                    'ftn': 'gfortran',
+                },
+            },
+            '*': {
+                'PrgEnv-cray': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-cray'],
+                },
+
+                'PrgEnv-gnu': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-gnu'],
+                },
+
+                'PrgEnv-intel': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-intel'],
+                },
+
+                'PrgEnv-pgi': {
+                    'type': 'ProgEnvironment',
+                    'modules': ['PrgEnv-pgi'],
+                },
+
+                'builtin': {
+                    'type': 'ProgEnvironment',
+                    'cc':  'cc',
+                    'cxx': '',
+                    'ftn': '',
+                },
+
+                'builtin-gcc': {
+                    'type': 'ProgEnvironment',
+                    'cc':  'gcc',
+                    'cxx': 'g++',
+                    'ftn': 'gfortran',
+                }
+            }
+        }
+    }
+
+    logging_config = {
+        'level': 'DEBUG',
+        'handlers': [
+            {
+                'type': 'file',
+                'name': 'reframe.log',
+                'level': 'DEBUG',
+                'format': '[%(asctime)s] %(levelname)s: '
+                          '%(check_info)s: %(message)s',
+                'append': False,
+            },
+
+            # Output handling
+            {
+                'type': 'stream',
+                'name': 'stdout',
+                'level': 'INFO',
+                'format': '%(message)s'
+            },
+            {
+                'type': 'file',
+                'name': 'reframe.out',
+                'level': 'INFO',
+                'format': '%(message)s',
+                'append': False,
+            }
+        ]
+    }
+
+    perf_logging_config = {
+        'level': 'DEBUG',
+        'handlers': [
+            {
+                'type': 'filelog',
+                'prefix': '%(check_system)s/%(check_partition)s',
+                'level': 'INFO',
+                'format': (
+                    '%(asctime)s|reframe %(version)s|'
+                    '%(check_info)s|jobid=%(check_jobid)s|'
+                    '%(check_perf_var)s=%(check_perf_value)s|'
+                    'ref=%(check_perf_ref)s '
+                    '(l=%(check_perf_lower_thres)s, '
+                    'u=%(check_perf_upper_thres)s)|'
+                    '%(check_perf_unit)s'
+                ),
+                'append': True
+            }
+        ]
+    }
+
+
+settings = ReframeSettings()
