86b2e23829a2e603d8946d3b4f8f03e503f7eb4a
bignamic <bignamini@cscs.ch>
2019-07-10 15:14:45 +0200

    unit test fix

--- unittests/test_pipeline.py
+++ unittests/test_pipeline.py
@@ -573,7 +573,7 @@ class TestSanityPatterns(unittest.TestCase):
                 'value3': (3.1, -0.1, 0.1),
             }
         }
-        self.assertRaises(SanityError, self.test.check_performance)
+        self.test.check_performance()
 
     def test_default_reference(self):
         self.write_performance_output(performance1=1.3,
