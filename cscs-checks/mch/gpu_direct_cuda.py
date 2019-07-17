a9bb2bbd05815ecb066e154325e36fe3b948108f
Luca <lucamar@cscs.ch>
2019-07-17 11:56:03 +0200

    Updating tests

--- cscs-checks/mch/gpu_direct_cuda.py
+++ cscs-checks/mch/gpu_direct_cuda.py
@@ -8,9 +8,7 @@ class GpuDirectCudaCheck(rfm.RegressionTest):
     def __init__(self):
         super().__init__()
         self.descr = 'tests gpu-direct for CUDA'
-        self.valid_systems = ['daint:gpu', 'dom:gpu', 'kesch:cn']
-        # FIXME: temporary workaround until the mvapich module is fixed
-        #        'PrgEnv-gnu-c2sm-gpu' will be added later
+        self.valid_systems = ['daint:gpu', 'dom:gpu', 'kesch:cn', 'tsa:cn']
         self.valid_prog_environs = ['PrgEnv-gnu']
         self.sourcepath = 'gpu_direct_cuda.cu'
         self.build_system = 'SingleSource'
@@ -28,6 +26,15 @@ class GpuDirectCudaCheck(rfm.RegressionTest):
                 'G2G': '1',
             }
             self.build_system.cxxflags = ['-ccbin', 'mpicxx', '-arch=sm_37']
+        elif self.current_system.name == 'tsa':
+            self.exclusive_access = True
+            self.valid_prog_environs = ['PrgEnv-gnu']
+            self.modules = ['cuda10.0/toolkit/10.0.130']
+            self.variables = {
+                'MV2_USE_CUDA': '1',
+                'G2G': '1',
+            }
+            self.build_system.cxxflags = ['-ccbin', 'mpicxx', '-arch=sm_70']
 
         self.num_tasks = 2
         self.num_gpus_per_node = 1
