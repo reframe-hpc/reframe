1e9b98e8c0d2bcad5dabdeea8b3bb3839c2d2558
Vasileios Karakasis <karakasis@cscs.ch>
2019-08-23 13:43:58 +0200

    Fix typo in comment

--- reframe/frontend/cli.py
+++ reframe/frontend/cli.py
@@ -498,7 +498,7 @@ def main():
         try:
             rt.system.preload_environ.load()
         except EnvironError as e:
-            printer.error("failed to load current's system environment; "
+            printer.error("failed to load current system's environment; "
                           "please check your configuration")
             printer.debug(str(e))
             raise
