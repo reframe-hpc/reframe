0aa140ab8536b25cb51ed42a15a0f402df2bd137
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2020-02-14 16:10:12 +0100

    Add magma tests

--- cscs-checks/libraries/magma/magma_checks.py
+++ cscs-checks/libraries/magma/magma_checks.py
@@ -7,7 +7,6 @@ import reframe.utility.sanity as sn
                         ['zsymmetrize'], ['ztranspose'])
 class MagmaCheck(rfm.RegressionTest):
     def __init__(self, subtest):
-        super().__init__()
         self.valid_systems = ['daint:gpu', 'dom:gpu']
         self.num_gpus_per_node = 1
         self.sanity_patterns = sn.assert_found(r'Result = PASS', self.stdout)
@@ -58,11 +57,7 @@ class MagmaCheck(rfm.RegressionTest):
                 'dom:gpu': {
                     'magma':  (3692.65, -0.05, None, 'Gflop/s'),
                     'cublas': (4269.31, -0.09, None, 'Gflop/s'),
-                },
-                '*': {
-                    'magma':  (0, None, None, 'Gflop/s'),
-                    'cublas': (0, None, None, 'Gflop/s'),
-                },
+                }
             }
         elif subtest == 'zsymmetrize':
             self.perf_patterns = {
@@ -75,10 +70,7 @@ class MagmaCheck(rfm.RegressionTest):
                 },
                 'dom:gpu': {
                     'gpu_perf': (158.3, -0.05, None, 'GB/s'),
-                },
-                '*': {
-                    'gpu_perf': (0, None, None, 'GB/s'),
-                },
+                }
             }
         elif subtest == 'ztranspose':
             self.perf_patterns = {
@@ -94,10 +86,7 @@ class MagmaCheck(rfm.RegressionTest):
                 },
                 'dom:gpu': {
                     'gpu_perf': (498.2, -0.05, None, 'GB/s'),
-                },
-                '*': {
-                    'gpu_perf': (0, None, None, 'GB/s'),
-                },
+                }
             }
         elif subtest == 'zunmbr':
             # This test fails to compile with Magma 2.4
@@ -114,8 +103,5 @@ class MagmaCheck(rfm.RegressionTest):
                 },
                 'dom:gpu': {
                     'gpu_perf': (254.7, -0.05, None, 'Gflop/s'),
-                },
-                '*': {
-                    'gpu_perf': (0, None, None, 'Gflop/s'),
-                },
+                }
             }
