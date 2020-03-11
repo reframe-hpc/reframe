ccaa108a2dd93f92967eb20a47e7559c9144b35f
Theofilos Manitaras <manitaras@cscs.ch>
2020-03-11 17:09:43 +0100

    Simplify unittesting of TERM signal handling
    * Explicitly call `os.kill` from a regression check.

---
+++ unittests/resources/checks_unlisted/selfkill.py
@@ -0,0 +1,29 @@
+# Copyright 2016-2020 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+#
+# Check for testing handling of the TERM signal
+#
+import os
+import signal
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+@rfm.simple_test
+class SelfKillCheck(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.local = True
+        self.valid_systems = ['*']
+        self.valid_prog_environs = ['*']
+        self.executable = 'echo hello'
+        self.sanity_patterns = sn.assert_found('hello', self.stdout)
+        self.tags = {type(self).__name__}
+        self.maintainers = ['TM']
+
+    @rfm.run_after('run')
+    def kill(self):
+        os.kill(os.getpid(), signal.SIGTERM)
