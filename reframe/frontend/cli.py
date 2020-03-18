0cf157c1f04cdef870cb38feee49cde083217bd6
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2020-03-18 13:40:26 +0100

    Add dependencies of a test in --list-detailed output

--- reframe/frontend/cli.py
+++ reframe/frontend/cli.py
@@ -25,6 +25,7 @@ from reframe.core.exceptions import (
     ReframeFatalError, ReframeForceExitError, SystemAutodetectionError
 )
 from reframe.core.exceptions import format_exception
+from reframe.core.pipeline import (DEPEND_EXACT, DEPEND_BY_ENV, DEPEND_FULLY)
 from reframe.frontend.executors import Runner, generate_testcases
 from reframe.frontend.executors.policies import (SerialExecutionPolicy,
                                                  AsynchronousExecutionPolicy)
@@ -33,19 +34,29 @@ from reframe.frontend.printer import PrettyPrinter
 
 
 def format_check(check, detailed):
+    def dep_type(t):
+        if t == DEPEND_EXACT:
+            return 'DEPEND_EXACT'
+        elif t == DEPEND_BY_ENV:
+            return 'DEPEND_BY_ENV'
+        elif t == DEPEND_FULLY:
+            return 'DEPEND_FULLY'
+
     lines = ['  * %s (found in %s)' % (check.name,
                                        inspect.getfile(type(check)))]
     flex = 'flexible' if check.num_tasks <= 0 else 'standard'
 
     if detailed:
         lines += [
-            '      - description: %s' % check.descr,
-            '      - systems: %s' % ', '.join(check.valid_systems),
-            '      - environments: %s' % ', '.join(check.valid_prog_environs),
-            '      - modules: %s' % ', '.join(check.modules),
-            '      - task allocation: %s' % flex,
-            '      - tags: %s' % ', '.join(check.tags),
-            '      - maintainers: %s' % ', '.join(check.maintainers)
+            f"      - description: {check.descr}",
+            f"      - systems: {', '.join(check.valid_systems)}",
+            f"      - environments: {', '.join(check.valid_prog_environs)}",
+            f"      - modules: {', '.join(check.modules)}",
+            f"      - task allocation: {flex}",
+            f"      - dependencies: "
+            f"{[(n, dep_type(t), s) for (n, t, s) in check.user_deps()]}",
+            f"      - tags: {', '.join(check.tags)}",
+            f"      - maintainers: {', '.join(check.maintainers)}"
         ]
 
     return '\n'.join(lines)
