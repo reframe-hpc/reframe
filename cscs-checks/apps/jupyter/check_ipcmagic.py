bf0b81d3e5a310ef387655c763ba7843845588c5
rafael <rafael.sarmiento@cscs.ch>
2020-06-04 10:26:56 +0200

    use default module version

--- cscs-checks/apps/jupyter/check_ipcmagic.py
+++ cscs-checks/apps/jupyter/check_ipcmagic.py
@@ -9,7 +9,7 @@ class IPCMagicCheck(rfm.RunOnlyRegressionTest):
         self.descr = 'Distributed training with TensorFlow using ipyparallel'
         self.valid_systems = ['daint:gpu', 'dom:gpu']
         self.valid_prog_environs = ['PrgEnv-gnu']
-        self.modules = ['ipcmagic/0.1-CrayGNU-19.10']
+        self.modules = ['ipcmagic']
         self.pre_run = [
             'module unload dask',
             'module load Horovod/0.16.4-CrayGNU-19.10-tf-1.14.0']
