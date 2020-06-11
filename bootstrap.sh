1b7e73161be9c9fe013367c21555964616600e3b
Vasileios Karakasis <karakasis@cscs.ch>
2020-06-11 23:13:31 +0200

    Add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright 2016-2020 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+#
+# Bootstrap script for running ReFrame from source
+#
+# Run once before the first run.
+
+pip3 install --upgrade pip
+pip3 install -r requirements.txt --prefix=external/
+
+if [ x"$1" == x"+docs" ]; then
+    pip3 install -r docs/requirements.txt --prefix=external/
+fi
