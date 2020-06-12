89935364c871cc551316c3291c27afde49dab42a
Vasileios Karakasis <karakasis@cscs.ch>
2020-06-13 00:38:58 +0200

    Fix bootstrap script for Daint/Dom

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,21 @@
 #
 # Bootstrap script for running ReFrame from source
 #
-# Run once before the first run.
+
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
 
 CMD()
 {
-    echo '==>' $* && $*
+    echo -e "${BLUE}==>${NC}" ${YELLOW}$*${NC} && $*
 }
 
 usage()
 {
     echo "Usage: $0 [-h] [+docs]"
-    echo "Bootstrap ReFrame; \
-run once before invoking ReFrame for the first time"
+    echo "Bootstrap ReFrame by pulling all its dependencies"
     echo "  -h      Print this help message and exit"
     echo "  +docs   Build also the documentation"
 }
@@ -33,20 +36,21 @@ while getopts "h" opt; do
 done
 
 shift $((OPTIND - 1))
-
 pyver=$(python3 -V | sed -n 's/Python \([0-9]\+\)\.\([0-9]\+\)\..*/\1.\2/p')
 
 # Install pip for Python 3
 CMD python3 -m ensurepip --root external/ --default-pip
 
-export PATH=external/bin:$PATH
-#export PYTHONPATH=external/usr/lib/python$pyver/site-packages:$PYTHONPATH
-export PYTHONPATH=external/:$PYTHONPATH
+# ensurepip installs pip in `external/usr/` whereas the --target option installs
+# everything under `external/`. That's why include both in the PYTHONPATH
 
-CMD pip install -q --upgrade pip --target=external/
-CMD pip install -q -r requirements.txt --upgrade --target=external/
+export PATH=$(pwd)/external/usr/bin:$PATH
+export PYTHONPATH=$(pwd)/external:$(pwd)/external/usr/lib/python$pyver/site-packages:$PYTHONPATH
+
+CMD python3 -m pip install -q --upgrade pip --target=external/
+CMD python3 -m pip install -q -r requirements.txt --target=external/ --upgrade
 
 if [ x"$1" == x"+docs" ]; then
-    CMD pip install -q -r docs/requirements.txt --upgrade --target=external/
+    CMD python3 -m pip install -q -r docs/requirements.txt --target=external/ --upgrade
     make -C docs
 fi
