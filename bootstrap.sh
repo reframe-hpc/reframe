39e2043aee3d3122ed38f86dcaeb8863559ef7e9
Vasileios Karakasis <karakasis@cscs.ch>
2020-06-15 15:56:51 +0200

    Allow a custom Python executable in the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,23 +23,29 @@ usage()
 {
     echo "Usage: $0 [-h] [+docs]"
     echo "Bootstrap ReFrame by pulling all its dependencies"
-    echo "  -h      Print this help message and exit"
-    echo "  +docs   Build also the documentation"
+    echo "  -P EXEC  Use EXEC as Python interpreter"
+    echo "  -h       Print this help message and exit"
+    echo "  +docs    Build also the documentation"
 }
 
 
-while getopts "h" opt; do
+while getopts "hP:" opt; do
     case $opt in
+        "P") python=$OPTARG ;;
         "h") usage && exit 0 ;;
         "?") usage && exit 0 ;;
     esac
 done
 
 shift $((OPTIND - 1))
-pyver=$(python3 -V | sed -n 's/Python \([0-9]\+\)\.\([0-9]\+\)\..*/\1.\2/p')
+if [ -z $python ]; then
+    python=python3
+fi
+
+pyver=$($python -V | sed -n 's/Python \([0-9]\+\)\.\([0-9]\+\)\..*/\1.\2/p')
 
 # Install pip for Python 3
-CMD python3 -m ensurepip --root external/ --default-pip
+CMD $python -m ensurepip --root external/ --default-pip
 
 # ensurepip installs pip in `external/usr/` whereas the --target option installs
 # everything under `external/`. That's why include both in the PYTHONPATH
@@ -47,10 +53,10 @@ CMD python3 -m ensurepip --root external/ --default-pip
 export PATH=$(pwd)/external/usr/bin:$PATH
 export PYTHONPATH=$(pwd)/external:$(pwd)/external/usr/lib/python$pyver/site-packages:$PYTHONPATH
 
-CMD python3 -m pip install -q --upgrade pip --target=external/
-CMD python3 -m pip install -q -r requirements.txt --target=external/ --upgrade
+CMD $python -m pip install -q --upgrade pip --target=external/
+CMD $python -m pip install -q -r requirements.txt --target=external/ --upgrade
 
 if [ x"$1" == x"+docs" ]; then
-    CMD python3 -m pip install -q -r docs/requirements.txt --target=external/ --upgrade
+    CMD $python -m pip install -q -r docs/requirements.txt --target=external/ --upgrade
     make -C docs
 fi
