601cbec574bfc2bf14d91e5e6d6a7a5d8ebd982a
Christopher Bignamini <christopher.bignamini@gmail.com>
2020-07-17 15:51:27 +0200

    Added workaround for dynamic linking

--- cscs-checks/libraries/math/trilinos_compile_run.py
+++ cscs-checks/libraries/math/trilinos_compile_run.py
@@ -21,6 +21,7 @@ class TrilinosTest(rfm.RegressionTest):
         # NOTE: PrgEnv-cray_classic does not support trilinos
         if linkage == 'static':
             self.valid_prog_environs += ['PrgEnv-cray']
+        self.linkage = linkage
 
         self.build_system = 'SingleSource'
         self.build_system.ldflags = ['-%s' % linkage, '-lparmetis']
@@ -57,9 +58,17 @@ class TrilinosTest(rfm.RegressionTest):
     def cdt2006_workaround_intel(self):
         if (self.current_environ.name == 'PrgEnv-intel' and
             os_ext.cray_cdt_version() == '20.06'):
-            self.modules += ['cray-netcdf-hdf5parallel']
-            self.prebuild_cmds = [
-                'ln -s $CRAY_NETCDF_HDF5PARALLEL_PREFIX/lib/pkgconfig/'
-                'netcdf-cxx4_parallel.pc netcdf_c++4_parallel.pc'
-            ]
-            self.variables['PKG_CONFIG_PATH'] = '.:$PKG_CONFIG_PATH'
+                self.modules += ['cray-netcdf-hdf5parallel']
+                self.prebuild_cmds = [
+                    'ln -s $CRAY_NETCDF_HDF5PARALLEL_PREFIX/lib/pkgconfig/'
+                    'netcdf-cxx4_parallel.pc netcdf_c++4_parallel.pc']
+                self.variables['PKG_CONFIG_PATH'] = '.:$PKG_CONFIG_PATH'
+
+    @rfm.run_before('compile')
+    def cdt2006_workaround_dynamic(self):
+        if (os_ext.cray_cdt_version() == '20.06' and
+            self.linkage == 'dynamic'):
+            self.variables['PATH'] = '/opt/cray/pe/cce/10.0.1/cce-clang/x86_64/bin:$PATH'
+            self.prgenv_flags[self.current_environ.name] += ['-fuse-ld=lld']
+            if self.current_environ.name == 'PrgEnv-gnu':
+                self.modules += ['gcc/9.3.0']
