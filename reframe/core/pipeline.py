f3f32744bafac5f90fff7ed5bee65494efe50341
Theofilos Manitaras <manitaras@cscs.ch>
2020-08-27 14:06:48 +0200

    Reformat in one line

--- reframe/core/pipeline.py
+++ reframe/core/pipeline.py
@@ -1436,10 +1436,7 @@ class RegressionTest(metaclass=RegressionTestMeta):
 
                 for var in variables:
                     name, unit = var
-                    ref_tuple = (
-                        0, None, None,
-                        unit or '(no unit specified)'
-                    )
+                    ref_tuple = (0, None, None, unit or '(no unit specified)')
                     self.reference.update({'*': {name: ref_tuple}})
 
             # We first evaluate and log all performance values and then we
