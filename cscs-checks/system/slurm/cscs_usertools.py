e977f649bd4d3efa5a87c63403cd97944f0c72f2
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2020-09-11 09:03:22 +0200

    Adjust cscs usertools
    - Remove monthly_usage test
    - Change sbucheck to accounting

--- cscs-checks/system/slurm/cscs_usertools.py
+++ cscs-checks/system/slurm/cscs_usertools.py
@@ -10,32 +10,16 @@ import reframe.utility.sanity as sn
 
 
 @rfm.simple_test
-class SbucheckCommandCheck(rfm.RunOnlyRegressionTest):
+class AccountingCommandCheck(rfm.RunOnlyRegressionTest):
     def __init__(self):
         self.valid_systems = ['daint:login', 'dom:login']
         self.descr = 'Slurm CSCS usertools sbucheck'
-        self.executable = 'sbucheck'
-        self.valid_prog_environs = ['PrgEnv-cray']
+        self.executable = 'accounting'
+        self.valid_prog_environs = ['builtin']
         self.num_tasks = 1
         self.num_tasks_per_node = 1
         self.tags = {'cscs_usertools', 'production',
                      'maintenance', 'single-node', 'ops'}
         self.sanity_patterns = sn.assert_found(
             r'Per-project usage at CSCS since', self.stdout)
-        self.maintainers = ['VH', 'TM']
-
-
-@rfm.simple_test
-class MonthlyUsageCheck(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.valid_systems = ['daint:login', 'dom:login']
-        self.descr = 'Slurm CSCS usertools monthly_usage'
-        self.executable = 'monthly_usage'
-        self.valid_prog_environs = ['PrgEnv-cray']
-        self.num_tasks = 1
-        self.num_tasks_per_node = 1
-        self.tags = {'cscs_usertools', 'production',
-                     'maintenance', 'single-node', 'ops'}
-        self.sanity_patterns = sn.assert_found(
-            r'Usage in Node hours for the Crays', self.stdout)
         self.maintainers = ['VH', 'TM']
