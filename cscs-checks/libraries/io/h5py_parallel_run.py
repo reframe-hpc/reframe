e5ff731525a1d181795a2533bb604cb228e7729e
Theofilos Manitaras <manitaras@cscs.ch>
2020-10-08 13:42:57 +0200

    Address PR comments

--- cscs-checks/libraries/io/h5py_parallel_run.py
+++ cscs-checks/libraries/io/h5py_parallel_run.py
@@ -14,14 +14,12 @@ class H5PyTest(rfm.RunOnlyRegressionTest):
         self.valid_systems = ['daint:gpu', 'daint:mc', 'dom:gpu', 'dom:mc']
         self.valid_prog_environs = ['builtin']
         self.modules = ['h5py']
-        self.num_nodes = 1
         self.num_tasks = 4
         expected_dataset = ', '.join(str(i) for i in range(self.num_tasks))
         self.sanity_patterns = sn.assert_found(
             rf'.*DATA\s+{{\s+\(0\): {expected_dataset}\s*}}',
             self.stdout
         )
-
         self.executable = 'python'
         self.executable_opts = ['h5py_mpi_test.py']
         self.postrun_cmds = ['h5dump parallel_test.hdf5']
