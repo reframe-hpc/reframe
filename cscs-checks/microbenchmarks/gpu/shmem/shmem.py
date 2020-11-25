bc3b71bb5b8c7e8869db3a39150c4704a1ce0ea0
Javier J. Otero Perez <jotero@cscs.ch>
2020-11-25 11:40:00 +0100

    Port shared bw test to AMD GPUs.

--- cscs-checks/microbenchmarks/gpu/shmem/shmem.py
+++ cscs-checks/microbenchmarks/gpu/shmem/shmem.py
@@ -11,21 +11,15 @@ import reframe.utility.sanity as sn
 @rfm.simple_test
 class GPUShmemTest(rfm.RegressionTest):
     def __init__(self):
-        self.sourcepath = 'shmem.cu'
-        self.build_system = 'SingleSource'
         self.valid_systems = ['daint:gpu', 'dom:gpu',
                               'ault:amdv100', 'ault:intelv100',
-                              'ault:amda100']
+                              'ault:amda100', 'ault:amdvega']
         self.valid_prog_environs = ['PrgEnv-gnu']
         self.num_tasks = 0
         self.num_tasks_per_node = 1
         self.num_gpus_per_node = 1
-        if self.current_system.name in {'daint', 'dom'}:
-            self.modules = ['craype-accel-nvidia60']
-
-        elif self.current_system.name in {'ault'}:
-            self.modules = ['cuda']
-
+        self.build_system = 'Make'
+        self.executable = 'shmem.x'
         self.sanity_patterns = sn.assert_eq(
             sn.count(sn.findall(r'Bandwidth', self.stdout)),
             self.num_tasks_assigned * 2)
@@ -52,6 +46,9 @@ class GPUShmemTest(rfm.RegressionTest):
             },
             'ault:amda100': {
                 'bandwidth': (18139, -0.01, None, 'GB/s')
+            },
+            'ault:amdvega': {
+                'bandwidth': (9209, -0.01, None, 'GB/s')
             }
         }
 
@@ -62,3 +59,42 @@ class GPUShmemTest(rfm.RegressionTest):
     @sn.sanity_function
     def num_tasks_assigned(self):
         return self.job.num_tasks
+
+    @rfm.run_after('setup')
+    def select_makefile(self):
+        cp = self.current_partition.fullname
+        if cp == 'ault:amdvega':
+            self.prebuild_cmds = ['cp makefile.hip Makefile']
+        else:
+            self.prebuild_cmds = ['cp makefile.cuda Makefile']
+
+    @rfm.run_after('setup')
+    def set_gpu_arch(self):
+        cp = self.current_partition.fullname
+
+        # Deal with the NVIDIA options first
+        nvidia_sm = None
+        if cp in {'tsa:cn', 'ault:intelv100', 'ault:amdv100'}:
+            nvidia_sm = '70'
+        elif cp == 'ault:amda100':
+            nvidia_sm = '80'
+        elif cp in {'dom:gpu', 'daint:gpu'}:
+            nvidia_sm == '60'
+
+        if nvidia_sm:
+            self.build_system.cxxflags += [f'-arch=sm_{nvidia_sm}']
+            if cp in {'dom:gpu', 'daint:gpu'}:
+                self.modules += ['craype-accel-nvidia60']
+            else:
+                self.modules += ['cuda']
+
+        # Deal with the AMD options
+        amd_trgt = None
+        if cp == 'ault:amdvega':
+            amd_trgt = 'gfx906,gfx908'
+
+        if amd_trgt:
+            self.build_system.cxxflags += [f'--amdgpu-target={amd_trgt}']
+            self.modules += ['rocm']
+
+
