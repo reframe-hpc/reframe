2a87f57a3919acc2899b8190eac7024345f01bcd
Javier J. Otero Perez <jotero@cscs.ch>
2020-12-09 15:34:41 +0100

    Cleanup dgemm-gpu output.

--- cscs-checks/microbenchmarks/gpu/dgemm/src/dgemm.cu
+++ cscs-checks/microbenchmarks/gpu/dgemm/src/dgemm.cu
@@ -5,10 +5,11 @@
  * all the elements of the resulting matrix will be just SIZE.
  */
 
-#define SIZE 2048
+#define SIZE 1024
 #define REPEAT 30
 
 #include <iostream>
+#include <unistd.h>
 
 #include "Xdevice/runtime.hpp"
 #include "Xdevice/blas.hpp"
@@ -39,44 +40,47 @@ namespace kernels
 }
 
 #define BLOCK_SIZE 128
+#define HOST_NAME_SIZE 128
 int main(int argc, char **argv)
 {
-    double * A;
-    double * B;
-    double * C;
 
-    XMalloc((void**)&A, sizeof(double)*SIZE*SIZE);
-    XMalloc((void**)&B, sizeof(double)*SIZE*SIZE);
-    XMalloc((void**)&C, sizeof(double)*SIZE*SIZE);
+    char hostname[HOST_NAME_SIZE];
+    gethostname(hostname, sizeof(hostname));
 
-    kernels::init_as_ones<double><<<(SIZE*SIZE+BLOCK_SIZE-1)/BLOCK_SIZE, BLOCK_SIZE>>>(A, SIZE*SIZE);
-    kernels::init_as_ones<double><<<(SIZE*SIZE+BLOCK_SIZE-1)/BLOCK_SIZE, BLOCK_SIZE>>>(B, SIZE*SIZE);
-    XDeviceSynchronize();
+    double tflops = SIZE*SIZE*SIZE*2.0 * 1E-12;
+    int num_devices, totalErrors = 0;
+    XGetDeviceCount(&num_devices);
 
-    XStream_t stream;
-    XStreamCreate(&stream);
-    XblasHandle_t blas_handle;
-    XblasCreate(&blas_handle);
-    XblasSetStream(blas_handle, stream);
-    const double alpha = 1.0;
-    const double beta = 0.0;
+    // Print device count
+    printf("[%s] Found %d device(s).\n", hostname, num_devices);
 
-    // Warmup call
-    XblasDgemm(blas_handle,
-               XBLAS_OP_N, XBLAS_OP_N,
-               SIZE, SIZE, SIZE,
-               &alpha,
-               (const double*)A, SIZE,
-               (const double*)B, SIZE,
-               &beta,
-               C, SIZE);
-
-    // Time the execution
-    XTimer t(stream);
-    t.start();
-
-    for (int i = 0; i < REPEAT; i++)
+    // Do the dgemm for all devices in the node.
+    for (int device = 0; device < num_devices; device++)
     {
+
+        XSetDevice(device);
+
+        double * A;
+        double * B;
+        double * C;
+        const double alpha = 1.0;
+        const double beta = 0.0;
+
+        XMalloc((void**)&A, sizeof(double)*SIZE*SIZE);
+        XMalloc((void**)&B, sizeof(double)*SIZE*SIZE);
+        XMalloc((void**)&C, sizeof(double)*SIZE*SIZE);
+
+        kernels::init_as_ones<double><<<(SIZE*SIZE+BLOCK_SIZE-1)/BLOCK_SIZE, BLOCK_SIZE>>>(A, SIZE*SIZE);
+        kernels::init_as_ones<double><<<(SIZE*SIZE+BLOCK_SIZE-1)/BLOCK_SIZE, BLOCK_SIZE>>>(B, SIZE*SIZE);
+        XDeviceSynchronize();
+
+        XStream_t stream;
+        XStreamCreate(&stream);
+        XblasHandle_t blas_handle;
+        XblasCreate(&blas_handle);
+        XblasSetStream(blas_handle, stream);
+
+        // Warmup call
         XblasDgemm(blas_handle,
                    XBLAS_OP_N, XBLAS_OP_N,
                    SIZE, SIZE, SIZE,
@@ -85,22 +89,47 @@ int main(int argc, char **argv)
                    (const double*)B, SIZE,
                    &beta,
                    C, SIZE);
+        XDeviceSynchronize();
+
+        // Time the execution
+        XTimer t(stream);
+        t.start();
+        for (int i = 0; i < REPEAT; i++)
+        {
+            XblasDgemm(blas_handle,
+                       XBLAS_OP_N, XBLAS_OP_N,
+                       SIZE, SIZE, SIZE,
+                       &alpha,
+                       (const double*)A, SIZE,
+                       (const double*)B, SIZE,
+                       &beta,
+                       C, SIZE);
+        }
+
+        // Calc the performance data in TFlops/sec
+        double perf = tflops/(t.stop()/REPEAT/1000.0);
+
+        XblasDestroy(blas_handle);
+        XStreamDestroy(stream);
+
+        // Verify that the final values of C are correct.
+        int * err, h_err = 0;
+        XMalloc((void**)&err, sizeof(int));
+        XMemcpy( err, &h_err, sizeof(int), XMemcpyHostToDevice);
+        kernels::verify<double><<<(SIZE+BLOCK_SIZE-1)/BLOCK_SIZE, BLOCK_SIZE>>>(C, SIZE*SIZE, err);
+        XMemcpy(&h_err, err, sizeof(int), XMemcpyDeviceToHost);
+        totalErrors += h_err;
+
+        // Print the performance results
+        printf("[%s][GPU %d] %4.2f TF/s\n", hostname, device, (float)perf);
+
+        XFree(A);
+        XFree(B);
+        XFree(C);
     }
 
-    std::cout << "Elapsed time: " << t.stop() << std::endl;
-
-    XblasDestroy(blas_handle);
-    XStreamDestroy(stream);
-
-    int * err, h_err = 0;
-    XMalloc((void**)&err, sizeof(int));
-    XMemset(&err, 0, sizeof(int));
-    kernels::verify<double><<<(SIZE+BLOCK_SIZE-1)/BLOCK_SIZE, BLOCK_SIZE>>>(C, SIZE*SIZE, err);
-
-    XDeviceSynchronize();
-    XMemcpy(&h_err, err, sizeof(int), XMemcpyDeviceToHost);
-    XDeviceSynchronize();
-    std::cout << "Number of errors:" << h_err << std::endl;
+    // Test if there were any errors and print the test result.
+    printf("[%s] Test %s\n", hostname, totalErrors == 0 ? "passed" : "failed");
 
     return 0;
 }
