6d64ec204eb8f9c39eedd76774e7b7857deefbdb
jgphpc <jgp@cscs.ch>
2021-01-04 14:00:47 +0100

    fix for review

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 from reframe import VERSION
 
 with open('README.md') as read_me:
-    long_description = ''.join(read_me.readlines()[15:])
+    long_description = ''.join(read_me.readlines()[16:])
 
 setuptools.setup(
     name='ReFrame-HPC',
