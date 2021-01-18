834403aeb497edb2ac486eb096e488f74ee37737
Theofilos Manitaras <manitaras@cscs.ch>
2021-01-18 16:07:52 +0100

    Update copyright in docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ReFrame'
-copyright = '2016-2020, CSCS/ETH Zurich'
+copyright = '2016-2021, CSCS/ETH Zurich'
 author = 'ReFrame Project Developers'
 
 # The version info for the project you're documenting, acts as replacement for
