8e9bb1ad3db8f471bacfff727381cc235968791c
jgp <jgp@cscs.ch>
2021-01-23 10:55:30 +0100

    typo2

--- cscs-checks/microbenchmarks/gpu/roofline/berkeley-ert-gpu.py
+++ cscs-checks/microbenchmarks/gpu/roofline/berkeley-ert-gpu.py
@@ -233,8 +233,9 @@ class P100_RunErt(RunErt_Base):
         self.time_limit = "10m"
         # set blocks and threads per block:
         self.ert_gpu_threads = ert_gpu_threads
-        maximum_number_of_threads = (gpu_specs[gpu]["multiprocessors"] *
-            gpu_specs[gpu]["maximum_number_of_threads_per_multiprocessor"]
+        maximum_number_of_threads = (
+            gpu_specs[gpu]["multiprocessors"]
+            * gpu_specs[gpu]["maximum_number_of_threads_per_multiprocessor"]
         )
         self.ert_gpu_blocks = int(maximum_number_of_threads / ert_gpu_threads)
         # The other steps are in the base class
@@ -356,8 +357,9 @@ class V100_RunErt(RunErt_Base):
         self.time_limit = "5m"
         # set blocks and threads per block:
         self.ert_gpu_threads = ert_gpu_threads
-        maximum_number_of_threads = (gpu_specs[gpu]["multiprocessors"] *
-            gpu_specs[gpu]["maximum_number_of_threads_per_multiprocessor"]
+        maximum_number_of_threads = (
+            gpu_specs[gpu]["multiprocessors"]
+            * gpu_specs[gpu]["maximum_number_of_threads_per_multiprocessor"]
         )
         self.ert_gpu_blocks = int(maximum_number_of_threads / ert_gpu_threads)
         # The other steps are in the base class
