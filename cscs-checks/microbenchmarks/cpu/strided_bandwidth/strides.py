7b16466c84a494b66353013f9899cd1bc6606156
Sebastian Keller <keller@cscs.ch>
2021-02-16 11:19:05 +0100

    updated strided bw test for eiger

--- cscs-checks/microbenchmarks/cpu/strided_bandwidth/strides.py
+++ cscs-checks/microbenchmarks/cpu/strided_bandwidth/strides.py
@@ -11,7 +11,7 @@ class StridedBase(rfm.RegressionTest):
     def __init__(self):
         self.sourcepath = 'strides.cpp'
         self.build_system = 'SingleSource'
-        self.valid_systems = ['daint:gpu', 'dom:gpu', 'daint:mc', 'dom:mc']
+        self.valid_systems = ['daint:gpu', 'dom:gpu', 'daint:mc', 'dom:mc', 'eiger:mc']
         self.valid_prog_environs = ['PrgEnv-gnu']
         self.num_tasks = 1
         self.num_tasks_per_node = 1
@@ -31,6 +31,7 @@ class StridedBase(rfm.RegressionTest):
             'daint:gpu': 24,
             'dom:mc':  72,
             'dom:gpu': 24,
+            'eiger:mc': 128,
         }
 
         self.maintainers = ['SK']
@@ -60,7 +61,10 @@ class StridedBandwidthTest(StridedBase):
             },
             'daint:mc': {
                 'bandwidth': (100, -0.1, 0.1, 'GB/s')
-            }
+            },
+            'eiger:mc': {
+                'bandwidth': (270, -0.1, 0.1, 'GB/s')
+            },
         }
 
     @rfm.run_before('run')
@@ -89,7 +93,10 @@ class StridedBandwidthTest64(StridedBase):
             },
             'daint:mc': {
                 'bandwidth': (12.5, -0.1, 0.2, 'GB/s')
-            }
+            },
+            'eiger:mc': {
+                'bandwidth': (33, -0.1, 0.2, 'GB/s')
+            },
         }
 
     @rfm.run_before('run')
@@ -118,7 +125,10 @@ class StridedBandwidthTest128(StridedBase):
             },
             'daint:mc': {
                 'bandwidth': (9.1, -0.1, 0.2, 'GB/s')
-            }
+            },
+            'eiger:mc': {
+                'bandwidth': (33, -0.1, 0.2, 'GB/s')
+            },
         }
 
     @rfm.run_before('run')
