59475dacb7437c34dc07117eff35fee20b658fb1
Luca <lucamar@cscs.ch>
2021-02-17 21:48:23 +0100

    VASP check

---
+++ cscs-checks/apps/vasp/vasp_check.py
@@ -0,0 +1,121 @@
+# Copyright 2016-2021 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+class VASPCheck(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.valid_prog_environs = ['builtin']
+        self.modules = ['VASP']
+
+        force = sn.extractsingle(r'1 F=\s+(?P<result>\S+)',
+                                        self.stdout, 'result', float)
+        self.sanity_patterns = sn.assert_reference(
+            force, -.85026214E+03, -1e-5, 1e-5)
+        self.keep_files = ['OUTCAR']
+
+        self.perf_patterns = {
+            'time': sn.extractsingle(r'Total CPU time used \(sec\):'
+                                     r'\s+(?P<time>\S+)', 'OUTCAR',
+                                     'time', float)
+        }
+
+        self.maintainers = ['LM']
+        self.tags = {'scs'}
+        self.strict_check = False
+        self.extra_resources = {
+            'switches': {
+                'num_switches': 1
+            }
+        }
+
+
+@rfm.parameterized_test(*([v]
+                          for v in ['maint', 'prod']))
+class VASPCpuCheck(VASPCheck):
+    def __init__(self, variant):
+        super().__init__()
+        self.descr = 'VASP CPU check (version: %s)' % variant
+        self.valid_systems = ['daint:mc', 'dom:mc', 'eiger:mc']
+        self.executable = 'vasp_std'
+        if self.current_system.name == 'dom':
+            self.num_tasks = 72
+            self.num_tasks_per_node = 12
+            self.use_multithreading = True
+        elif self.current_system.name == 'eiger':
+            self.num_tasks = 96
+            self.num_tasks_per_node = 16
+            self.num_cpus_per_task = 16
+            self.num_tasks_per_core = 1
+            self.use_multithreading = False
+            self.variables = {
+                'MPICH_OFI_STARTUP_CONNECT': '1',
+                'OMP_NUM_THREADS': '8',
+                'OMP_PLACES': 'cores',
+                'OMP_PROC_BIND': 'close'
+            }
+        else:
+            self.num_tasks = 32
+            self.num_tasks_per_node = 2
+            self.use_multithreading = True
+
+        references = {
+            'maint': {
+                'dom:mc': {'time': (148.7, None, 0.05, 's')},
+                'daint:mc': {'time': (105.3, None, 0.20, 's')},
+                'eiger:mc': {'time': (100.0, None, 0.10, 's')}
+            },
+            'prod': {
+                'dom:mc': {'time': (148.7, None, 0.05, 's')},
+                'daint:mc': {'time': (105.3, None, 0.20, 's')},
+                'eiger:mc': {'time': (100.0, None, 0.10, 's')}
+            }
+        }
+
+        self.reference = references[variant]
+        self.tags |= {'maintenance' if variant == 'maint' else 'production'}
+
+    @rfm.run_before('run')
+    def set_task_distribution(self):
+        self.job.options = ['--distribution=block:block']
+
+    @rfm.run_before('run')
+    def set_cpu_binding(self):
+        self.job.launcher.options = ['--cpu-bind=cores']
+
+
+@rfm.parameterized_test(*([v]
+                          for v in ['maint', 'prod']))
+class VASPGpuCheck(VASPCheck):
+    def __init__(self, variant):
+        super().__init__()
+        self.descr = 'VASP GPU check (version: %s)' % variant
+        self.valid_systems = ['daint:gpu', 'dom:gpu']
+        self.executable = 'vasp_gpu'
+        self.variables = {'CRAY_CUDA_MPS': '1'}
+
+        self.num_gpus_per_node = 1
+        if self.current_system.name == 'dom':
+            self.num_tasks = 6
+            self.num_tasks_per_node = 1
+        else:
+            self.num_tasks = 16
+            self.num_tasks_per_node = 1
+
+        references = {
+            'maint': {
+                'dom:gpu': {'time': (61.0, None, 0.10, 's')},
+                'daint:gpu': {'time': (46.7, None, 0.20, 's')},
+            },
+            'prod': {
+                'dom:gpu': {'time': (61.0, None, 0.10, 's')},
+                'daint:gpu': {'time': (46.7, None, 0.20, 's')},
+            }
+        }
+
+        self.reference = references[variant]
+        self.tags |= {'maintenance' if variant == 'maint' else 'production'}
