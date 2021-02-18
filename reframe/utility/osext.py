4a3953fb56eec25820f2162bfd064420d6eb28f2
Vasileios Karakasis <karakasis@cscs.ch>
2021-02-18 17:58:16 +0100

    Fix PEP8 issues

--- reframe/utility/osext.py
+++ reframe/utility/osext.py
@@ -570,8 +570,8 @@ def unique_abs_paths(paths, prune_children=True):
 
 
 def cray_cdt_version():
-    '''Return the Cray Development Toolkit (CDT) version or :class:`None` if the
-    version cannot be retrieved.'''
+    '''Return the Cray Development Toolkit (CDT) version or :class:`None` if
+    the version cannot be retrieved.'''
 
     rcfile = os.getenv('MODULERCFILE', '/opt/cray/pe/cdt/default/modulerc')
     try:
