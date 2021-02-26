d32013afe03d7b612eefb5b7649902273e925a08
rafael <rafael.sarmiento@cscs.ch>
2021-02-26 12:40:57 +0100

    add pyspark version

--- cscs-checks/apps/spark/spark_check.py
+++ cscs-checks/apps/spark/spark_check.py
@@ -10,26 +10,22 @@ import reframe.utility.sanity as sn
 from reframe.core.backends import getlauncher
 
 
-@rfm.simple_test
-class SparkCheck(rfm.RunOnlyRegressionTest):
+class SparkBaseCheck(rfm.RunOnlyRegressionTest):
     def __init__(self):
-        self.descr = 'Simple calculation of pi with Spark'
         self.valid_systems = ['daint:gpu', 'daint:mc',
                               'dom:gpu', 'dom:mc']
         self.valid_prog_environs = ['builtin']
         self.modules = ['Spark']
-        self.sourcesdir = None
         self.prerun_cmds = ['start-all.sh']
         self.postrun_cmds = ['stop-all.sh']
-        self.num_tasks = 2
+        self.num_tasks = 3
         self.num_tasks_per_node = 1
         pi_value = sn.extractsingle(r'Pi is roughly\s+(?P<pi>\S+)',
                                     self.stdout, 'pi', float)
         self.sanity_patterns = sn.assert_lt(sn.abs(pi_value - math.pi), 0.01)
-        self.maintainers = ['TM', 'TR']
+        self.maintainers = ['TM', 'RS']
         self.tags = {'production'}
 
-    @rfm.run_before('run')
     def prepare_run(self):
         if self.current_partition.fullname in ['daint:gpu', 'dom:gpu']:
             num_workers = 12
@@ -39,16 +35,44 @@ class SparkCheck(rfm.RunOnlyRegressionTest):
             exec_cores = 9
 
         self.variables = {
-            'SPARK_WORKER_CORES': '%s' % num_workers,
+            'SPARK_WORKER_CORES': f'{num_workers}',
             'SPARK_LOCAL_DIRS': '"/tmp"',
         }
-        self.executable = (
-            f'spark-submit --conf spark.default.parallelism={num_workers} '
-            f'--conf spark.executor.cores={exec_cores} '
-            f'--conf spark.executor.memory=15g --master $SPARKURL '
-            f'--class org.apache.spark.examples.SparkPi '
-            f'$EBROOTSPARK/examples/jars/spark-examples*.jar 10000;'
-        )
+        self.executable = 'spark-submit'
+        self.executable_opts = [
+            f'--conf spark.default.parallelism={num_workers}',
+            f'--conf spark.executor.cores={exec_cores}',
+            f'--conf spark.executor.memory=15g',
+            f'--master $SPARKURL'
+        ]
         # The job launcher has to be changed since the `spark-submit`
         # script is not used with srun.
         self.job.launcher = getlauncher('local')()
+
+
+@rfm.simple_test
+class SparkCheck(SparkBaseCheck):
+    def __init__(self):
+        super().__init__()
+        self.descr = 'Simple calculation of pi with Spark'
+        self.sourcesdir = None
+
+    @rfm.run_before('run')
+    def prepare_run(self):
+        super().prepare_run()
+        self.executable_opts.extend([
+            '--class org.apache.spark.examples.SparkPi',
+            '$EBROOTSPARK/examples/jars/spark-examples*.jar 10000'
+        ])
+
+
+@rfm.simple_test
+class PySparkCheck(SparkBaseCheck):
+    def __init__(self):
+        super().__init__()
+        self.descr = 'Simple calculation of pi with PySpark'
+
+    @rfm.run_before('run')
+    def prepare_run(self):
+        super().prepare_run()
+        self.executable_opts.append('spark_pi.py')
