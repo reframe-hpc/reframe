26ddc98b631a651a902876e9b4767c7275f4544c
Vasileios Karakasis <karakasis@cscs.ch>
2021-03-01 15:40:56 +0100

    Address PR comments

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 from reframe import VERSION
 
 with open('README.md') as read_me:
-    long_description = ''.join(read_me.readlines()[18:])
+    long_description = ''.join(read_me.readlines()[17:])
 
 setuptools.setup(
     name='ReFrame-HPC',
