392f0fbffafe5d46ca6532ec262066ad77cadedc
rafael <rafael.sarmiento@cscs.ch>
2021-03-02 08:37:54 +0100

    fix comments

--- cscs-checks/apps/spark/spark_check.py
+++ cscs-checks/apps/spark/spark_check.py
@@ -10,8 +10,12 @@ import reframe.utility.sanity as sn
 from reframe.core.backends import getlauncher
 
 
-class SparkBaseCheck(rfm.RunOnlyRegressionTest):
+@rfm.simple_test
+class SparkCheck(rfm.RunOnlyRegressionTest):
+    variant = parameter(['spark', 'pyspark'])
+
     def __init__(self):
+        self.descr = f'Simple calculation of pi with {self.variant}'
         self.valid_systems = ['daint:gpu', 'daint:mc',
                               'dom:gpu', 'dom:mc']
         self.valid_prog_environs = ['builtin']
@@ -26,6 +30,7 @@ class SparkBaseCheck(rfm.RunOnlyRegressionTest):
         self.maintainers = ['TM', 'RS']
         self.tags = {'production'}
 
+    @rfm.run_before('run')
     def prepare_run(self):
         if self.current_partition.fullname in ['daint:gpu', 'dom:gpu']:
             num_workers = 12
@@ -35,7 +40,7 @@ class SparkBaseCheck(rfm.RunOnlyRegressionTest):
             exec_cores = 9
 
         self.variables = {
-            'SPARK_WORKER_CORES': f'{num_workers}',
+            'SPARK_WORKER_CORES': str(num_workers),
             'SPARK_LOCAL_DIRS': '"/tmp"',
         }
         self.executable = 'spark-submit'
@@ -45,34 +50,14 @@ class SparkBaseCheck(rfm.RunOnlyRegressionTest):
             f'--conf spark.executor.memory=15g',
             f'--master $SPARKURL'
         ]
+        if self.variant == 'spark':
+            self.executable_opts.extend([
+                '--class org.apache.spark.examples.SparkPi',
+                '$EBROOTSPARK/examples/jars/spark-examples*.jar 10000'
+            ])
+        else:
+            self.executable_opts.append('spark_pi.py')
+
         # The job launcher has to be changed since the `spark-submit`
         # script is not used with srun.
         self.job.launcher = getlauncher('local')()
-
-
-@rfm.simple_test
-class SparkCheck(SparkBaseCheck):
-    def __init__(self):
-        super().__init__()
-        self.descr = 'Simple calculation of pi with Spark'
-        self.sourcesdir = None
-
-    @rfm.run_before('run')
-    def prepare_run(self):
-        super().prepare_run()
-        self.executable_opts.extend([
-            '--class org.apache.spark.examples.SparkPi',
-            '$EBROOTSPARK/examples/jars/spark-examples*.jar 10000'
-        ])
-
-
-@rfm.simple_test
-class PySparkCheck(SparkBaseCheck):
-    def __init__(self):
-        super().__init__()
-        self.descr = 'Simple calculation of pi with PySpark'
-
-    @rfm.run_before('run')
-    def prepare_run(self):
-        super().prepare_run()
-        self.executable_opts.append('spark_pi.py')
