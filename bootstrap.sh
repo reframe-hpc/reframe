73d0fcdc2859eb271231f425c980a590eed97a5e
Theofilos Manitaras <manitaras@cscs.ch>
2021-03-18 10:55:50 +0100

    Color 'bootstrap.sh' only if stdout is a terminal

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,12 @@
 # Bootstrap script for running ReFrame from source
 #
 
-BLUE='\033[0;34m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m'
+if [ -t 1 ]; then
+    BLUE='\033[0;34m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    NC='\033[0m'
+fi
 
 CMD()
 {
