b5dae052050e6ecce1bcc1a2619e057c21488ff2
Javier Otero <jotero@cscs.ch>
2021-03-23 12:53:00 +0100

    Add copyright

--- cscs-checks/prgenv/hip/build_hip.py
+++ cscs-checks/prgenv/hip/build_hip.py
@@ -1,7 +1,11 @@
-import reframe as rfm
-import reframe.utility.sanity as sn
+# Copyright 2016-2021 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# ReFrame Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
 
 import os
+import reframe as rfm
+import reframe.utility.sanity as sn
 
 
 @rfm.simple_test
