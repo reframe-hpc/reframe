33829a15aea4aadff315b1a90832d17c42a91e05
Javier Otero <jotero@cscs.ch>
2021-03-23 12:51:24 +0100

    Add hello world test

--- cscs-checks/prgenv/hip/build_hip.py
+++ cscs-checks/prgenv/hip/build_hip.py
@@ -10,16 +10,18 @@ class BuildHip(rfm.RegressionTest):
 
     # HIP build variables
     hip_path = variable(str, value='hip')
+    hip_full_path = variable(str)
     hip_platform = variable(str, value='nvcc')
 
     valid_systems = ['daint:gpu', 'dom:gpu']
     valid_prog_environs = ['PrgEnv-gnu']
-    sourcesdir =  'https://github.com/ROCm-Developer-Tools/HIP.git'
+    sourcesdir = 'https://github.com/ROCm-Developer-Tools/HIP.git'
     build_system = 'CMake'
     postbuild_cmds = ['make install']
     executable = f'{hip_path}/bin/hipcc'
     executable_opts = ['--version']
     maintainers = ['JO']
+    tags = {'production'}
 
     @rfm.run_before('compile')
     def set_compile_options(self):
@@ -35,3 +37,40 @@ class BuildHip(rfm.RegressionTest):
     @rfm.run_before('sanity')
     def set_sanity_patterns(self):
         self.sanity_patterns = sn.assert_found(r'nvcc:\s+NVIDIA', self.stdout)
+
+
+@rfm.simple_test
+class HelloHip(rfm.RegressionTest):
+    '''A Hello World test for HIP.'''
+
+    sample = variable(str, value='HelloWorld')
+    sample_dir = variable(str, value='HIP-Examples-Applications')
+
+    valid_systems = ['daint:gpu', 'dom:gpu']
+    valid_prog_environs = ['PrgEnv-gnu']
+    modules = ['cdt-cuda']
+    sourcesdir = 'https://github.com/ROCm-Developer-Tools/HIP-Examples.git'
+    build_system = 'Make'
+    maintainers = ['JO']
+    tags = {'production'}
+
+    def __init__(self):
+        self.depends_on('BuildHip')
+
+    @rfm.require_deps
+    def get_hip_path(self, BuildHip):
+        self.hip_path = BuildHip().hip_full_path
+
+    @rfm.run_before('compile')
+    def set_env(self):
+        self.variables = {'HIP_PATH': f'{self.hip_path}'}
+        self.build_system.cxx = os.path.join(self.hip_path, 'bin', 'hipcc')
+        self.prebuild_cmds = [f'cd {self.sample_dir}/{self.sample}']
+
+    @rfm.run_before('run')
+    def set_executable(self):
+        self.executable = f'{self.sample_dir}/{self.sample}/{self.sample}'
+
+    @rfm.run_before('sanity')
+    def set_sanity(self):
+        self.sanity_patterns = sn.assert_found(r'HelloWorld', self.stdout)
