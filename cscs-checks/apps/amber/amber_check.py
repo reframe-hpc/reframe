387130e5efb474cdff2684b02f4f88c771f8fc76
Christopher Bignamini <christopher.bignamini@gmail.com>
2021-04-08 15:02:47 +0200

    Syntax fix

--- cscs-checks/apps/amber/amber_check.py
+++ cscs-checks/apps/amber/amber_check.py
@@ -67,7 +67,8 @@ class AmberCheck(AmberBaseCheck):
                     'perf': (30.0, -0.05, None, 'ns/day')
                 },
             }
-            self.tags |= {'health' if variant == 'prod' and scale == 'small'}
+            if variant == 'prod' and scale == 'small':
+                self.tags.add('health')
         elif arch == 'CPU':
             self.valid_systems = ['daint:mc']
             if scale == 'small':
