f6304d368d6a8475e99021449c6ca05bfcfbf4a6
Christopher Bignamini <christopher.bignamini@gmail.com>
2021-04-08 18:16:58 +0200

    Init function removed and sanity patterns definitio moved to pipeline hook

--- cscs-checks/tools/jupyter/jupyter_tests.py
+++ cscs-checks/tools/jupyter/jupyter_tests.py
@@ -9,32 +9,36 @@ import reframe.utility.sanity as sn
 
 @rfm.simple_test
 class JupyterHubSubmitTest(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.valid_systems = ['daint:jupyter_gpu', 'daint:jupyter_mc',
-                              'dom:jupyter_gpu', 'dom:jupyter_mc']
-        self.valid_prog_environs = ['*']
-        self.sourcesdir = None
-        self.executable = 'hostname'
-        self.time_limit = '1m'
-        self.max_pending_time = '7m'
+    valid_systems = ['daint:jupyter_gpu', 'daint:jupyter_mc',
+                     'dom:jupyter_gpu', 'dom:jupyter_mc']
+    valid_prog_environs = ['*']
+    sourcesdir = None
+    executable = 'hostname'
+    time_limit = '1m'
+    max_pending_time = '7m'
+    tags = {'production', 'post-maintenance', 'health'}
+    maintainers = ['RS', 'TR']
+
+    @rfm.run_before('sanity')
+    def set_sanity_check(self):
         self.sanity_patterns = sn.assert_found(r'nid\d+', self.stdout)
-        self.tags = {'production', 'post-maintenance', 'health'}
-        self.maintainers = ['RS', 'TR']
 
 
 @rfm.simple_test
 class JupyterHubServerCheck(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.descr = 'Check JupyterHub server status and version'
-        self.valid_systems = ['daint:jupyter_gpu', 'daint:jupyter_mc',
-                              'dom:jupyter_gpu', 'dom:jupyter_mc']
-        self.valid_prog_environs = ['*']
-        self.sourcesdir = None
-        self.executable = 'curl https://jupyter.cscs.ch/hub/api/'
-        self.time_limit = '30s'
-        self.num_tasks = 1
-        self.num_tasks_per_node = 1
+    descr = 'Check JupyterHub server status and version'
+    valid_systems = ['daint:jupyter_gpu', 'daint:jupyter_mc',
+                     'dom:jupyter_gpu', 'dom:jupyter_mc']
+    valid_prog_environs = ['*']
+    sourcesdir = None
+    executable = 'curl https://jupyter.cscs.ch/hub/api/'
+    time_limit = '30s'
+    num_tasks = 1
+    num_tasks_per_node = 1
+    tags = {'health'}
+    maintainers = ['CB', 'TR']
+
+    @rfm.run_before('sanity')
+    def set_sanity_check(self):
         self.sanity_patterns = sn.assert_found(r'{"version": "1.3.0"}',
                                                self.stdout)
-        self.tags = {'health'}
-        self.maintainers = ['CB', 'TR']
