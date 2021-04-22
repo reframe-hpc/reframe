07d8f11302b8a4e2c3b8a7100c71715493e8805a
Vasileios Karakasis <karakasis@cscs.ch>
2021-04-22 15:58:51 +0200

    PBS: Assume that jobs have finished in case of exitcode == 35

--- reframe/core/schedulers/pbs.py
+++ reframe/core/schedulers/pbs.py
@@ -194,9 +194,9 @@ class PbsJobScheduler(sched.JobScheduler):
         # If qstat cannot find any of the job IDs, it will return 153.
         # Otherwise, it will return with return code 0 and print information
         # only for the jobs it could find.
-        if completed.returncode == 153:
-            self.log('Return code is 153: jobids not known by scheduler, '
-                     'assuming all jobs completed')
+        if completed.returncode in (153, 35):
+            self.log(f'Return code is {completed.returncode}: '
+                     f'assuming all jobs completed')
             for job in jobs:
                 job._state = 'COMPLETED'
                 job._completed = True
