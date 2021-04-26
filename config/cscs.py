9850ec6273daa2eaafc28dfc3e12543d755f88c8
Luca <lucamar@cscs.ch>
2021-04-26 18:02:22 +0200

    Remove system Alps and set hostname alps for Eiger instead

--- config/cscs.py
+++ config/cscs.py
@@ -663,104 +663,11 @@ site_configuration = {
                 }
             ]
         },
-        {
-            'name': 'alps',
-            'descr': 'Alps Cray EX Supercomputer',
-            'hostnames': [
-                'alps'
-            ],
-            'modules_system': 'lmod',
-            'resourcesdir': '/apps/common/UES/reframe/resources',
-            'partitions': [
-                {
-                    'name': 'login',
-                    'scheduler': 'local',
-                    'environs': [
-                        'builtin',
-                        'PrgEnv-aocc',
-                        'PrgEnv-cray',
-                        'PrgEnv-gnu',
-                        'PrgEnv-intel',
-                        'cpeAMD',
-                        'cpeCray',
-                        'cpeGNU',
-                        'cpeIntel'
-                    ],
-                    'descr': 'Login nodes',
-                    'max_jobs': 4,
-                    'launcher': 'local'
-                },
-                {
-                    'name': 'eiger',
-                    'descr': 'Multicore nodes (AMD EPYC 7742, 256|512GB/cn)',
-                    'scheduler': 'slurm',
-                    'environs': [
-                        'builtin',
-                        'PrgEnv-aocc',
-                        'PrgEnv-cray',
-                        'PrgEnv-gnu',
-                        'PrgEnv-intel',
-                        'cpeAMD',
-                        'cpeCray',
-                        'cpeGNU',
-                        'cpeIntel'
-                    ],
-                    'max_jobs': 100,
-                    'resources': [
-                        {
-                            'name': 'switches',
-                            'options': [
-                                '--switches={num_switches}'
-                            ]
-                        },
-                        {
-                            'name': 'memory',
-                            'options': [
-                                '--mem={mem_per_node}'
-                            ]
-                        },
-                    ],
-                    'launcher': 'srun'
-                },
-                {
-                    'name': 'pilatus',
-                    'descr': 'Multicore nodes (AMD EPYC 7742, 256|512GB/cn)',
-                    'scheduler': 'slurm',
-                    'environs': [
-                        'builtin',
-                        'PrgEnv-aocc',
-                        'PrgEnv-cray',
-                        'PrgEnv-gnu',
-                        'PrgEnv-intel',
-                        'cpeAMD',
-                        'cpeCray',
-                        'cpeGNU',
-                        'cpeIntel'
-                    ],
-                    'max_jobs': 100,
-                    'resources': [
-                        {
-                            'name': 'switches',
-                            'options': [
-                                '--switches={num_switches}'
-                            ]
-                        },
-                        {
-                            'name': 'memory',
-                            'options': [
-                                '--mem={mem_per_node}'
-                            ]
-                        },
-                    ],
-                    'launcher': 'srun'
-                },
-            ]
-        },
-        {
+        { 
             'name': 'eiger',
             'descr': 'Alps Cray EX Supercomputer',
             'hostnames': [
-                'eiger'
+                'alps'
             ],
             'modules_system': 'lmod',
             'resourcesdir': '/apps/common/UES/reframe/resources',
