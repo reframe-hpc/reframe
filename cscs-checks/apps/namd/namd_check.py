a1f12162f9663e076e2f6099fef3e96fdd12599e
Christopher Bignamini <christopher.bignamini@gmail.com>
2021-06-10 18:05:56 +0200

    Arch parametrization added

--- cscs-checks/apps/namd/namd_check.py
+++ cscs-checks/apps/namd/namd_check.py
@@ -9,10 +9,13 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 
-class NamdBaseCheck(rfm.RunOnlyRegressionTest):
+@rfm.simple_test
+class NamdCheck(rfm.RunOnlyRegressionTest):
     scale = parameter(['small', 'large'])
     variant = parameter(['maint', 'prod'])
+    arch = parameter(['gpu', 'cpu'])
 
+    valid_prog_environs = ['builtin', 'cpeIntel']
     modules = ['NAMD']
     executable = 'namd2'
     use_multithreading = True
@@ -25,13 +28,36 @@ class NamdBaseCheck(rfm.RunOnlyRegressionTest):
         }
     }
 
-    def set_description(self, arch):
-        self.descr = f'NAMD check ({arch}, {self.variant})'
+    @rfm.run_after('init')
+    def set_description(self):
+        self.descr = f'NAMD check ({self.arch}, {self.variant})'
         self.tags |= {'maintenance' if self.variant == 'maint'
                       else 'production'}
 
     @rfm.run_after('init')
+    def set_valid_systems(self):
+        if self.arch == 'gpu':
+            self.valid_systems = ['daint:gpu']
+            if self.scale == 'small':
+                self.valid_systems += ['dom:gpu']
+        else:
+            self.valid_systems = ['daint:mc', 'eiger:mc', 'pilatus:mc']
+            if self.scale == 'small':
+                self.valid_systems += ['dom:mc']
+
+    @rfm.run_after('init')
     def set_job_configuration(self):
+        if self.arch == 'gpu':
+            self.executable_opts = ['+idlepoll', '+ppn 23', 'stmv.namd']
+            self.num_cpus_per_task = 24
+            self.num_gpus_per_node = 1
+        else:
+            # On Eiger a no-smp NAMD version is the default
+            if self.current_system.name in ['eiger', 'pilatus']:
+                self.executable_opts = ['+idlepoll', 'stmv.namd']
+            else:
+                self.executable_opts = ['+idlepoll', '+ppn 71', 'stmv.namd']
+                self.num_cpus_per_task = 72
         if self.scale == 'small':
             # On Eiger a no-smp NAMD version is the default
             if self.current_system.name in ['eiger', 'pilatus']:
@@ -51,9 +77,9 @@ class NamdBaseCheck(rfm.RunOnlyRegressionTest):
     @rfm.run_after('init')
     def set_valid_prgenv(self):
         if self.current_system.name == 'pilatus':
-            self.valid_prog_environs = ['cpeIntel']
+            self.valid_prog_environs.remove('builtin')
         else:
-            self.valid_prog_environs = ['builtin']
+            self.valid_prog_environs.remove('cpeIntel')
 
     @rfm.run_before('compile')
     def set_sources_dir(self):
@@ -81,71 +107,27 @@ class NamdBaseCheck(rfm.RunOnlyRegressionTest):
                 r's/step (?P<days_ns>\S+) days/ns \S+ MB memory',
                 self.stdout, 'days_ns', float))
         }
-
-
-@rfm.simple_test
-class NamdGPUCheck(NamdBaseCheck):
-    valid_systems = ['daint:gpu']
-    executable_opts = ['+idlepoll', '+ppn 23', 'stmv.namd']
-    num_cpus_per_task = 24
-    num_gpus_per_node = 1
-
-    @rfm.run_after('init')
-    def set_arch(self):
-        super().set_description('gpu')
-
-    @rfm.run_after('init')
-    def update_valid_systems(self):
-        if self.scale == 'small':
-            self.valid_systems += ['dom:gpu']
-
-    @rfm.run_before('sanity')
-    def set_performance_reference(self):
-        if self.scale == 'small':
-            self.reference = {
-                'dom:gpu': {'days_ns': (0.15, None, 0.05, 'days/ns')},
-                'daint:gpu': {'days_ns': (0.15, None, 0.05, 'days/ns')}
-            }
+        if self.arch == 'gpu':
+            if self.scale == 'small':
+                self.reference = {
+                    'dom:gpu': {'days_ns': (0.15, None, 0.05, 'days/ns')},
+                    'daint:gpu': {'days_ns': (0.15, None, 0.05, 'days/ns')}
+                }
+            else:
+                self.reference = {
+                    'daint:gpu': {'days_ns': (0.07, None, 0.05, 'days/ns')}
+                }
         else:
-            self.reference = {
-                'daint:gpu': {'days_ns': (0.07, None, 0.05, 'days/ns')}
-            }
-
-
-@rfm.simple_test
-class NamdCPUCheck(NamdBaseCheck):
-    valid_systems = ['daint:mc', 'eiger:mc', 'pilatus:mc']
-
-    @rfm.run_after('init')
-    def set_arch(self):
-        super().set_description('cpu')
-
-    @rfm.run_after('init')
-    def update_valid_systems(self):
-        if self.scale == 'small':
-            self.valid_systems += ['dom:mc']
-
-    @rfm.run_before('run')
-    def set_executable_options(self):
-        # On Eiger a no-smp NAMD version is the default
-        if self.current_system.name in ['eiger', 'pilatus']:
-            self.executable_opts = ['+idlepoll', 'stmv.namd']
-        else:
-            self.executable_opts = ['+idlepoll', '+ppn 71', 'stmv.namd']
-            self.num_cpus_per_task = 72
-
-    @rfm.run_before('sanity')
-    def set_performance_reference(self):
-        if self.scale == 'small':
-            self.reference = {
-                'dom:mc': {'days_ns': (0.51, None, 0.05, 'days/ns')},
-                'daint:mc': {'days_ns': (0.51, None, 0.05, 'days/ns')},
-                'eiger:mc': {'days_ns': (0.12, None, 0.05, 'days/ns')},
-                'pilatus:mc': {'days_ns': (0.12, None, 0.05, 'days/ns')},
-            }
-        else:
-            self.reference = {
-                'daint:mc': {'days_ns': (0.28, None, 0.05, 'days/ns')},
-                'eiger:mc': {'days_ns': (0.05, None, 0.05, 'days/ns')},
-                'pilatus:mc': {'days_ns': (0.05, None, 0.05, 'days/ns')}
-            }
+            if self.scale == 'small':
+                self.reference = {
+                    'dom:mc': {'days_ns': (0.51, None, 0.05, 'days/ns')},
+                    'daint:mc': {'days_ns': (0.51, None, 0.05, 'days/ns')},
+                    'eiger:mc': {'days_ns': (0.12, None, 0.05, 'days/ns')},
+                    'pilatus:mc': {'days_ns': (0.12, None, 0.05, 'days/ns')},
+                }
+            else:
+                self.reference = {
+                    'daint:mc': {'days_ns': (0.28, None, 0.05, 'days/ns')},
+                    'eiger:mc': {'days_ns': (0.05, None, 0.05, 'days/ns')},
+                    'pilatus:mc': {'days_ns': (0.05, None, 0.05, 'days/ns')}
+                }
