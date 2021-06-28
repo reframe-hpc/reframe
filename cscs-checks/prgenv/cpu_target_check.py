82b25b3756cb0c4ee3a60dea01264df76d114a91
Theofilos Manitaras <manitaras@cscs.ch>
2021-06-28 16:25:40 +0200

    Enable CPU target test for eiger:login

--- cscs-checks/prgenv/cpu_target_check.py
+++ cscs-checks/prgenv/cpu_target_check.py
@@ -10,7 +10,7 @@ import reframe.utility.sanity as sn
 @rfm.simple_test
 class CrayCPUTargetTest(rfm.RunOnlyRegressionTest):
     descr = 'Checks whether CRAY_CPU_TARGET is set'
-    valid_systems = ['daint:login', 'dom:login', 'pilatus:login']
+    valid_systems = ['daint:login', 'dom:login', 'eiger:login', 'pilatus:login']
     valid_prog_environs = ['PrgEnv-cray', 'PrgEnv-gnu', 'PrgEnv-intel',
                            'PrgEnv-pgi', 'PrgEnv-nvidia']
     sourcesdir = None
