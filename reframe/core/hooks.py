8587e211844541732d2fa41144f99c24cc9d86a0
Javier Otero <jotero@cscs.ch>
2021-07-05 12:09:03 +0200

    Bugfix hook registry update args

--- reframe/core/hooks.py
+++ reframe/core/hooks.py
@@ -170,16 +170,18 @@ class HookRegistry:
     def __getattr__(self, name):
         return getattr(self.__hooks, name)
 
-    def update(self, hooks, *, denied_hooks={}):
+    def update(self, hooks, *, denied_hooks=None):
         '''Update the hook registry with the hooks from another hook registry.
 
         The optional ``denied_hooks`` argument takes a set of disallowed
         hook names, preventing their inclusion into the current hook registry.
         '''
+        denied_hooks = denied_hooks or set()
         for phase, hks in hooks.items():
             self.__hooks.setdefault(phase, util.OrderedSet())
-            for h in (hk for hk in hks if hk.__name__ not in denied_hooks):
-                self.__hooks[phase].add(h)
+            for h in hks:
+                if h.__name__ not in denied_hooks:
+                    self.__hooks[phase].add(h)
 
     def __repr__(self):
         return repr(self.__hooks)
