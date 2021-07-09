6e4de3653229df5b9ae5bc4640cc61fb74f4c5da
Vasileios Karakasis <karakasis@cscs.ch>
2021-07-10 00:51:49 +0200

    Fix CI runner script for the tutorials
    ... and remove stale prints from the frontend.

--- reframe/frontend/cli.py
+++ reframe/frontend/cli.py
@@ -817,8 +817,6 @@ def main():
             printer.debug(dependencies.format_deps(testgraph))
             if options.restore_session is not None:
                 testgraph, restored_cases = report.restore_dangling(testgraph)
-                print(dependencies.format_deps(testgraph))
-                print(restored_cases)
 
         testcases = dependencies.toposort(
             testgraph,
