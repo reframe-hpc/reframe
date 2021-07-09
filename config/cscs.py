08c4200cd71f947c48a98b0196a22bfbf6190f0e
Luca <lucamar@cscs.ch>
2021-07-09 10:21:19 +0200

    Add account and constraint to Eiger configuration

--- config/cscs.py
+++ config/cscs.py
@@ -717,6 +717,7 @@ site_configuration = {
                         'cpeIntel'
                     ],
                     'max_jobs': 100,
+                    'access': ['-Cmc', f'--account={osext.osgroup()}'],
                     'resources': [
                         {
                             'name': 'switches',
