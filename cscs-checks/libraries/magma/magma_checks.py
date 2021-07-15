5ec121fa995f359a8fa6435f6002c09439e9bae7
Victor Holanda Rusu <victor.holanda@cscs.ch>
2021-07-15 14:56:43 +0200

    Update magma tests

--- cscs-checks/libraries/magma/magma_checks.py
+++ cscs-checks/libraries/magma/magma_checks.py
@@ -7,29 +7,36 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 
-@rfm.parameterized_test(['cblas_z'], ['zgemm'],
-                        ['zsymmetrize'], ['ztranspose'])
+@rfm.simple_test
 class MagmaCheck(rfm.RegressionTest):
-    def __init__(self, subtest):
-        self.valid_systems = ['daint:gpu', 'dom:gpu']
-        self.num_gpus_per_node = 1
-        self.sanity_patterns = sn.assert_found(r'Result = PASS', self.stdout)
+    subtest = parameter(['cblas_z', 'zgemm', 'zsymmetrize', 'ztranspose'])
+    valid_systems = ['daint:gpu', 'dom:gpu']
+    valid_prog_environs = ['builtin']
+    num_gpus_per_node = 1
+    prebuild_cmds = ['patch < patch.txt']
+    modules = ['magma']
+    maintainers = ['AJ', 'SK']
+    tags = {'scs', 'production', 'maintenance'}
 
-        self.prebuild_cmds = ['patch < patch.txt']
+    @run_after('init')
+    def set_build_system_opts(self):
         self.build_system = 'Make'
-        self.valid_prog_environs = ['builtin']
-        self.build_system.makefile = 'Makefile_%s' % subtest
+        self.build_system.makefile = 'Makefile_%s' % self.subtest
         # Compile with -O0 since with a higher level the compiler seems to
-        # optimise something away
+        # optimise stuff away
         self.build_system.cflags = ['-O0']
         self.build_system.cxxflags = ['-O0', '-std=c++11']
         self.build_system.ldflags = ['-lcusparse', '-lcublas', '-lmagma',
                                      '-lmagma_sparse']
-        self.executable = './testing_' + subtest
-        self.modules = ['magma']
-        self.maintainers = ['AJ', 'SK']
-        self.tags = {'scs', 'production', 'maintenance'}
-        if subtest == 'cblas_z':
+        self.executable = './testing_' + self.subtest
+
+    @run_after('init')
+    def set_sanity_patterns(self):
+        self.sanity_patterns = sn.assert_found(r'Result = PASS', self.stdout)
+
+    @run_after('init')
+    def set_performance_patterns(self):
+        if self.subtest == 'cblas_z':
             self.perf_patterns = {
                 'duration': sn.extractsingle(r'Duration: (\S+)',
                                              self.stdout, 1, float)
@@ -42,7 +49,7 @@ class MagmaCheck(rfm.RegressionTest):
                     'duration': (0.10, None, 1.05, 's'),
                 },
             }
-        elif subtest == 'zgemm':
+        elif self.subtest == 'zgemm':
             self.executable_opts = ['--range 1088:3136:1024']
             self.perf_patterns = {
                 'magma': sn.extractsingle(
@@ -63,7 +70,7 @@ class MagmaCheck(rfm.RegressionTest):
                     'cublas': (4269.31, -0.09, None, 'Gflop/s'),
                 }
             }
-        elif subtest == 'zsymmetrize':
+        elif self.subtest == 'zsymmetrize':
             self.perf_patterns = {
                 'gpu_perf': sn.extractsingle(r'GPU performance: (\S+)',
                                              self.stdout, 1, float),
@@ -76,7 +83,7 @@ class MagmaCheck(rfm.RegressionTest):
                     'gpu_perf': (158.3, -0.05, None, 'GB/s'),
                 }
             }
-        elif subtest == 'ztranspose':
+        elif self.subtest == 'ztranspose':
             self.perf_patterns = {
                 'gpu_perf':
                     sn.extractsingle(
@@ -92,8 +99,8 @@ class MagmaCheck(rfm.RegressionTest):
                     'gpu_perf': (498.2, -0.05, None, 'GB/s'),
                 }
             }
-        elif subtest == 'zunmbr':
-            # This test fails to compile with Magma 2.4
+        elif self.subtest == 'zunmbr':
+            # FIXME: update the test, because it fails to compile with Magma 2.4
             self.perf_patterns = {
                 'gpu_perf':
                     sn.extractsingle(
