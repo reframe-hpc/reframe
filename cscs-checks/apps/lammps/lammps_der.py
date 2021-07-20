4b5e94cf75e1d00f70b24a961015c2728afe64f9
Sergei Kliavinek <klyavinekss@gmail.com>
2021-07-20 11:59:54 +0300

    update lammps test

--- cscs-checks/apps/lammps/lammps_der.py
+++ cscs-checks/apps/lammps/lammps_der.py
@@ -12,10 +12,35 @@ from reframe.base_check import LAMMPSBaseCheck
 
 REFERENCE_ENERGY = {
     # every system has a different reference energy and drift
-    'small': (-4.6195, 6.0E-04),
-    'large': (-4.6195, 6.0E-04)
+    'prod': (-4.6195, 6.0E-04),
+    'maint': (-4.6195, 6.0E-04)
 }
 
+dom_gpu_small = {
+    'maint': (3457, -0.10, None, 'timesteps/s'),
+    'prod': (3132, -0.05, None, 'timesteps/s'),
+}
+
+daint_gpu_small = {
+    'maint': (2524, -0.10, None, 'timesteps/s'),
+    'prod': (2400, -0.40, None, 'timesteps/s'),
+}
+
+REFERENCE_GPU_PERFORMANCE_SMALL = {
+        'dom:gpu': dom_gpu_small,
+        'daint:gpu': daint_gpu_small,
+}
+
+
+daint_gpu_large = {
+    'maint': (3832, -0.05, None, 'timesteps/s'),
+    'prod': (3260, -0.50, None, 'timesteps/s'),
+}
+
+REFERENCE_GPU_PERFORMANCE_LARGE = {
+        'daint:gpu': daint_gpu_large,
+}
+'''
 REFERENCE_GPU_PERFORMANCE = {
     'maint': {
         'small': {
@@ -36,7 +61,52 @@ REFERENCE_GPU_PERFORMANCE = {
         }
     },
 }
+'''
 
+dom_cpu_small = {
+    'prod': (4394, -0.05, None, 'timesteps/s'),
+}
+
+daint_cpu_small = {
+    'prod': (3824, -0.10, None, 'timesteps/s'),
+}
+
+eiger_cpu_small = {
+    'prod': (4500, -0.10, None, 'timesteps/s'),
+}
+
+pilatus_cpu_small = {
+    'prod': (5000, -0.10, None, 'timesteps/s'),
+}
+
+REFERENCE_CPU_PERFORMANCE_SMALL = {
+    'dom:mc': dom_cpu_small,
+    'daint:mc': daint_cpu_small,
+    'eiger:mc': eiger_cpu_small,
+    'pilatus:mc': pilatus_cpu_small
+
+}
+
+daint_cpu_large = {
+    'prod': (5310, -0.65, None, 'timesteps/s'),
+}
+
+eiger_cpu_large = {
+    'prod': (6500, -0.10, None, 'timesteps/s'),
+}
+
+pilatus_cpu_large = {
+    'prod': (7500, -0.10, None, 'timesteps/s'),
+}
+
+REFERENCE_CPU_PERFORMANCE_LARGE = {
+    'daint:mc': daint_cpu_large,
+    'eiger:mc': eiger_cpu_large,
+    'pilatus:mc': pilatus_cpu_large,
+
+}
+
+'''
 REFERENCE_CPU_PERFORMANCE = {
     'prod': {
         'small': {
@@ -52,7 +122,7 @@ REFERENCE_CPU_PERFORMANCE = {
         }
     },
 }
-
+'''
 
 class LAMMPSCheck(LAMMPSBaseCheck):
         strict_check = False
@@ -79,27 +149,32 @@ class LAMMPSCheck(LAMMPSBaseCheck):
                     self.valid_prog_environs = ['builtin']
 
         @run_after('init')
-        def set_ref_tags(self):
-            self.reference = self.references[self.variant][self.benchmark]
-            self.tags |= {'maintenance' if self.variant == 'maint' else 'production'}
+        def set_tags(self):
+            self.tags |= {'maintenance' if self.benchmark == 'maint' else 'production'}
 
 
 @rfm.simple_test
 class LAMMPSGPUCheck(LAMMPSCheck):
-        benchmark = parameter(['small', 'large'])
-        variant = parameter(['prod', 'maint'])
+        scale = parameter(['small', 'large'])
+        benchmark = parameter(['prod', 'maint'])
         valid_systems = ['daint:gpu']
         executable = 'lmp_mpi'
         input_file = 'in.lj.gpu'
         executable_opts = ['-sf gpu', '-pk gpu 1', '-in', input_file]
         variables = {'CRAY_CUDA_MPS': '1'}
         num_gpus_per_node = 1
-        references = REFERENCE_GPU_PERFORMANCE
         ener_ref = REFERENCE_ENERGY
 
         @run_after('init')
+        def set_reference(self):
+            if self.scale == 'small':
+                self.reference = REFERENCE_GPU_PERFORMANCE_SMALL
+            else:
+                self.reference = REFERENCE_GPU_PERFORMANCE_LARGE
+
+        @run_after('init')
         def set_num_tasks(self):
-                if self.benchmark == 'small':
+                if self.scale == 'small':
                     self.valid_systems += ['dom:gpu']
                     self.num_tasks = 12
                     self.num_tasks_per_node = 2
@@ -109,16 +184,22 @@ class LAMMPSGPUCheck(LAMMPSCheck):
 
 @rfm.simple_test
 class LAMMPSCPUCheck(LAMMPSCheck):
-        variant = parameter(['prod'])
+        scale = parameter(['small', 'large'])
+        benchmark = parameter(['prod'])
         valid_systems = ['daint:mc', 'eiger:mc', 'pilatus:mc']
-        benchmark = parameter(['small', 'large'])
         input_file = 'in.lj.cpu'
-        references = REFERENCE_CPU_PERFORMANCE
         ener_ref = REFERENCE_ENERGY
 
         @run_after('init')
+        def set_reference(self):
+            if self.scale == 'small':
+                self.reference = REFERENCE_CPU_PERFORMANCE_SMALL
+            else:
+                self.reference = REFERENCE_CPU_PERFORMANCE_LARGE
+
+        @run_after('init')
         def set_num_tasks(self):
-            if self.benchmark == 'small':
+            if self.scale == 'small':
                 self.valid_systems += ['dom:mc']
                 self.num_tasks = 216
                 self.num_tasks_per_node = 36
