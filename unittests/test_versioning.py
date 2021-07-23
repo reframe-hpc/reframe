9bb8a62e03e7e84cc36cea78ce1740bf21ba1878
Vasileios Karakasis <karakasis@cscs.ch>
2021-07-23 22:13:58 +0200

    Remove support for old version strings

--- unittests/test_versioning.py
+++ unittests/test_versioning.py
@@ -42,20 +42,3 @@ def test_version_validation():
 
     with pytest.raises(ValueError):
         versioning.VersionValidator('>1')
-
-
-def test_parse(monkeypatch):
-    monkeypatch.setattr(warnings, '_RAISE_DEPRECATION_ALWAYS', True)
-    with pytest.warns(warnings.ReframeDeprecationWarning,
-                      match="please use the conformant '3.5.0'"):
-        deprecated = versioning.parse('3.5')
-
-    assert deprecated == versioning.parse('3.5.0')
-    with pytest.warns(warnings.ReframeDeprecationWarning,
-                      match="please use the conformant '3.5.0-dev.0'"):
-        deprecated = versioning.parse('3.5-dev0')
-
-    assert deprecated == versioning.parse('3.5.0-dev.0')
-    assert str(versioning.parse('3.5.0')) == '3.5.0'
-    assert str(versioning.parse('3.5.0-dev.1')) == '3.5.0-dev.1'
-    assert str(versioning.parse('3.5.0-dev.1+HASH')) == '3.5.0-dev.1+HASH'
