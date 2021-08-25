60fd1a49fcd13d87b06043bdf19ea763bd33d526
mahendrapaipuri <mahendra.paipuri@inria.fr>
2021-08-25 13:25:43 +0200

    Add configurable options to generate CI function

--- reframe/frontend/ci.py
+++ reframe/frontend/ci.py
@@ -3,7 +3,9 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+import os
 import sys
+import copy
 import yaml
 
 import reframe.core.exceptions as errors
@@ -18,6 +20,19 @@ def _emit_gitlab_pipeline(testcases):
     prefix = 'rfm-stage/${CI_COMMIT_SHORT_SHA}'
     checkpath = config.get('general/0/check_search_path')
     recurse = config.get('general/0/check_search_recursive')
+    verbosity = 'v' * int(config.get('general/0/verbose'))
+
+    # Collect the generate CI options
+    before_script = config.get('generate-ci/0/before_script')
+    after_script = config.get('generate-ci/0/after_script')
+    artifacts = config.get('generate-ci/0/artifacts')
+    artifacts_expiry = config.get('generate-ci/0/artifacts_expiry')
+
+    # Need to append prefix to artifacts
+    if artifacts:
+        artifacts = [os.path.join(prefix, a) for a in artifacts]
+    else:
+        artifacts = []
 
     def rfm_command(testcase):
         if config.filename != '<builtin>':
@@ -25,7 +40,6 @@ def _emit_gitlab_pipeline(testcases):
         else:
             config_opt = ''
 
-        report_file = f'{testcase.check.name}-report.json'
         if testcase.level:
             restore_files = ','.join(
                 f'{t.check.name}-report.json' for t in tc.deps
@@ -38,9 +52,11 @@ def _emit_gitlab_pipeline(testcases):
             f'--prefix={prefix}', config_opt,
             f'{" ".join("-c " + c for c in checkpath)}',
             f'-R' if recurse else '',
-            f'--report-file={report_file}',
+            f'--report-file={testcase.check.name}-report.json',
+            f'--report-junit={testcase.check.name}-report.xml',
             f'--restore-session={restore_files}' if restore_files else '',
-            '-n', testcase.check.name, '-r'
+            f'{"".join("-" + verbosity)}' if verbosity else '',
+            '-n', f'{testcase.check.name}$', '-r',  # regex $ to indicate end
         ])
 
     max_level = 0   # We need the maximum level to generate the stages section
@@ -51,16 +67,46 @@ def _emit_gitlab_pipeline(testcases):
         },
         'stages': []
     }
+
+    # Name of the image used for CI. If user does not explicitly provide
+    # image keyword on the top of CI script, this variable does not exist
+    image_name = os.getenv('CI_JOB_IMAGE')
+    if image_name:
+        json = {
+            'image': image_name,
+            **json,
+        }
+
     for tc in testcases:
         json[f'{tc.check.name}'] = {
             'stage': f'rfm-stage-{tc.level}',
+            'before_script': copy.deepcopy(before_script),
             'script': [rfm_command(tc)],
+            'after_script': copy.deepcopy(after_script),
             'artifacts': {
-                'paths': [f'{tc.check.name}-report.json']
+                'paths': [f'{tc.check.name}-report.json',
+                          f'{tc.check.name}-report.xml'] + artifacts,
+                'expire_in': artifacts_expiry,
             },
             'needs': [t.check.name for t in tc.deps]
         }
         max_level = max(max_level, tc.level)
+
+    # Add a last job that gathers all artifacts from precedent jobs
+    json['GatherArtifacts'] = {
+        'stage': f'rfm-stage-{max_level + 1}',
+        'script': 'echo "Gathering artifacts from all jobs..."',
+        'needs': [tc.check.name for tc in testcases],
+        'artifacts': {
+            'paths': [f'{tc.check.name}-report.json' for tc in testcases] +
+            [f'{tc.check.name}-report.xml' for tc in testcases] +
+            artifacts,
+            'expire_in': artifacts_expiry,
+        },
+    }
+
+    # We incremented one stage by above gather artifacts job
+    max_level += 1
 
     json['stages'] = [f'rfm-stage-{m}' for m in range(max_level+1)]
     return json
