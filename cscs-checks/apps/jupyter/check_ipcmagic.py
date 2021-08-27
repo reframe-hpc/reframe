1022be5a207b65e167937816375f686d2a860363
Theofilos Manitaras <manitaras@cscs.ch>
2021-08-27 16:09:59 +0200

    Modernize Jupyter IPCMagic test

--- cscs-checks/apps/jupyter/check_ipcmagic.py
+++ cscs-checks/apps/jupyter/check_ipcmagic.py
@@ -11,54 +11,63 @@ from reframe.core.backends import getlauncher
 
 @rfm.simple_test
 class IPCMagicCheck(rfm.RunOnlyRegressionTest):
-    def __init__(self):
-        self.descr = 'Distributed training with TensorFlow using ipyparallel'
-        self.valid_systems = ['daint:gpu', 'dom:gpu']
-        self.valid_prog_environs = ['PrgEnv-gnu']
-        self.modules = [
-            f'ipcmagic',
-            f'Horovod/0.21.0-CrayGNU-{osext.cray_cdt_version()}-tf-2.4.0'
-        ]
-        self.num_tasks = 2
-        self.num_tasks_per_node = 1
-        self.executable = 'ipython'
-        self.executable_opts = ['tf-hvd-sgd-ipc-tf2.py']
-        nids = sn.extractall(r'nid(?P<nid>\d+)',
-                             self.stdout, 'nid', str)
-        self.sanity_patterns = sn.all([
-            sn.assert_ne(nids, []),
-            sn.assert_ne(nids[0], nids[1])
-        ])
-        self.reference = {
-            'daint:gpu': {
-                'slope': (2.0, -0.1, 0.1, None),
-                'offset': (0.0, -0.1, 0.1, None),
-                'retries': (0, None, None, None),
-                'time': (10, None, None, 's'),
-            },
-            'dom:gpu': {
-                'slope': (2.0, -0.1, 0.1, None),
-                'offset': (0.0, -0.1, 0.1, None),
-                'retries': (0, None, None, None),
-                'time': (10, None, None, 's'),
-            }
+    descr = 'Distributed training with TensorFlow using ipyparallel'
+    valid_systems = ['daint:gpu', 'dom:gpu']
+    valid_prog_environs = ['PrgEnv-gnu']
+    modules = [
+        f'ipcmagic', f'jupyterlab',
+        f'Horovod/0.21.0-CrayGNU-{osext.cray_cdt_version()}-tf-2.4.0'
+    ]
+    num_tasks = 2
+    num_tasks_per_node = 1
+    executable = 'ipython'
+    executable_opts = ['tf-hvd-sgd-ipc-tf2.py']
+    reference = {
+        'daint:gpu': {
+            'slope': (2.0, -0.1, 0.1, None),
+            'offset': (0.0, -0.1, 0.1, None),
+            'retries': (0, None, None, None),
+            'time': (10, None, None, 's'),
+        },
+        'dom:gpu': {
+            'slope': (2.0, -0.1, 0.1, None),
+            'offset': (0.0, -0.1, 0.1, None),
+            'retries': (0, None, None, None),
+            'time': (10, None, None, 's'),
         }
-        self.perf_patterns = {
-            'slope': sn.extractsingle(r'slope=(?P<slope>\S+)',
-                                      self.stdout, 'slope', float),
-            'offset': sn.extractsingle(r'offset=(?P<offset>\S+)',
-                                       self.stdout, 'offset', float),
-            'retries': 4 - sn.count(sn.findall(r'IPCluster is already running',
-                                               self.stdout)),
-            'time': sn.extractsingle(r'IPCluster is ready\!\s+'
-                                     r'\((?P<time>\d+) seconds\)',
-                                     self.stdout, 'time', float)
-        }
-        self.maintainers = ['RS', 'TR']
-        self.tags = {'production'}
+    }
+
+    maintainers = ['RS', 'TR']
+    tags = {'production'}
+
+    @sanity_function
+    def assert_nids(self):
+        nids = sn.extractall(r'nid(?P<nid>\d+)', self.stdout, 'nid', str)
+        return sn.all([sn.assert_ne(nids, []), sn.assert_ne(nids[0], nids[1])])
+
+    @performance_function('')
+    def slope(self):
+        return sn.extractsingle(r'slope=(?P<slope>\S+)', self.stdout,
+                                'slope', float)
+
+    @performance_function('')
+    def offset(self):
+        return sn.extractsingle(r'offset=(?P<offset>\S+)', self.stdout,
+                                'offset', float)
+
+    @performance_function('')
+    def retries(self):
+        return 4 - sn.count(sn.findall(r'IPCluster is already running',
+                                       self.stdout))
+
+    @performance_function('s')
+    def time(self):
+        return sn.extractsingle(r'IPCluster is ready\!\s+'
+                                r'\((?P<time>\d+) seconds\)',
+                                self.stdout, 'time', float)
 
     @run_before('run')
-    def prepare_run(self):
+    def set_local_launcher(self):
         # Change the job launcher since `ipython`
         # needs to be launched without `srun`.
         self.job.launcher = getlauncher('local')()
