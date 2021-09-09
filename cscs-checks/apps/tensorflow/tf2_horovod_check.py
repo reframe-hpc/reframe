3c97aa1e9a6aba563edf004a6ffef751d8b56c6c
Sergei Kliavinek <klyavinekss@gmail.com>
2021-09-09 17:47:13 +0300

    add redesigned tensorflow tests

--- cscs-checks/apps/tensorflow/tf2_horovod_check.py
+++ cscs-checks/apps/tensorflow/tf2_horovod_check.py
@@ -4,80 +4,67 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import reframe as rfm
-import reframe.utility.sanity as sn
 import reframe.utility.osext as osext
+from hpctestlib.apps.tensorflow.base_check import TensorFlow2Horovod_BaseTest
+
+
+REFERENCE_SMALL_PERFOMANCE = {
+    'dom:gpu': {
+        'throughput': (1712, -0.05, None, 'images/s'),
+        'throughput_per_gpu': (214, -0.05, None, 'images/s'),
+    },
+    'daint:gpu': {
+        'throughput': (1712, -0.05, None, 'images/s'),
+        'throughput_per_gpu': (214, -0.05, None, 'images/s')
+    },
+}
+
+REFERENCE_LARGE_PERFOMANCE = {
+    'daint:gpu': {
+        'throughput': (6848, -0.05, None, 'images/s'),
+        'throughput_per_gpu': (214, -0.05, None, 'images/s')
+    },
+}
 
 
 @rfm.simple_test
-class TensorFlow2HorovodTest(rfm.RunOnlyRegressionTest):
+class TensorFlow2HorovodTestCSCS(TensorFlow2Horovod_BaseTest):
     variant = parameter(['small', 'large'])
+    sourcesdir = None
+    num_tasks_per_node = 1
+    num_cpus_per_task = 12
+    batch_size = 64
+    tags = {'production'}
+    maintainers = ['RS', 'TR']
+    valid_prog_environs = ['builtin']
+    valid_systems = ['daint:gpu']
+    modules = [
+        f'Horovod/0.21.0-CrayGNU-{osext.cray_cdt_version()}-tf-2.4.0'
+    ]
 
-    def __init__(self):
-        self.descr = 'Distributed training with TensorFlow2 and Horovod'
-        self.valid_systems = ['daint:gpu']
-        self.valid_prog_environs = ['builtin']
-        self.modules = [
-            f'Horovod/0.21.0-CrayGNU-{osext.cray_cdt_version()}-tf-2.4.0'
-        ]
-        self.sourcesdir = None
-        self.num_tasks_per_node = 1
-        self.num_cpus_per_task = 12
+    @run_after('init')
+    def set_num_task(self):
         if self.variant == 'small':
             self.valid_systems += ['dom:gpu']
             self.num_tasks = 8
-            self.reference = {
-                'dom:gpu': {
-                    'throughput': (1712, -0.05, None, 'images/s'),
-                    'throughput_per_gpu': (214, -0.05, None, 'images/s'),
-                },
-                'daint:gpu': {
-                    'throughput': (1712, -0.05, None, 'images/s'),
-                    'throughput_per_gpu': (214, -0.05, None, 'images/s')
-                },
-            }
+            self.reference = REFERENCE_SMALL_PERFOMANCE
         else:
             self.num_tasks = 32
-            self.reference = {
-                'daint:gpu': {
-                    'throughput': (6848, -0.05, None, 'images/s'),
-                    'throughput_per_gpu': (214, -0.05, None, 'images/s')
-                },
-            }
-        self.perf_patterns = {
-            'throughput': sn.extractsingle(
-                rf'Total img/sec on {self.num_tasks} GPU\(s\): '
-                rf'(?P<throughput>\S+) \S+',
-                self.stdout, 'throughput', float),
-            'throughput_per_gpu': sn.extractsingle(
-                r'Img/sec per GPU: (?P<throughput_per_gpu>\S+) \S+',
-                self.stdout, 'throughput_per_gpu', float)
-        }
-        model = 'InceptionV3'
-        batch_size = 64
-        self.sanity_patterns = sn.all([
-            sn.assert_found(rf'Model: {model}', self.stdout),
-            sn.assert_found(rf'Batch size: {batch_size}', self.stdout)
-        ])
+            self.reference = REFERENCE_LARGE_PERFOMANCE
+
+    @run_after('init')
+    def set_executable_opts(self):
         self.variables = {
             'NCCL_DEBUG': 'INFO',
             'NCCL_IB_HCA': 'ipogif0',
             'NCCL_IB_CUDA_SUPPORT': '1',
             'OMP_NUM_THREADS': '$SLURM_CPUS_PER_TASK',
         }
-        script = 'tensorflow2_synthetic_benchmark.py'
-        self.prerun_cmds = ['wget https://raw.githubusercontent.com/horovod/'
-                            'horovod/842d1075e8440f15e84364f494645c28bf20c3ae/'
-                            'examples/tensorflow2_synthetic_benchmark.py',
-                            'sed -i "s/weights=None/weights=None, '
-                            f'input_shape=(224, 224, 3)/g" {script}']
-        self.executable = 'python'
         self.executable_opts = [
-            f'{script}',
-            f'--model {model}',
-            f'--batch-size {batch_size}',
+            f'{self.script}',
+            f'--model {self.model}',
+            f'--batch-size {self.batch_size}',
             '--num-iters 5',
             '--num-batches-per-iter 5',
             '--num-warmup-batches 5',
         ]
-        self.tags = {'production'}
-        self.maintainers = ['RS', 'TR']
