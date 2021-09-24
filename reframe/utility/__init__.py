7eb4b205274d071b03f9a16903340a395c1192b1
Rafael Sarmiento <rafael.sarmiento@cscs.ch>
2021-09-24 16:23:09 +0200

    use __file__ for reframe root reference

--- reframe/utility/__init__.py
+++ reframe/utility/__init__.py
@@ -81,7 +81,10 @@ def import_module_from_file(filename, force=False):
         filename = os.path.join(filename, '__init__.py')
 
     # Express filename relative to reframe
-    rel_filename = os.path.relpath(filename, sys.path[0])
+    rfm_root = os.path.realpath(os.path.join(os.path.dirname(__file__),
+                                '../..'))
+    rel_filename = os.path.relpath(filename, rfm_root)
+    # rel_filename = os.path.relpath(filename, __file__)
     module_name = _get_module_name(rel_filename)
     if rel_filename.startswith('..'):
         # We cannot use the standard Python import mechanism here, because the
