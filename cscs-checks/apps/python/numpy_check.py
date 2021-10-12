e56abd5da5e078a781a578eb7a0b20a723aaa8b7
Theofilos Manitaras <manitaras@cscs.ch>
2021-10-12 14:41:23 +0200

    Address PR comments

--- cscs-checks/apps/python/numpy_check.py
+++ cscs-checks/apps/python/numpy_check.py
@@ -5,55 +5,46 @@
 
 import reframe as rfm
 
-from hpctestlib.apps.python.numpy.base_check import Numpy_BaseCheck
-
-
-REFERENCE_PERFOMANCE = {
-    'daint:gpu': {
-        'dot': (0.4, None, 0.05, 'seconds'),
-        'svd': (0.37, None, 0.05, 'seconds'),
-        'cholesky': (0.12, None, 0.05, 'seconds'),
-        'eigendec': (3.5, None, 0.05, 'seconds'),
-        'inv': (0.21, None, 0.05, 'seconds'),
-    },
-    'daint:mc': {
-        'dot': (0.3, None, 0.05, 'seconds'),
-        'svd': (0.35, None, 0.05, 'seconds'),
-        'cholesky': (0.1, None, 0.05, 'seconds'),
-        'eigendec': (4.14, None, 0.05, 'seconds'),
-        'inv': (0.16, None, 0.05, 'seconds'),
-    },
-    'dom:gpu': {
-        'dot': (0.4, None, 0.05, 'seconds'),
-        'svd': (0.37, None, 0.05, 'seconds'),
-        'cholesky': (0.12, None, 0.05, 'seconds'),
-        'eigendec': (3.5, None, 0.05, 'seconds'),
-        'inv': (0.21, None, 0.05, 'seconds'),
-    },
-    'dom:mc': {
-        'dot': (0.3, None, 0.05, 'seconds'),
-        'svd': (0.35, None, 0.05, 'seconds'),
-        'cholesky': (0.1, None, 0.05, 'seconds'),
-        'eigendec': (4.14, None, 0.05, 'seconds'),
-        'inv': (0.16, None, 0.05, 'seconds'),
-    },
-}
+from hpctestlib.apps.python.numpy.numpy_ops import numpy_ops_check
 
 
 @rfm.simple_test
-class Numpy_TestCSCS(Numpy_BaseCheck):
+class cscs_numpy_test(numpy_ops_check):
     valid_prog_environs = ['builtin']
     valid_systems = ['daint:gpu', 'daint:mc', 'dom:gpu', 'dom:mc']
     modules = ['numpy']
-    variables = {
-        'OMP_NUM_THREADS': '$SLURM_CPUS_PER_TASK',
-    }
     num_tasks_per_node = 1
     use_multithreading = False
-    reference = REFERENCE_PERFOMANCE
+    all_ref = {
+        'haswell': {
+            'dot': (0.4, None, 0.05, 'seconds'),
+            'svd': (0.37, None, 0.05, 'seconds'),
+            'cholesky': (0.12, None, 0.05, 'seconds'),
+            'eigendec': (3.5, None, 0.05, 'seconds'),
+            'inv': (0.21, None, 0.05, 'seconds'),
+        },
+        'broadwell': {
+            'dot': (0.3, None, 0.05, 'seconds'),
+            'svd': (0.35, None, 0.05, 'seconds'),
+            'cholesky': (0.1, None, 0.05, 'seconds'),
+            'eigendec': (4.14, None, 0.05, 'seconds'),
+            'inv': (0.16, None, 0.05, 'seconds'),
+        }
+    }
     tags = {'production'}
     maintainers = ['RS', 'TR']
 
     @run_after('setup')
     def set_num_cpus_per_task(self):
         self.num_cpus_per_task = self.current_partition.processor.num_cores
+        variables = {
+            'OMP_NUM_THREADS': self.num_cpus_per_task
+        }
+
+    @run_before('performance')
+    def set_perf_ref(self):
+        arch = self.current_partition.processor.arch
+        pname = self.current_partition.fullname
+        self.reference = {
+            pname: self.all_ref[arch]
+        }
--- hpctestlib/apps/python/numpy/numpy_ops.py
+++ hpctestlib/apps/python/numpy/numpy_ops.py
@@ -7,59 +7,68 @@ import reframe as rfm
 import reframe.utility.sanity as sn
 
 
-class Numpy_BaseCheck(rfm.RunOnlyRegressionTest, pin_prefix=True):
-    '''Base class for the NumPy Test.
-    NumPy is the fundamental package for scientific computing in Python.
-    It is a Python library that provides a multidimensional array object,
-    various derived objects (such as masked arrays and matrices), and an
-    assortment of routines for fast operations on arrays, including
-    mathematical, logical, shape manipulation, sorting, selecting, I/O,
-    discrete Fourier transforms, basic linear algebra, basic statistical
-    operations, random simulation and much more (see numpy.org).
+class numpy_ops_check(rfm.RunOnlyRegressionTest, pin_prefix=True):
+    '''NumPy basic operations test.
 
-    The presented abstract run-only class checks the numpy perfomance.
-    This test checks whether some basic operations (such as matrix product,
-    SVD decomposition, Cholesky decomposition, eigendecomposition, and
-    inverse matrix calculation) are performed, and also checks the
-    execution time of these operations. The default assumption is that
-    NumPy is already installed on the device under test.
+    `NumPy <https://numpy.org/>`__ is the fundamental package for scientific
+    computing in Python.
+    It provides a multidimensional array object, various derived objects
+    (such as masked arrays and matrices), and an assortment of routines
+    for fast operations on arrays, including mathematical, logical, shape
+    manipulation, sorting, selecting, I/O, discrete Fourier transforms,
+    basic linear algebra, basic statistical operations, random simulation
+    and much more.
+
+    This test test performs some fundamental NumPy linear algebra operations
+    (matrix product, SVD, Cholesky decomposition, eigendecomposition, and
+    inverse matrix calculation) and users the execution time as a performance
+    metric. The default assumption is that NumPy is already installed on the
+    currest system.
     '''
 
+    #: See :attr:`~reframe.core.pipeline.RegressionTest.executable`.
+    #:
+    #: :required: No
     executable = 'python'
+
+    #: See :attr:`~reframe.core.pipeline.RegressionTest.executable_opts`.
+    #:
+    #: :required: No
     executable_opts = ['np_ops.py']
 
-    @run_after('init')
-    def set_description(self):
-        self.mydescr = 'Test a few typical numpy operations'
+    #: See :attr:`~reframe.core.pipeline.RegressionTest.descr`.
+    #:
+    #: :required: No
+    descr = 'Test NumPy operations: dot, svd, cholesky, eigen and inv'
 
     @performance_function('seconds', perf_key='dot')
-    def set_perf_dot(self):
+    def time_dot(self):
         return sn.extractsingle(
             r'^Dotted two 4096x4096 matrices in\s+(?P<dot>\S+)\s+s',
             self.stdout, 'dot', float)
 
     @performance_function('seconds', perf_key='svd')
-    def set_perf_svd(self):
+    def time_svd(self):
         return sn.extractsingle(
             r'^SVD of a 2048x1024 matrix in\s+(?P<svd>\S+)\s+s',
             self.stdout, 'svd', float)
 
     @performance_function('seconds', perf_key='cholesky')
-    def set_perf_cholesky(self):
+    def time_cholesky(self):
         return sn.extractsingle(
             r'^Cholesky decomposition of a 2048x2048 matrix in'
             r'\s+(?P<cholesky>\S+)\s+s',
             self.stdout, 'cholesky', float)
 
     @performance_function('seconds', perf_key='eigendec')
-    def set_perf_eigendec(self):
+    def time_eigendec(self):
         return sn.extractsingle(
             r'^Eigendecomposition of a 2048x2048 matrix in'
             r'\s+(?P<eigendec>\S+)\s+s',
             self.stdout, 'eigendec', float)
 
     @performance_function('seconds', perf_key='inv')
-    def set_perf_inv(self):
+    def time_inv(self):
         return sn.extractsingle(
             r'^Inversion of a 2048x2048 matrix in\s+(?P<inv>\S+)\s+s',
             self.stdout, 'inv', float)
