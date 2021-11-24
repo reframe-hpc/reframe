b75f5b914ce9a6c57c304f5f526aa8860ee23cae
Vasileios Karakasis <karakasis@cscs.ch>
2021-11-24 21:17:06 +0100

    Fix hook execution order in the same class

--- reframe/core/pipeline.py
+++ reframe/core/pipeline.py
@@ -197,15 +197,11 @@ class RegressionTest(RegressionMixin, jsonext.JSONSerializable):
 
     @classmethod
     def pipeline_hooks(cls):
-        # The hook registry stores hooks in MRO order, but we want to attach
-        # the hooks in reverse order so that the more specialized hooks (those
-        # at the beginning of the MRO list) will be executed last
-
         ret = {}
         for hook in cls._rfm_hook_registry:
             for stage in hook.stages:
                 try:
-                    ret[stage].insert(0, hook.fn)
+                    ret[stage].append(hook.fn)
                 except KeyError:
                     ret[stage] = [hook.fn]
 
