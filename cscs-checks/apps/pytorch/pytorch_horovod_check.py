6cf6423be21f03d86a501917cf2061ceb4ca71f4
rafael <rafael.sarmiento@cscs.ch>
2021-11-30 14:59:28 +0100

    fix comments

--- cscs-checks/apps/pytorch/pytorch_horovod_check.py
+++ cscs-checks/apps/pytorch/pytorch_horovod_check.py
@@ -12,7 +12,6 @@ from hpctestlib.ml.pytorch.horovod import pytorch_cnn_check
 @rfm.simple_test
 class cscs_pytorch_horovod_check(pytorch_cnn_check):
     num_nodes = parameter([1, 8, 32])
-    model_name = parameter(['inception_v3', 'resnet50'])
     num_tasks_per_node = 1
     batch_size = 64
     valid_systems = ['daint:gpu']
@@ -20,6 +19,17 @@ class cscs_pytorch_horovod_check(pytorch_cnn_check):
     modules = ['PyTorch']
     tags |= {'production'}
     maintainers = ['sarafael', 'henrique']
+    allref = {
+        'sm_60': {
+            'inception_v3': {
+                'throughput_per_gpu': (131, -0.05, None, 'images/s'),
+            },
+            'resnet50': {
+                'throughput_per_gpu': (201, -0.05, None, 'images/s'),
+            }
+        }
+    }
+    model_name = parameter(allref['sm_60'].keys())
 
     @run_after('init')
     def setup_filtering_criteria(self):
@@ -39,16 +49,12 @@ class cscs_pytorch_horovod_check(pytorch_cnn_check):
             'NCCL_IB_CUDA_SUPPORT': '1',
             'OMP_NUM_THREADS': str(self.num_cpus_per_task)
         }
-        ref_per_gpu_sm60 = 131 if self.model == 'inception_v3' else 201
-        ref_total_sm60 = ref_per_gpu_sm60 * self.num_nodes
-        allref = {
-            'sm_60': {
-                'throughput_total': (ref_total_sm60, -0.05, None, 'images/s'),
-                'throughput_iteration': (ref_per_gpu_sm60, -0.05, None,
-                                         'images/s')
-            }
-        }
+        ref_per_gpu = self.allref['sm_60'][self.model]['throughput_per_gpu'][0]
+        ref_total = ref_per_gpu * self.num_nodes
         with contextlib.suppress(KeyError):
             self.reference = {
-                '*': allref[self.num_nodes]['sm_60']
+                '*': {
+                    **self.allref['sm_60'][self.model],
+                    'throughput_total': (ref_total, -0.05, None, 'images/s'),
+                }
             }
