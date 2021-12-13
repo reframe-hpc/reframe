8d06cf8688c5301db408751246bb6f2adc2e01a5
Vasileios Karakasis <karakasis@cscs.ch>
2021-12-14 00:35:43 +0100

    Fix unit tests for Python 3.6

--- reframe/core/logging.py
+++ reframe/core/logging.py
@@ -470,9 +470,11 @@ class Logger(logging.Logger):
     def setLevel(self, level):
         self.level = _check_level(level)
 
-        # Clear the internal cache of the base logger, otherwise the logger
-        # will remain disabled if its level is raised and then lowered again
-        self._cache.clear()
+        if sys.version_info[:2] >= (3, 7):
+            # Clear the internal cache of the base logger, otherwise the
+            # logger will remain disabled if its level is raised and then
+            # lowered again
+            self._cache.clear()
 
     def makeRecord(self, name, level, fn, lno, msg, args, exc_info,
                    func=None, extra=None, sinfo=None):
