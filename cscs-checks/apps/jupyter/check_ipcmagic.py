8922f29a8789dd23a0fcfbdc7c9a2caf1c92f0e1
Luca <lucamar@cscs.ch>
2022-01-12 17:15:55 +0100

    Fix conflict on Piz Daint

--- cscs-checks/apps/jupyter/check_ipcmagic.py
+++ cscs-checks/apps/jupyter/check_ipcmagic.py
@@ -12,7 +12,14 @@ from hpctestlib.interactive.jupyter.ipcmagic import ipcmagic_check
 class cscs_ipcmagic_check(ipcmagic_check):
     valid_systems = ['daint:gpu', 'dom:gpu']
     valid_prog_environs = ['builtin']
-    modules = ['JuliaExtensions', 'jupyterlab', 'Horovod']
+
+    @run_after('init')
+    def set_modules(self):
+        if self.current_system.name == 'dom':
+            self.modules = ['Horovod', 'JuliaExtensions', 'jupyterlab']
+        else:
+            self.modules = ['Horovod', 'jupyterlab']
+
     maintainers = ['RS', 'TR']
     tags = {'production'}
     reference = {
