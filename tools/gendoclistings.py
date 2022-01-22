76e0266728032b9ccd0758b918beddc3d9f4b07a
Vasileios Karakasis <karakasis@cscs.ch>
2022-01-22 20:29:53 +0100

    Update tutorial listings

--- tools/gendoclistings.py
+++ tools/gendoclistings.py
@@ -15,8 +15,7 @@ def print_usage():
 
 ListingInfo = collections.namedtuple(
     'ListingInfo',
-    ['command', 'filename', 'tags', 'filters', 'env', 'xfail'],
-    defaults=[None, None, False]
+    ['command', 'filename', 'tags', 'filters', 'env', 'xfail']
 )
 
 
@@ -28,9 +27,8 @@ def remove_system_opt(s):
     return s.replace(' --system=catalina', '')
 
 
-def replace_paths(s):
-    cwd = os.getcwd()
-    return s.replace(cwd, '/path/to/reframe').replace(os.getenv('HOME'), '/home/user')
+def replace_home(s):
+    return s.replace(os.getenv('HOME'), '/home/user')
 
 
 def replace_user(s):
@@ -44,53 +42,191 @@ def replace_hostname(s):
 
 
 DEFAULT_FILTERS = [remove_nocolor_opt, remove_system_opt,
-                   replace_paths, replace_user, replace_hostname]
+                   replace_home, replace_user, replace_hostname]
 
 
 LISTINGS = {
     'hello1': ListingInfo(
-        './bin/reframe --nocolor -c tutorials/basics/hello/hello1.py -r',
+        './bin/reframe -c tutorials/basics/hello/hello1.py -r',
         'docs/listings/hello1.txt',
-        {'local'},
-        DEFAULT_FILTERS
+        {'local', 'tutorial-basics'},
+        DEFAULT_FILTERS,
+        env={'RFM_COLORIZE': 'n'},
+        xfail=False
     ),
     'run-report': ListingInfo(
         f'cat {os.getenv("HOME")}/.reframe/reports/run-report.json',
         'docs/listings/run-report.json',
-        {'local'},
-        DEFAULT_FILTERS
+        {'local', 'tutorial-basics'},
+        DEFAULT_FILTERS,
+        env=None,
+        xfail=False
     ),
     'hello2': ListingInfo(
-        './bin/reframe --nocolor -c tutorials/basics/hello/hello2.py -r',
+        './bin/reframe -c tutorials/basics/hello/hello2.py -r',
         'docs/listings/hello2.txt',
-        {'local'},
+        {'local', 'tutorial-basics'},
         DEFAULT_FILTERS,
+        env={'RFM_COLORIZE': 'n'},
         xfail=True
     ),
     'hello2_catalina': ListingInfo(
-        './bin/reframe -C tutorials/config/settings.py --system=catalina --nocolor -c tutorials/basics/hello/hello2.py -r',
+        './bin/reframe -C tutorials/config/settings.py --system=catalina -c tutorials/basics/hello/hello2.py -r',
         'docs/listings/hello2_catalina.txt',
-        {'local'},
-        DEFAULT_FILTERS
+        {'local', 'tutorial-basics'},
+        DEFAULT_FILTERS,
+        env={'RFM_COLORIZE': 'n'},
+        xfail=False
     ),
     'hellomp1': ListingInfo(
-        './bin/reframe --system=catalina --nocolor -c tutorials/basics/hellomp/hellomp1.py -r',
+        './bin/reframe --system=catalina -c tutorials/basics/hellomp/hellomp1.py -r',
         'docs/listings/hellomp1.txt',
-        {'local'},
+        {'local', 'tutorial-basics'},
         DEFAULT_FILTERS,
         env={
-            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py')
-        }
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
     ),
     'hellomp2': ListingInfo(
-        './bin/reframe --system=catalina --nocolor -c tutorials/basics/hellomp/hellomp2.py -r',
+        './bin/reframe --system=catalina -c tutorials/basics/hellomp/hellomp2.py -r',
         'docs/listings/hellomp2.txt',
-        {'local'},
+        {'local', 'tutorial-basics'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=True
+    ),
+    'alltests_daint': ListingInfo(
+        './bin/reframe -c tutorials/basics/ -R -n "HelloMultiLangTest|HelloThreadedExtended2Test|StreamWithRefTest" --performance-report -r',
+        'docs/listings/alltests_daint.txt',
+        {'remote', 'tutorial-basics'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'stream4_daint': ListingInfo(
+        './bin/reframe -c tutorials/basics/stream/stream4.py -r --performance-report',
+        'docs/listings/stream4_daint.txt',
+        {'remote', 'tutorial-basics'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'osu_bench_deps': ListingInfo(
+        './bin/reframe -c tutorials/deps/osu_benchmarks.py -r',
+        'docs/listings/osu_bench_deps.txt',
+        {'remote', 'tutorial-deps'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'osu_latency_list': ListingInfo(
+        './bin/reframe -c tutorials/deps/osu_benchmarks.py -n OSULatencyTest -l',
+        'docs/listings/osu_latency_list.txt',
+        {'remote', 'tutorial-deps'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'osu_latency_unresolved_deps': ListingInfo(
+        './bin/reframe -c tutorials/deps/osu_benchmarks.py -n OSULatencyTest --system=daint:gpu -l',
+        'docs/listings/osu_latency_unresolved_deps.txt',
+        {'remote', 'tutorial-deps'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'osu_bench_list_concretized': ListingInfo(
+        './bin/reframe -c tutorials/deps/osu_benchmarks.py -lC',
+        'docs/listings/osu_bench_list_concretized.txt',
+        {'remote', 'tutorial-deps'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'osu_bench_list_concretized_gnu': ListingInfo(
+        './bin/reframe -c tutorials/deps/osu_benchmarks.py -n OSULatencyTest -L -p builtin -p gnu',
+        'docs/listings/osu_bench_list_concretized_gnu.txt',
+        {'remote', 'tutorial-deps'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'param_deps_list': ListingInfo(
+        './bin/reframe -c tutorials/deps/parameterized.py -l',
+        'docs/listings/param_deps_list.txt',
+        {'local', 'tutorial-deps'},
+        DEFAULT_FILTERS,
+        env=None,
+        xfail=False
+    ),
+    'osu_bench_fixtures_list': ListingInfo(
+        './bin/reframe -c tutorials/fixtures/osu_benchmarks.py -l',
+        'docs/listings/osu_bench_fixtures_list.txt',
+        {'remote', 'tutorial-fixtures'},
         DEFAULT_FILTERS,
         env={
             'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py')
         },
-        xfail=True
+        xfail=False
+    ),
+    'osu_bandwidth_concretized_daint': ListingInfo(
+        './bin/reframe -c tutorials/fixtures/osu_benchmarks.py -n osu_bandwidth_test -lC',
+        'docs/listings/osu_bandwidth_concretized_daint.txt',
+        {'remote', 'tutorial-fixtures'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'osu_bandwidth_concretized_daint_pgi': ListingInfo(
+        './bin/reframe -c tutorials/fixtures/osu_benchmarks.py -n osu_bandwidth_test -lC -p pgi',
+        'docs/listings/osu_bandwidth_concretized_daint_pgi.txt',
+        {'remote', 'tutorial-fixtures'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
+    ),
+    'osu_bench_fixtures_run': ListingInfo(
+        './bin/reframe -c tutorials/fixtures/osu_benchmarks.py -r',
+        'docs/listings/osu_bench_fixtures_run.txt',
+        {'remote', 'tutorial-fixtures'},
+        DEFAULT_FILTERS,
+        env={
+            'RFM_CONFIG_FILE': os.path.join(os.getcwd(), 'tutorials/config/settings.py'),
+            'RFM_COLORIZE': 'n'
+        },
+        xfail=False
     )
 }
 
@@ -104,7 +240,7 @@ if __name__ == '__main__':
         choice = 'all'
 
     for name, info in LISTINGS.items():
-        if choice != 'all' and choice != name:
+        if (choice != 'all' and choice not in info.tags and choice != name):
             continue
 
         print(f'Generating listing {name}...')
