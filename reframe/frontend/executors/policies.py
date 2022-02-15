b72b7eb95e7d3d448ccc29f17f8afacc71fa5421
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2022-02-15 19:25:40 +0100

    Minor changes

--- reframe/frontend/executors/policies.py
+++ reframe/frontend/executors/policies.py
@@ -181,7 +181,7 @@ class SerialExecutionPolicy(ExecutionPolicy, TaskEventListener):
 
     def on_task_failure(self, task):
         self._num_failed_tasks += 1
-        msg = f'{task.check.info()}'
+        msg = f'{task.info()}'
         if task.failed_stage == 'cleanup':
             self.printer.status('ERROR', msg, just='right')
         else:
@@ -202,7 +202,7 @@ class SerialExecutionPolicy(ExecutionPolicy, TaskEventListener):
             )
 
     def on_task_success(self, task):
-        msg = f'{task.check.info()}'
+        msg = f'{task.info()}'
         self.printer.status('OK', msg, just='right')
         timings = task.pipeline_timings(['setup',
                                          'compile_complete',
@@ -427,8 +427,6 @@ class AsynchronousExecutionPolicy(ExecutionPolicy, TaskEventListener):
                 return 1
         elif self.deps_succeeded(task):
             try:
-                part = task.testcase.partition
-                env  = task.testcase.environ.name
                 self.printer.status('RUN', task.info())
                 task.setup(task.testcase.partition,
                            task.testcase.environ,
@@ -453,7 +451,7 @@ class AsynchronousExecutionPolicy(ExecutionPolicy, TaskEventListener):
             return 1
         else:
             # Not all dependencies have finished yet
-            getlogger().debug2(f'{task.check.info()} waiting for dependencies')
+            getlogger().debug2(f'{task.info()} waiting for dependencies')
             return 0
 
     def _advance_ready_compile(self, task):
