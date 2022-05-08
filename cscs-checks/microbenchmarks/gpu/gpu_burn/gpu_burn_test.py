d4874bf496a7a7900e5cb3ef1bbd1ea334fe57d9
Vasileios Karakasis <karakasis@cscs.ch>
2022-05-08 13:28:38 +0200

    Base CSCS GPU burn test on the library version

--- cscs-checks/microbenchmarks/gpu/gpu_burn/gpu_burn_test.py
+++ cscs-checks/microbenchmarks/gpu/gpu_burn/gpu_burn_test.py
@@ -3,61 +3,41 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-
-import reframe as rfm
-import reframe.utility.sanity as sn
-import reframe.utility.osext as osext
-from reframe.core.exceptions import SanityError
-
+import reframe as rfm   # noqa: F501
 from hpctestlib.microbenchmarks.gpu.gpu_burn import gpu_burn_check
-import cscstests.microbenchmarks.gpu.hooks as hooks
 
 
 @rfm.simple_test
-class cscs_gpu_burn_check(GpuBurn):
-    use_doubles = True
+class cscs_gpu_burn_check(gpu_burn_check):
+    use_dp = True
     duration = 40
-    valid_systems = [
-        'daint:gpu', 'dom:gpu', 'arolla:cn', 'tsa:cn', 'ault:amdv100',
-        'ault:intelv100', 'ault:amda100', 'ault:amdvega'
-    ]
-    valid_prog_environs = ['PrgEnv-gnu']
     exclusive_access = True
     reference = {
         'dom:gpu': {
-            'min_perf': (4115, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (4115, -0.10, None, 'Gflop/s'),
         },
         'daint:gpu': {
-            'min_perf': (4115, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (4115, -0.10, None, 'Gflop/s'),
         },
         'arolla:cn': {
-            'min_perf': (5861, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (5861, -0.10, None, 'Gflop/s'),
         },
         'tsa:cn': {
-            'min_perf': (5861, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (5861, -0.10, None, 'Gflop/s'),
         },
         'ault:amda100': {
-            'min_perf': (15000, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (15000, -0.10, None, 'Gflop/s'),
         },
         'ault:amdv100': {
-            'min_perf': (5500, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (5500, -0.10, None, 'Gflop/s'),
         },
         'ault:intelv100': {
-            'min_perf': (5500, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (5500, -0.10, None, 'Gflop/s'),
         },
         'ault:amdvega': {
-            'min_perf': (3450, -0.10, None, 'Gflop/s'),
+            'gpu_perf_min': (3450, -0.10, None, 'Gflop/s'),
         },
     }
 
     maintainers = ['@vkarak']
     tags = {'diagnostic', 'benchmark', 'craype'}
-
-    # Inject external hooks
-    @run_after('setup')
-    def set_gpu_arch(self):
-        hooks.set_gpu_arch(self)
-
-    @run_before('run')
-    def set_num_gpus_per_node(self):
-        hooks.set_num_gpus_per_node(self)
