82f031495cffc50ae006ecc7f7b99ae48f07e715
Vasileios Karakasis <vkarak@gmail.com>
2022-11-18 23:08:39 +0100

    Fix tutorial CI

---
+++ tutorials/config/daint-mem.py
@@ -0,0 +1,101 @@
+# Copyright 2016-2022 Swiss National Supercomputing Centre (CSCS/ETH Zurich)
+# and other ReFrame Project Developers. See the top-level LICENSE file for
+# details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+# rfmdocstart: site-configuration
+site_configuration = {
+    'systems': [
+        {
+            'name': 'daint',
+            'descr': 'Piz Daint Supercomputer',
+            'hostnames': ['daint'],
+            'modules_system': 'tmod32',
+            'partitions': [
+                {
+                    'name': 'login',
+                    'descr': 'Login nodes',
+                    'scheduler': 'local',
+                    'launcher': 'local',
+                    'environs': ['builtin', 'gnu', 'intel', 'nvidia', 'cray'],
+                },
+                # rfmdocstart: memory
+                {
+                    'name': 'gpu',
+                    'descr': 'Hybrid nodes',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-C gpu', '-A csstaff'],
+                    'environs': ['gnu', 'intel', 'nvidia', 'cray'],
+                    'max_jobs': 100,
+                    'resources': [
+                        {
+                            'name': 'memory',
+                            'options': ['--mem={size}']
+                        }
+                    ]
+                },
+                {
+                    'name': 'mc',
+                    'descr': 'Multicore nodes',
+                    'scheduler': 'slurm',
+                    'launcher': 'srun',
+                    'access': ['-C mc', '-A csstaff'],
+                    'environs': ['gnu', 'intel', 'nvidia', 'cray'],
+                    'max_jobs': 100,
+                    'resources': [
+                        {
+                            'name': 'memory',
+                            'options': ['--mem={size}']
+                        }
+                    ]
+                }
+                # rfmdocend: memory
+            ]
+        }
+    ],
+    'environments': [
+        {
+            'name': 'gnu',
+            'modules': ['PrgEnv-gnu'],
+            'cc': 'cc',
+            'cxx': 'CC',
+            'ftn': 'ftn',
+            'target_systems': ['daint']
+        },
+        {
+            'name': 'cray',
+            'modules': ['PrgEnv-cray'],
+            'cc': 'cc',
+            'cxx': 'CC',
+            'ftn': 'ftn',
+            'target_systems': ['daint']
+        },
+        {
+            'name': 'intel',
+            'modules': ['PrgEnv-intel'],
+            'cc': 'cc',
+            'cxx': 'CC',
+            'ftn': 'ftn',
+            'target_systems': ['daint']
+        },
+        {
+            'name': 'nvidia',
+            'modules': ['PrgEnv-nvidia'],
+            'cc': 'cc',
+            'cxx': 'CC',
+            'ftn': 'ftn',
+            'target_systems': ['daint']
+        },
+        {
+            'name': 'builtin',
+            'cc': 'cc',
+            'cxx': 'CC',
+            'ftn': 'ftn',
+            'target_systems': ['daint']
+        }
+    ]
+}
+# rfmdocend: site-configuration
