a0a711f7de9ddafe5fbb4b367dc053f51b4e8539
Vasileios Karakasis <vkarak@gmail.com>
2023-02-11 21:06:53 +0100

    List any unknown tag in the `Other` section

--- ci-scripts/genrelnotes.py
+++ ci-scripts/genrelnotes.py
@@ -15,8 +15,8 @@ def usage():
     sys.stderr.write('Usage: %s PREV_RELEASE CURR_RELEASE\n' % sys.argv[0])
 
 
-def extract_release_notes(git_output, tag):
-    return re.findall(r'pull request (#\d+).*\s*\[%s\] (.*)' % tag, git_output)
+def extract_release_notes(git_output):
+    return re.findall(r'pull request (#\d+).*\s*\[(\S+)\] (.*)', git_output)
 
 
 if __name__ == '__main__':
@@ -39,20 +39,24 @@ if __name__ == '__main__':
         sys.stderr.write(e.stdout)
         sys.exit(1)
 
-    titles = {
+    tag_mapping = {
         'feat': '## New features',
         'enhancement': '## Enhancements',
         'doc': '## Enhancements',
         'bugfix': '## Bug fixes',
         'testlib': '## Test library'
     }
-    sections = collections.OrderedDict()
-    for tag in ['feat', 'enhancement', 'doc', 'bugfix', 'testlib', 'ci']:
-        title_line = titles.get(tag, '## Other')
-        sections.setdefault(title_line, [])
-        for pr, descr in extract_release_notes(completed.stdout, tag):
-            descr_line = '- %s (%s)' % (descr, pr)
-            sections[title_line].append(descr_line)
+    sections = {
+        '## New features': [],
+        '## Enhancements': [],
+        '## Bug fixes': [],
+        '## Test library': [],
+        '## Other': []
+    }
+    for pr, tag, descr in extract_release_notes(completed.stdout):
+        title_line = tag_mapping.get(tag, '## Other')
+        descr_line = f'- {descr} ({pr})'
+        sections[title_line].append(descr_line)
 
     print('# Release Notes')
     for sec_title, sec_lines in sections.items():
