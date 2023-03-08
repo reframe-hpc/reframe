d56d515662acfa9106764d17f76cdeda82ec6b08
Vasileios Karakasis <vkarak@gmail.com>
2023-03-08 23:07:15 +0100

    Minor style changes

--- unittests/test_buildsystems.py
+++ unittests/test_buildsystems.py
@@ -304,11 +304,12 @@ def test_spack_no_env(environ, tmp_path):
 
 def test_spack_env_config(environ, tmp_path):
     build_system = bs.Spack()
-    build_system.config_opts = ['section1:header1:value1', 'section2:header2:value2']
+    build_system.config_opts = ['section1:header1:value1',
+                                'section2:header2:value2']
     with osext.change_dir(tmp_path):
         assert build_system.emit_build_commands(environ) == [
             'spack env create -d rfm_spack_env',
-            'spack -e rfm_spack_env config add "config:install_tree:root:opt/spack"',
+            'spack -e rfm_spack_env config add "config:install_tree:root:opt/spack"',  # noqa: E501
             'spack -e rfm_spack_env config add "section1:header1:value1"',
             'spack -e rfm_spack_env config add "section2:header2:value2"',
             'spack -e rfm_spack_env install',
