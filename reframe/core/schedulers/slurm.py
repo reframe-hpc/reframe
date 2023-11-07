c4cf710aee1245c2eb9417b275353ee419cb7727
Eirini Koutsaniti <ekoutsaniti@gmail.com>
2023-11-07 13:23:50 +0100

    Add basic firecrest implementation

--- reframe/core/schedulers/slurm.py
+++ reframe/core/schedulers/slurm.py
@@ -3,9 +3,11 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+import firecrest as fc
 import functools
 import glob
 import itertools
+import os
 import re
 import shlex
 import time
@@ -64,6 +66,11 @@ def slurm_state_pending(state):
     return False
 
 
+def join_and_normalize(*args):
+    joined_path = os.path.join(*args)
+    normalized_path = os.path.normpath(joined_path)
+    return normalized_path
+
 _run_strict = functools.partial(osext.run_command, check=True)
 
 
@@ -87,6 +94,35 @@ class _SlurmJob(sched.Job):
             self._nodelist = completed.stdout.splitlines()
 
         return self._nodelist
+
+    @property
+    def is_array(self):
+        return self._is_array
+
+    @property
+    def is_cancelling(self):
+        return self._is_cancelling
+
+
+class _SlurmFirecrestJob(sched.Job):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._is_array = False
+        self._is_cancelling = False
+
+        # FIXME get this dynamically
+        self._remotedir_prefix = '/scratch/snx3000/eirinik/rfc_remote'
+        self._remotedir = None
+        self._localdir = None
+
+        self._local_filetimestamps = {}
+        self._remote_filetimestamps = {}
+        self._first_submission = True
+
+        # The compacted nodelist as reported by Slurm. This must be updated in
+        # every poll as Slurm may be slow in reporting the exact nodelist
+        self._nodespec = None
+        self._stage_prefix = rt.runtime().stage_prefix
 
     @property
     def is_array(self):
@@ -692,3 +728,222 @@ class _SlurmNode(sched.Node):
 
     def __str__(self):
         return self._name
+
+@register_scheduler('slurmfc')
+class SlurmFirecrestJobScheduler(SlurmJobScheduler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # FIXME set these in a better way
+        client_id = os.environ.get("FIRECREST_CLIENT_ID")
+        client_secret = os.environ.get("FIRECREST_CLIENT_SECRET")
+        token_uri = "https://auth.cscs.ch/auth/realms/firecrest-clients/protocol/openid-connect/token"
+
+        # Setup the client for the specific account
+        self.client = fc.Firecrest(
+            firecrest_url="https://firecrest.cscs.ch",
+            authorization=fc.ClientCredentialsAuth(client_id, client_secret, token_uri)
+        )
+        self._system_name = 'daint'
+
+    def make_job(self, *args, **kwargs):
+        return _SlurmFirecrestJob(*args, **kwargs)
+
+    def _push_artefacts(self, job):
+        for dirpath, dirnames, filenames in os.walk('.'):
+            for d in dirnames:
+                new_dir = join_and_normalize(job._remotedir, dirpath, d)
+                self.log(f'Creating remote directory {new_dir}')
+                self.client.mkdir(self._system_name, new_dir, p=True)
+
+            remote_dir_path = join_and_normalize(job._remotedir, dirpath)
+            for f in filenames:
+                local_norm_path = join_and_normalize(job._localdir, dirpath, f)
+                modification_time = os.path.getmtime(local_norm_path)
+                if job._local_filetimestamps.get(local_norm_path) != modification_time:
+                    job._local_filetimestamps[local_norm_path] = modification_time
+
+                    self.log(f'Uploading file {f} in {join_and_normalize(job._remotedir, dirpath)}')
+                    self.client.simple_upload(
+                        self._system_name,
+                        local_norm_path,
+                        remote_dir_path
+                    )
+
+            # Update timestamps for remote directory
+            remote_files = self.client.list_files(
+                self._system_name,
+                remote_dir_path,
+                show_hidden=True
+            )
+            for f in remote_files:
+                local_norm_path = join_and_normalize(remote_dir_path, f['name'])
+                job._remote_filetimestamps[local_norm_path] = f['last_modified']
+
+
+
+    def _pull_artefacts(self, job):
+        def firecrest_walk(directory):
+            contents = self.client.list_files(self._system_name, directory)
+
+            dirs = []
+            nondirs = []
+
+            for item in contents:
+                if item['type'] == 'd':
+                    dirs.append(item['name'])
+                else:
+                    nondirs.append((item['name'], item["last_modified"]))
+
+            yield directory, dirs, nondirs
+
+            for item in dirs:
+                item_path = f"{directory}/{item['name']}"
+                yield from firecrest_walk(item_path)
+
+        for dirpath, dirnames, files in firecrest_walk(job._remotedir):
+            local_dirpath = join_and_normalize(
+                job._localdir,
+                os.path.relpath(
+                    dirpath,
+                    job._remotedir
+                )
+            )
+            for d in dirnames:
+                new_dir = join_and_normalize(local_dirpath, d)
+                self.log(f'Creating local directory {new_dir}')
+                os.makedirs(new_dir)
+
+            for (f, modification_time) in files:
+                norm_path = join_and_normalize(dirpath, f)
+                local_file_path = join_and_normalize(local_dirpath, f)
+                if job._remote_filetimestamps.get(norm_path) != modification_time:
+                    self.log(f'Downloading file {f} in {local_dirpath}')
+                    self.client.simple_download(
+                        self._system_name,
+                        norm_path,
+                        local_file_path
+                    )
+
+                    job._remote_filetimestamps[norm_path] = modification_time
+
+                job._local_filetimestamps[local_file_path] = os.path.getmtime(local_file_path)
+
+
+    def submit(self, job):
+        job._localdir = os.getcwd()
+        job._remotedir = os.path.join(
+            job._remotedir_prefix,
+            os.path.relpath(os.getcwd(), job._stage_prefix)
+        )
+
+        if job._first_submission:
+            # Create clean stage directory in the remote system
+            try:
+                self.client.simple_delete(self._system_name, job._remotedir)
+            except fc.HeaderException:
+                # The delete request will raise an exception if it doesn't exist
+                pass
+
+            self.client.mkdir(self._system_name, job._remotedir, p=True)
+            job._first_submission = False
+
+            self._push_artefacts(job)
+
+        intervals = itertools.cycle([1, 2, 3])
+        while True:
+            try:
+                # Make request for submission
+                submission_result = self.client.submit(
+                    self._system_name,
+                    os.path.join(job._remotedir, job.script_filename),
+                    local_file=False
+                )
+                break
+            except fc.FirecrestException as e:
+                stderr = e.responses[-1].json().get('error', '')
+                error_match = re.search(
+                    rf'({"|".join(self._resubmit_on_errors)})', stderr
+                )
+                if not self._resubmit_on_errors or not error_match:
+                    raise
+
+                t = next(intervals)
+                self.log(
+                    f'encountered a job submission error: '
+                    f'{error_match.group(1)}: will resubmit after {t}s'
+                )
+                time.sleep(t)
+
+        job._jobid = str(submission_result['jobid'])
+        job._submit_time = time.time()
+
+    def allnodes(self):
+        raise NotImplementedError('firecrest slurm backend does not support node listing')
+
+    def filternodes(self, job, nodes):
+        raise NotImplementedError(
+            'firecrest slurm backend does not support node filtering'
+        )
+
+    def poll(self, *jobs):
+        '''Update the status of the jobs.'''
+
+        if jobs:
+            # Filter out non-jobs
+            jobs = [job for job in jobs if job is not None]
+
+        if not jobs:
+            return
+
+        poll_results = self.client.poll(
+            self._system_name, [job.jobid for job in jobs]
+        )
+        job_info = {}
+        for r in poll_results:
+            # Take into account both job arrays and heterogeneous jobs
+            jobid = re.split(r'_|\+', r['jobid'])[0]
+            job_info.setdefault(jobid, []).append(r)
+
+        for job in jobs:
+            try:
+                jobarr_info = job_info[job.jobid]
+            except KeyError:
+                continue
+
+            # Join the states with ',' in case of job arrays|heterogeneous jobs
+            job._state = ','.join(m['state'] for m in jobarr_info)
+
+            self._cancel_if_pending_too_long(job)
+            if slurm_state_completed(job.state):
+                # Since Slurm exitcodes are positive take the maximum one
+                job._exitcode = max(
+                    int(m['exit_code'].split(":")[0]) for m in jobarr_info
+                )
+
+            # Use ',' to join nodes to be consistent with Slurm syntax
+            job._nodespec = ','.join(m['nodelist'] for m in jobarr_info)
+            # self._update_completion_time(
+            #     job, (m.group('end') for m in jobarr_info)
+            # )
+
+    def wait(self, job):
+        # Quickly return in case we have finished already
+        self._pull_artefacts(job)
+        if self.finished(job):
+            if job.is_array:
+                self._merge_files(job)
+
+            return
+
+        intervals = itertools.cycle([1, 2, 3])
+        while not self.finished(job):
+            self.poll(job)
+            time.sleep(next(intervals))
+
+        self._pull_artefacts(job)
+        if job.is_array:
+            self._merge_files(job)
+
+    def cancel(self, job):
+        self.client.cancel(job.system_name, job.jobid)
+        job._is_cancelling = True
