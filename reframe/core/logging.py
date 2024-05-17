538320093018ae68a9d20692386ace52a5b7c9b8
Vasileios Karakasis <vkarak@gmail.com>
2024-05-17 23:42:03 +0200

    Fix logging crash when log format is `%(check_#ALL)s`

--- reframe/core/logging.py
+++ reframe/core/logging.py
@@ -301,8 +301,8 @@ class CheckFieldFormatter(logging.Formatter):
     # NOTE: This formatter will work only for the '%' style
     def __init__(self, fmt=None, datefmt=None, perffmt=None,
                  ignore_keys=None, style='%'):
-        super().__init__(fmt, datefmt, style)
-
+        super().__init__(fmt, datefmt, style,
+                         validate=(fmt != '%(check_#ALL)s'))
         self.__fmt = fmt
         self.__fmtperf = perffmt[:-1] if perffmt else ''
         self.__specs = re.findall(r'\%\((\S+?)\)s', fmt)
