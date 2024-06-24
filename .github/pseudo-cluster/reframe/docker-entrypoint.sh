af35910b3009a7a3967fc8ebf637d5901b43822f
Vasileios Karakasis <vkarak@gmail.com>
2024-06-25 00:37:53 +0200

    Re-enable codecov in the project
    We now combine the coverage from all unit tests that are run with
    different containers, so we have the overall coverage of the project.

--- .github/pseudo-cluster/reframe/docker-entrypoint.sh
+++ .github/pseudo-cluster/reframe/docker-entrypoint.sh
@@ -8,10 +8,11 @@ sudo service munge start
 cp -r /usr/local/share/reframe .
 cd reframe
 ./bootstrap.sh
+pip install pytest-cov
 
 echo "Running unittests with backend scheduler: ${BACKEND}"
 
 tempdir=$(mktemp -d -p /scratch)
-TMPDIR=$tempdir ./test_reframe.py -v \
+TMPDIR=$tempdir ./test_reframe.py --cov=reframe --cov-report=xml \
     --rfm-user-config=ci-scripts/configs/ci-cluster.py \
     --rfm-user-system=pseudo-cluster:compute-${BACKEND:-squeue}
