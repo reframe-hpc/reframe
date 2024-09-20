32e50c20390373617e31010bae557d06fc8a4a35
Brandon Cook <bgcook@lbl.gov>
2024-09-20 16:22:33 -0700

    fix: use decorator variant with arguments
    The variant of the decorator without arguments was added in Python 3.8.

--- reframe/core/modules.py
+++ reframe/core/modules.py
@@ -1031,7 +1031,7 @@ class LModImpl(TMod4Impl):
 
         return ret
 
-    @functools.lru_cache
+    @functools.lru_cache(maxsize=128)
     def conflicted_modules(self, module):
         if module.collection:
             # Conflicts have no meaning in module collection. The modules
