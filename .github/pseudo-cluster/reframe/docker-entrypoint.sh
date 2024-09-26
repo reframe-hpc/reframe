45541fb2da9fcc66e18b077f6e1a612743376835
Theofilos Manitaras <manitaras@cscs.ch>
2024-09-26 12:02:14 +0200

    Address PR comments
    Signed-off-by: Theofilos Manitaras <manitaras@cscs.ch>

--- .github/pseudo-cluster/reframe/docker-entrypoint.sh
+++ .github/pseudo-cluster/reframe/docker-entrypoint.sh
@@ -20,5 +20,5 @@ echo "Running unittests with backend scheduler: ${BACKEND}"
 
 tempdir=$(mktemp -d -p /scratch)
 TMPDIR=$tempdir ./test_reframe.py -v \
-    --rfm-user-config=config/ci-cluster.py \
+    --rfm-user-config=ci-scripts/configs/ci-cluster.py \
     --rfm-user-system=pseudo-cluster:compute-${BACKEND:-squeue}
